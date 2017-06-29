8beab97a7053659371e2710396edb6bfb33219b2
Catorpilor <jiaojiangonghitster@gmail.com>
2017-06-29 14:49:19 +0800

    simplify

--- merge-two-sorted-lists/sort.go
+++ merge-two-sorted-lists/sort.go
@@ -11,22 +11,20 @@ func MergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	go func() {
 		defer close(min)
 		for {
-			if l1 != nil && l2 != nil {
-				if l1.Val < l2.Val {
-					min <- l1
-					l1 = l1.Next
-				} else {
-					min <- l2
-					l2 = l2.Next
-				}
-			} else if l1 == nil && l2 != nil {
+			if l1 == nil {
 				min <- l2
 				return
-			} else if l2 == nil && l1 != nil {
+			}
+			if l2 == nil {
 				min <- l1
 				return
+			}
+			if l1.Val < l2.Val {
+				min <- l1
+				l1 = l1.Next
 			} else {
-				return
+				min <- l2
+				l2 = l2.Next
 			}
 		}
 	}()
