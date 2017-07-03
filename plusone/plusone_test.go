b636c3e518a5fa7a5f1824ec72fb7a0956c2242f
Catorpilor <jiaojiangonghitster@gmail.com>
2017-07-03 22:04:48 +0800

    plus one

---
+++ plusone/plusone_test.go
@@ -0,0 +1,28 @@
+package plusone
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPlusOne(t *testing.T) {
+	cases := []struct {
+		name   string
+		inputs []int
+		expect []int
+	}{
+		{"1 digit is zero", []int{0}, []int{1}},
+		{"1 digit is 9", []int{9}, []int{1, 0}},
+		{"2 normal digits", []int{2, 3}, []int{2, 4}},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			ret := PlusOne(c.inputs)
+			if !reflect.DeepEqual(ret, c.expect) {
+				t.Fatalf("expected %v, but got %v, with inputs %v",
+					c.expect, ret, c.inputs)
+			}
+		})
+	}
+	t.Log("passed")
+}
