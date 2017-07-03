2a9f0b0ebd38e43b84219b10036b7f385d6ee104
Catorpilor <jiaojiangonghitster@gmail.com>
2017-07-03 15:54:23 +0800

    sub testing

--- strstr/strstr_test.go
+++ strstr/strstr_test.go
@@ -2,27 +2,28 @@ package strstr
 
 import "testing"
 
-type res struct {
-	haystack string
-	needle   string
-	expect   int
-}
-
 func TestStrStr(t *testing.T) {
-	cases := []*res{
-		&res{"", "", 0},
-		&res{"", "1", -1},
-		&res{"hello", "el", 1},
-		&res{"aaa", "aaaa", -1},
-		&res{"aaa", "bbb", -1},
-		&res{"mississippi", "pi", 9},
+	cases := []struct {
+		name     string
+		haystack string
+		needle   string
+		expect   int
+	}{
+		{"empty haystack and needle", "", "", 0},
+		{"empty haystack", "", "1", -1},
+		{"hello_el", "hello", "el", 1},
+		{"aaa_aaaa", "aaa", "aaaa", -1},
+		{"aaa_bbb", "aaa", "bbb", -1},
+		{"mississippi_pi", "mississippi", "pi", 9},
 	}
 	for _, c := range cases {
-		ret := StrStr(c.haystack, c.needle)
-		if ret != c.expect {
-			t.Fatalf("expected %d, but got %d, with inputs (%s,%s)",
-				c.expect, ret, c.haystack, c.needle)
-		}
+		t.Run(c.name, func(t *testing.T) {
+			ret := StrStr(c.haystack, c.needle)
+			if ret != c.expect {
+				t.Fatalf("expected %d, but got %d, with inputs (%s,%s)",
+					c.expect, ret, c.haystack, c.needle)
+			}
+		})
 	}
 	t.Log("passed")
 }
