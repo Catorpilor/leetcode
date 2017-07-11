8f407bb1ee09cf66ef64cc2081b02eabb9d4c17a
Catorpilor <jiaojiangonghitster@gmail.com>
2017-07-11 21:00:21 +0800

    excel sheet column ttile

---
+++ 168_excel_sheet_column_title/excel_test.go
@@ -0,0 +1,25 @@
+package excel
+
+import "testing"
+
+func TestConvertToTitle(t *testing.T) {
+	cases := []struct {
+		name   string
+		n      int
+		expect string
+	}{
+		{"1", 1, "A"},
+		{"27", 27, "AA"},
+		{"28", 28, "AB"},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			ret := ConvertToTitle(c.n)
+			if ret != c.expect {
+				t.Fatalf("expected %s, but got %s, with input %d",
+					c.expect, ret, c.n)
+			}
+		})
+	}
+	t.Log("pass")
+}
