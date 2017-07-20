b498da8c5ea6e4fd81f51db11e7e79b8241b3dde
Catorpilor <jiaojiangonghitster@gmail.com>
2017-07-20 17:02:49 +0800

    house robber

---
+++ 198_house_robber/rob.go
@@ -0,0 +1,28 @@
+package robber
+
+func Rob(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+	if l == 1 {
+		return nums[l-1]
+	}
+	sum := make([]int, l)
+	_ = copy(sum, nums)
+
+	for i := 1; i < l; i++ {
+		for j := 0; j < i; j++ {
+			if j+1 != i {
+				if nums[i]+sum[j] > sum[i] {
+					sum[i] = nums[i] + sum[j]
+				}
+			} else {
+				if sum[j] > sum[i] {
+					sum[i] = sum[j]
+				}
+			}
+		}
+	}
+	return sum[l-1]
+}
---
+++ 198_house_robber/rob_test.go
@@ -0,0 +1,31 @@
+package robber
+
+import "testing"
+
+func TestRob(t *testing.T) {
+	cases := []struct {
+		name   string
+		nums   []int
+		expect int
+	}{
+		{"nil slice", nil, 0},
+		{"single element", []int{1}, 1},
+		{"2 elements", []int{1, 2}, 2},
+		{"3 elements", []int{2, 1, 4}, 6},
+		{"3 identical elements", []int{1, 1, 1}, 2},
+		{"4 elements", []int{1, 2, 1, 1}, 3},
+		{"5 elements", []int{2, 1, 4, 3, 7}, 13},
+		{"6 elements", []int{2, 1, 4, 3, 7, 5}, 13},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			ret := Rob(c.nums)
+			if c.expect != ret {
+				t.Fatalf("expected %d but got %d, with inputs %v",
+					c.expect, ret, c.nums)
+			}
+		})
+	}
+	t.Log("pass")
+}
