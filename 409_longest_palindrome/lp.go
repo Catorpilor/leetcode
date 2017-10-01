5197394d361ccd7a4d01d1d9e73c193c72596471
Catorpilor <jiaojiangonghitster@gmail.com>
2017-10-02 00:14:57 +0800

    my version with better code

--- 409_longest_palindrome/lp.go
+++ 409_longest_palindrome/lp.go
@@ -18,5 +18,28 @@ func LongestPalindrome(s string) int {
 		return count*2 + 1
 	}
 	return count * 2
+}
 
+func LongestPalindrome2(s string) int {
+	if len(s) <= 1 {
+		return len(s)
+	}
+	h := [58]int{}
+	for _, c := range s {
+		h[c-'A'] += 1
+	}
+	eve, odd := 0, 0
+	for _, v := range h {
+		// this is the trick part
+		// but better than my first version
+		// which i thought
+		// if it is odd plus v - 1
+		// otherwise plus v
+		eve += v & ^1
+		if v%2 == 1 {
+			// only add once
+			odd = 1
+		}
+	}
+	return eve + odd
 }
--- 409_longest_palindrome/lp_test.go
+++ 409_longest_palindrome/lp_test.go
@@ -2,7 +2,7 @@ package lp
 
 import "testing"
 
-func TestLongestPalindrome(t *testing.T) {
+func TestLongestPalindrome2(t *testing.T) {
 	st := []struct {
 		name, s string
 		exp     int
@@ -15,7 +15,7 @@ func TestLongestPalindrome(t *testing.T) {
 	}
 	for _, c := range st {
 		t.Run(c.name, func(t *testing.T) {
-			ret := LongestPalindrome(c.s)
+			ret := LongestPalindrome2(c.s)
 			if ret != c.exp {
 				t.Fatalf("expected %d but got %d, with input %s",
 					c.exp, ret, c.s)
