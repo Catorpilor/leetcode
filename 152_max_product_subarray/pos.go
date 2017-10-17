dc7f6fd5f401c7409727025a5cb50e2a67a4ad3b
Catorpilor <jiaojiangonghitster@gmail.com>
2017-10-17 19:56:03 +0800

    simple dp solution

--- 152_max_product_subarray/pos.go
+++ 152_max_product_subarray/pos.go
@@ -9,6 +9,9 @@ func MaxPos(nums []int) int {
 	if n == 1 {
 		return nums[0]
 	}
+	// maxP stores the product for ith element in nums
+	// ex : nums = [7,-2,-4]
+	// related maxP = [7,-2,56]
 	maxP := make([]int, n)
 	maxP[0] = nums[0]
 	ret := maxP[0]
@@ -19,11 +22,14 @@ func MaxPos(nums []int) int {
 	for i := 1; i < n; i++ {
 		if nums[i] > 0 || lastNidx == n {
 			maxP[i] = utils.Max(nums[i], nums[i]*maxP[i-1])
+			// travese until the first negtive number occurs
+			// set lastIdx to i
 			if nums[i] < 0 {
 				lastNidx = i
 			}
 		} else {
 			tempM, j := 1, i
+			// go back to the lastNidx
 			for ; j >= lastNidx; j-- {
 				tempM *= nums[j]
 			}
@@ -36,4 +42,26 @@ func MaxPos(nums []int) int {
 		ret = utils.Max(ret, maxP[i])
 	}
 	return ret
+}
+
+func MaxPos2(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	// maxsoFar stores the max product we've found
+	// so far
+	maxSofar := nums[0]
+	for i, maxv, minv := 1, maxSofar, maxSofar; i < len(nums); i++ {
+		// multipled by a negative number makes a big number smaller,
+		// small number bigger, so we swap them
+		if nums[i] < 0 {
+			maxv, minv = minv, maxv
+		}
+		maxv = utils.Max(nums[i], maxv*nums[i])
+		minv = utils.Min(nums[i], minv*nums[i])
+
+		maxSofar = utils.Max(maxSofar, maxv)
+	}
+	return maxSofar
+
 }
--- 152_max_product_subarray/pos_test.go
+++ 152_max_product_subarray/pos_test.go
@@ -27,3 +27,29 @@ func TestMaxPos(t *testing.T) {
 		})
 	}
 }
+
+func TestMaxPos2(t *testing.T) {
+	st := []struct {
+		name string
+		nums []int
+		exp  int
+	}{
+		{"single element", []int{1}, 1},
+		{"two positive elements", []int{2, 3}, 6},
+		{"two with one negtive", []int{2, -3}, 2},
+		{"failed1", []int{7, -2, -4}, 56},
+		{"failed2", []int{2, -5, -2, -4, 3}, 24},
+		{"test1", []int{2, 3, -2, 4}, 6},
+		{"test2", []int{-2, -3, -3, 2, 4, -4}, 576},
+	}
+
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			ret := MaxPos2(c.nums)
+			if ret != c.exp {
+				t.Fatalf("expected %d but got %d, with inputs %v",
+					c.exp, ret, c.nums)
+			}
+		})
+	}
+}
