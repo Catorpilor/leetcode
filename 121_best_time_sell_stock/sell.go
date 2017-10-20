b1127b7de863988fa06db049d8dccb2d59ec5dea
Catorpilor <jiaojiangonghitster@gmail.com>
2017-10-20 19:27:21 +0800

    using utils.Max instead

--- 121_best_time_sell_stock/sell.go
+++ 121_best_time_sell_stock/sell.go
@@ -1,19 +1,14 @@
 package sell
 
+import "github.com/catorpilor/leetcode/utils"
+
 func Sell(prices []int) int {
 	maxCur := 0
 	maxSofar := 0
 	for i := 1; i < len(prices); i++ {
 		maxCur += prices[i] - prices[i-1]
-		maxCur = max(0, maxCur)
-		maxSofar = max(maxCur, maxSofar)
+		maxCur = utils.Max(0, maxCur)
+		maxSofar = utils.Max(maxCur, maxSofar)
 	}
 	return maxSofar
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
