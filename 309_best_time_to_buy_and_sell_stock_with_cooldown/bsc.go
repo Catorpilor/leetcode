d5bba72c973ee3c5b0e30d83ff3c2adad5958aaa
Catorpilor <jiaojiangonghitster@gmail.com>
2017-10-21 16:22:34 +0800

    add comments and O(1) space algorithm

--- 309_best_time_to_buy_and_sell_stock_with_cooldown/bsc.go
+++ 309_best_time_to_buy_and_sell_stock_with_cooldown/bsc.go
@@ -1,12 +1,28 @@
 package bsc
 
-import "github.com/catorpilor/leetcode/utils"
+import (
+	"math"
+
+	"github.com/catorpilor/leetcode/utils"
+)
 
 // MaxProfit returns the maxprofit for prices
 func MaxProfit(prices []int) int {
 	if len(prices) < 2 {
 		return 0
 	}
+
+	// buy[i] represents the max profit before day i which end with buying at i
+	// sell[i] represents the max profit before day i which end with selling at i
+	// rest[i] represents the max profit before day i which end with resting at i.
+	// so we have
+	// buy[i] = max(buy[i-1],rest[i-1]-prices[i])
+	// sell[i] = max(sell[i-1],buy[i-1]+price[i])
+	// rest[i] = max(sell[i-1],buy[i-1],rest[i-1])
+	// for rest[i] we can ensure that it equals to sell[i-1]
+	// so the aformentioned three equations can be rewirte to 2
+	// buy[i] = max(buy[i-1],sell[i-2]-prices[i])
+	// sell[i] = max(sell[i-1],buy[i-1]+prices[i])
 	buy, sell := make([]int, len(prices)), make([]int, len(prices))
 	buy[0] = -prices[0]
 	buy[1] = utils.Max(buy[0], -prices[1])
@@ -16,4 +32,22 @@ func MaxProfit(prices []int) int {
 		sell[i] = utils.Max(sell[i-1], buy[i-1]+prices[i])
 	}
 	return sell[len(prices)-1]
+}
+
+func MaxProfit2(prices []int) int {
+	n := len(prices)
+	if n < 2 {
+		return 0
+	}
+	// since the aformentioned two euqations have to do with only three variables
+	// so we can reduce the O(n) space to O(1)
+	var prev_buy, prev_sell, buy, sell int
+	buy = math.MinInt32
+	for _, p := range prices {
+		prev_buy = buy
+		buy = utils.Max(prev_buy, prev_sell-p)
+		prev_sell = sell
+		sell = utils.Max(prev_sell, prev_buy+p)
+	}
+	return sell
 }
