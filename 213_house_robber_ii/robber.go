e8ba5dbafe34c80672b9f83d437afd340a80828c
Catorpilor <jiaojiangonghitster@gmail.com>
2017-10-28 22:28:13 +0800

    house robber ii

--- 213_house_robber_ii/robber.go
+++ 213_house_robber_ii/robber.go
@@ -1,8 +1,27 @@
 package robber
 
+import "github.com/catorpilor/leetcode/utils"
+
 func Robber(nums []int) int {
 	// brute force
 	if nums == nil || len(nums) == 0 {
 		return 0
 	}
+	n := len(nums)
+	if n == 1 {
+		return nums[0]
+	}
+
+	return utils.Max(rob(nums, 0, n-2), rob(nums, 1, n-1))
+
+}
+
+func rob(nums []int, l, r int) int {
+	var include, exclude int
+	for i := l; i <= r; i++ {
+		t, e := include, exclude
+		include = e + nums[i]
+		exclude = utils.Max(t, e)
+	}
+	return utils.Max(include, exclude)
 }
--- 213_house_robber_ii/robber_test.go
+++ 213_house_robber_ii/robber_test.go
@@ -13,7 +13,7 @@ func TestRobber(t *testing.T) {
 		{"single element", []int{1}, 1},
 		{"two elements", []int{1, 2}, 2},
 		{"3 elements", []int{1, 2, 3}, 3},
-		{"test 31524", []int{3, 1, 5, 2, 4}, 8},
+		{"test 31524", []int{3, 1, 5, 2, 4}, 9},
 	}
 	for _, c := range st {
 		t.Run(c.name, func(t *testing.T) {
