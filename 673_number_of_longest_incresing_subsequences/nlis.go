232019376d074e7f45539cc2415ee674db7ba5d7
Catorpilor <jiaojiangonghitster@gmail.com>
2017-11-07 22:27:54 +0800

    no need to handle nums[i] <= nums[j]

--- 673_number_of_longest_incresing_subsequences/nlis.go
+++ 673_number_of_longest_incresing_subsequences/nlis.go
@@ -1,7 +1,5 @@
 package nlis
 
-import "github.com/catorpilor/leetcode/utils"
-
 func NumberOfLIS(nums []int) int {
 	n := len(nums)
 	if n <= 1 {
@@ -10,9 +8,8 @@ func NumberOfLIS(nums []int) int {
 	// dp[i] represents # of lis ended with nums[i]
 	// mLen[i] represents the length of LIS ended with nums[i]
 	dp, mLen := make([]int, n), make([]int, n)
-	dp[0] = 1
 	for i := range mLen {
-		mLen[i] = 1
+		dp[i], mLen[i] = 1, 1
 	}
 	for i := 1; i < n; i++ {
 		for j := i - 1; j >= 0; j-- {
@@ -23,8 +20,6 @@ func NumberOfLIS(nums []int) int {
 				} else if mLen[i] == mLen[j]+1 {
 					dp[i] += dp[j]
 				}
-			} else {
-				dp[i] = utils.Max(dp[i], 1)
 			}
 		}
 	}
