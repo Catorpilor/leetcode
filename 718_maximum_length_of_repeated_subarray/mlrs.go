5b80d39bbbc9022907351adea9890ebceee31043
Catorpilor <jiaojiangonghitster@gmail.com>
2017-11-09 22:31:35 +0800

    rolling hash

--- 718_maximum_length_of_repeated_subarray/mlrs.go
+++ 718_maximum_length_of_repeated_subarray/mlrs.go
@@ -1,6 +1,8 @@
 package mlrs
 
 import (
+	"reflect"
+
 	"github.com/catorpilor/leetcode/utils"
 )
 
@@ -28,4 +30,72 @@ func MaxLength(nums1, nums2 []int) int {
 		}
 	}
 	return ret
+}
+
+func MaxLength2(nums1, nums2 []int) int {
+	// rolling hash
+	n1, n2 := len(nums1), len(nums2)
+	if n1 == 0 || n2 == 0 {
+		return 0
+	}
+	low, hi := 0, utils.Min(n1, n2)+1
+	for low < hi {
+		mid := (hi-low)/2 + low
+		if check(mid, nums1, nums2) {
+			low = mid + 1
+		} else {
+			hi = mid
+		}
+
+	}
+	if low > 0 {
+		return low - 1
+	}
+	return 0
+}
+
+const base = 16777619
+
+func check(length int, nums1, nums2 []int) bool {
+	hash := make(map[uint32][]int)
+	for i, v := range rollingHash(nums1, length) {
+		if _, ok := hash[v]; ok {
+			hash[v] = append(hash[v], i)
+		} else {
+			hash[v] = []int{i}
+		}
+	}
+	for j, v := range rollingHash(nums2, length) {
+		for _, i := range hash[v] {
+			if reflect.DeepEqual(nums1[i:i+length], nums2[j:j+length]) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func rollingHash(nums []int, l int) []uint32 {
+	if l == 0 {
+		return nil
+	}
+	var multi uint32 = 1
+	for i := 0; i < l-1; i++ {
+		multi *= base
+	}
+
+	ret := make([]uint32, len(nums)-l+1)
+	ret[0] = hash(nums[:l])
+	for i := 1; i < len(nums)-l+1; i++ {
+		ret[i] = (ret[i-1]-multi*uint32(nums[i-1]))*base + uint32(nums[i+l-1])
+	}
+	return ret
+}
+
+func hash(s []int) uint32 {
+	var h uint32
+	for _, v := range s {
+		h = h*base + uint32(v)
+	}
+	return h
 }
--- 718_maximum_length_of_repeated_subarray/mlrs_test.go
+++ 718_maximum_length_of_repeated_subarray/mlrs_test.go
@@ -24,3 +24,27 @@ func TestMaxLength(t *testing.T) {
 		})
 	}
 }
+
+func TestMaxLength2(t *testing.T) {
+	st := []struct {
+		name         string
+		nums1, nums2 []int
+		exp          int
+	}{
+		{"nil nums1", nil, []int{1, 2, 3}, 0},
+		{"nil nums2", []int{1, 2, 3}, nil, 0},
+		{"single element", []int{1}, []int{1, 2, 3}, 1},
+		{"test 1", []int{1, 2, 3, 2, 1}, []int{3, 2, 1, 4, 7}, 3},
+		{"failed 1", []int{0, 1, 1, 1, 1}, []int{1, 0, 1, 0, 1}, 2},
+		{"failed 2", []int{70, 39, 25, 40, 7}, []int{52, 20, 67, 5, 31}, 0},
+	}
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			ret := MaxLength2(c.nums1, c.nums2)
+			if ret != c.exp {
+				t.Fatalf("expected %d but got %d ,with input %v and %v",
+					c.exp, ret, c.nums1, c.nums2)
+			}
+		})
+	}
+}
