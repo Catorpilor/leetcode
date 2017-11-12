a676f6a61b1eed649c0333b5cba76ec0995a796b
Catorpilor <jiaojiangonghitster@gmail.com>
2017-11-12 20:34:47 +0800

    with comments

--- 650_2_keys_keyboard/keys.go
+++ 650_2_keys_keyboard/keys.go
@@ -5,6 +5,12 @@ func MinStep(n int) int {
 		return 0
 	}
 	dp := make([]int, n+1)
+	// dp[k] represents the **min steps** to get k 'A's
+	// so to get min steps, the action must be ended with paste
+	// suppose the last copy-all pressed when there are j 'A' s
+	// to get k 'A's we have to (k-j)/j steps with paste
+	// so the total steps to get k 'A's from j 'A's is 1 (copy-all) + (k-j)/j (paste)
+	// so we have this formular dp[k] = j + dp[k/j]
 	for i := 1; i <= n; i++ {
 		dp[i] = i
 		for j := 2; j < i; j++ {
