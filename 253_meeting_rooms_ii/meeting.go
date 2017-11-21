fc48be8c5db3eb2a957139133688212af8facfaa
Catorpilor <jiaojiangonghitster@gmail.com>
2017-11-21 19:46:41 +0800

    two more intresting solutions for meeting rooms

--- 253_meeting_rooms_ii/meeting.go
+++ 253_meeting_rooms_ii/meeting.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math"
 	"sort"
+
+	"github.com/catorpilor/leetcode/utils"
 )
 
 type interval struct {
@@ -88,6 +90,60 @@ func MinRooms2(intervals []interval) int {
 		}
 		// }
 	}
-	fmt.Println(rooms)
 	return len(rooms)
+}
+
+func MinRooms3(intervals []interval) int {
+	n := len(intervals)
+	if n <= 1 {
+		return n
+	}
+	times := make([]int, n*2)
+	for i := range intervals {
+		times[2*i] = intervals[i].Start
+		times[2*i+1] = -intervals[i].End
+	}
+	sort.Slice(times, func(i, j int) bool {
+		if utils.Abs(times[i]) == utils.Abs(times[j]) {
+			return i < j
+		}
+		return utils.Abs(times[i]) < utils.Abs(times[j])
+	})
+	var cur, ret int
+	for i := range times {
+		if times[i] >= 0 {
+			cur += 1
+			ret = utils.Max(ret, cur)
+		} else {
+			cur -= 1
+		}
+	}
+	fmt.Println(times)
+	return ret
+}
+
+func MinRooms4(intervals []interval) int {
+	n := len(intervals)
+	if n <= 1 {
+		return n
+	}
+	start, end := make([]int, n), make([]int, n)
+	for i := range intervals {
+		start[i], end[i] = intervals[i].Start, intervals[i].End
+	}
+	sort.Slice(start, func(i, j int) bool {
+		return start[i] < start[j]
+	})
+	sort.Slice(end, func(i, j int) bool {
+		return end[i] < end[j]
+	})
+	var rooms, endIdx int
+	for i := range start {
+		if start[i] < end[endIdx] {
+			rooms += 1
+		} else {
+			endIdx += 1
+		}
+	}
+	return rooms
 }
--- 253_meeting_rooms_ii/meeting_test.go
+++ 253_meeting_rooms_ii/meeting_test.go
@@ -51,3 +51,53 @@ func TestMinRooms2(t *testing.T) {
 		})
 	}
 }
+
+func TestMinRooms3(t *testing.T) {
+	st := []struct {
+		name      string
+		intervals []interval
+		exp       int
+	}{
+		{"empty intervals", []interval{}, 0},
+		{"single", []interval{interval{1, 2}}, 1},
+		{"no overlapping", []interval{interval{1, 2}, interval{3, 4}}, 1},
+		{"with overlapping", []interval{interval{0, 20}, interval{5, 10}, interval{6, 8}}, 3},
+		{"failed 1", []interval{interval{1, 5}, interval{8, 9}, interval{8, 9}}, 2},
+		{"failed 2", []interval{interval{4, 19}, interval{6, 13}, interval{11, 20}, interval{13, 17}}, 3},
+		{"failed 3", []interval{interval{2, 36}, interval{3, 4}, interval{13, 34}, interval{16, 20}, interval{39, 40}}, 3},
+	}
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			ret := MinRooms3(c.intervals)
+			if ret != c.exp {
+				t.Fatalf("expected %d but got %d, with input %v",
+					c.exp, ret, c.intervals)
+			}
+		})
+	}
+}
+
+func TestMinRooms4(t *testing.T) {
+	st := []struct {
+		name      string
+		intervals []interval
+		exp       int
+	}{
+		{"empty intervals", []interval{}, 0},
+		{"single", []interval{interval{1, 2}}, 1},
+		{"no overlapping", []interval{interval{1, 2}, interval{3, 4}}, 1},
+		{"with overlapping", []interval{interval{0, 20}, interval{5, 10}, interval{6, 8}}, 3},
+		{"failed 1", []interval{interval{1, 5}, interval{8, 9}, interval{8, 9}}, 2},
+		{"failed 2", []interval{interval{4, 19}, interval{6, 13}, interval{11, 20}, interval{13, 17}}, 3},
+		{"failed 3", []interval{interval{2, 36}, interval{3, 4}, interval{13, 34}, interval{16, 20}, interval{39, 40}}, 3},
+	}
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			ret := MinRooms4(c.intervals)
+			if ret != c.exp {
+				t.Fatalf("expected %d but got %d, with input %v",
+					c.exp, ret, c.intervals)
+			}
+		})
+	}
+}
