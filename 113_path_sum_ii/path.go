4293e5aac58c63d7d25d2d2257dd8e945ba21064
Catorpilor <jiaojiangonghitster@gmail.com>
2017-12-21 21:04:36 +0800

    path sum ii

---
+++ 113_path_sum_ii/path.go
@@ -0,0 +1,34 @@
+package path
+
+import (
+	"github.com/catorpilor/leetcode/utils"
+)
+
+func PathSum(root *utils.TreeNode, sum int) [][]int {
+	var ret [][]int
+	if root == nil {
+		return ret
+	}
+	var temp []int
+	helper(root, sum, &temp, &ret)
+	return ret
+}
+
+func helper(node *utils.TreeNode, k int, nodes *[]int, res *[][]int) {
+	n := len(*nodes)
+	*nodes = append(*nodes, node.Val)
+	if node.Left == nil && node.Right == nil && k == node.Val {
+		// find one
+		bak := make([]int, len(*nodes))
+		copy(bak, *nodes)
+		*res = append(*res, bak)
+	} else {
+		if node.Left != nil {
+			helper(node.Left, k-node.Val, nodes, res)
+		}
+		if node.Right != nil {
+			helper(node.Right, k-node.Val, nodes, res)
+		}
+	}
+	*nodes = (*nodes)[:n]
+}
