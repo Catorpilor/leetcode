ab63c545cda73c47567085a68bd0854cf7942456
Catorpilor <jiaojiangonghitster@gmail.com>
2017-12-28 23:02:31 +0800

    number of islands

---
+++ 200_number_of_islands/islands.go
@@ -0,0 +1,90 @@
+package islands
+
+import (
+	"github.com/catorpilor/leetcode/utils"
+)
+
+func NumOfIslands(grid [][]byte) int {
+	row := len(grid)
+	if row == 0 {
+		return 0
+	}
+	col := len(grid[0])
+	if col == 0 {
+		return 0
+	}
+	var ret int
+	// O(M*N) time space
+	// Space: Worst case O(M*N)
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if grid[i][j] == '1' {
+				// root of tree
+				ret++
+				dfs(&grid, i, j, row, col)
+			}
+		}
+	}
+	return ret
+}
+
+func dfs(grid *[][]byte, curRow, curCol, rows, cols int) {
+	// set grid[curRow][curCol] to '0' marked visited
+	(*grid)[curRow][curCol] = '0'
+	if curRow-1 >= 0 && (*grid)[curRow-1][curCol] == '1' {
+		dfs(grid, curRow-1, curCol, rows, cols)
+	}
+	if curRow+1 < rows && (*grid)[curRow+1][curCol] == '1' {
+		dfs(grid, curRow+1, curCol, rows, cols)
+	}
+	if curCol+1 < cols && (*grid)[curRow][curCol+1] == '1' {
+		dfs(grid, curRow, curCol+1, rows, cols)
+	}
+	if curCol-1 >= 0 && (*grid)[curRow][curCol-1] == '1' {
+		dfs(grid, curRow, curCol-1, rows, cols)
+	}
+}
+
+func NumOfIslands2(grid [][]byte) int {
+	rows := len(grid)
+	if rows == 0 {
+		return 0
+	}
+	cols := len(grid[0])
+	if cols == 0 {
+		return 0
+	}
+	// bfs
+	var ret int
+	type pair struct{ r, c int }
+	q := utils.NewQueue()
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if grid[i][j] == '1' {
+				ret++
+				grid[i][j] = '0' // mark visited
+				q.Enroll(pair{i, j})
+				for !q.IsEmpty() {
+					p := q.Pull().(pair)
+					if p.r-1 >= 0 && grid[p.r-1][p.c] == '1' {
+						q.Enroll(pair{p.r - 1, p.c})
+						grid[p.r-1][p.c] = '0'
+					}
+					if p.r+1 < rows && grid[p.r+1][p.c] == '1' {
+						q.Enroll(pair{p.r + 1, p.c})
+						grid[p.r+1][p.c] = '0'
+					}
+					if p.c-1 >= 0 && grid[p.r][p.c-1] == '1' {
+						q.Enroll(pair{p.r, p.c - 1})
+						grid[p.r][p.c-1] = '0'
+					}
+					if p.c+1 < cols && grid[p.r][p.c+1] == '1' {
+						q.Enroll(pair{p.r, p.c + 1})
+						grid[p.r][p.c+1] = '0'
+					}
+				}
+			}
+		}
+	}
+	return ret
+}
---
+++ 200_number_of_islands/islands_test.go
@@ -0,0 +1,51 @@
+package islands
+
+import "testing"
+
+func TestNumOfIslands(t *testing.T) {
+	st := []struct {
+		name string
+		grid [][]byte
+		exp  int
+	}{
+		{"empty", [][]byte{}, 0},
+		{"single", [][]byte{[]byte{'1'}}, 1},
+		{"single0", [][]byte{[]byte{'0'}}, 0},
+		{"test1", [][]byte{[]byte{'1', '1', '1', '1', '0'},
+			[]byte{'1', '1', '0', '1', '0'}, []byte{'1', '1', '0', '0', '0'},
+			[]byte{'0', '0', '0', '0', '1'}}, 2},
+	}
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			ret := NumOfIslands(c.grid)
+			if ret != c.exp {
+				t.Fatalf("expected %d but got %d with input %v",
+					c.exp, ret, c.grid)
+			}
+		})
+	}
+}
+
+func TestNumOfIslands2(t *testing.T) {
+	st := []struct {
+		name string
+		grid [][]byte
+		exp  int
+	}{
+		{"empty", [][]byte{}, 0},
+		{"single", [][]byte{[]byte{'1'}}, 1},
+		{"single0", [][]byte{[]byte{'0'}}, 0},
+		{"test1", [][]byte{[]byte{'1', '1', '1', '1', '0'},
+			[]byte{'1', '1', '0', '1', '0'}, []byte{'1', '1', '0', '0', '0'},
+			[]byte{'0', '0', '0', '0', '1'}}, 2},
+	}
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			ret := NumOfIslands2(c.grid)
+			if ret != c.exp {
+				t.Fatalf("expected %d but got %d with input %v",
+					c.exp, ret, c.grid)
+			}
+		})
+	}
+}
