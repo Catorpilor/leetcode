4b179d79171b07231ca1191801768c8593c5e646
Catorpilor <jiaojiangonghitster@gmail.com>
2017-12-30 17:18:20 +0800

    union find to solve number of islands

--- 200_number_of_islands/islands.go
+++ 200_number_of_islands/islands.go
@@ -88,3 +88,71 @@ func NumOfIslands2(grid [][]byte) int {
 	}
 	return ret
 }
+
+func NumOfIslands3(grid [][]byte) int {
+	// union-find
+	rows := len(grid)
+	if rows == 0 {
+		return 0
+	}
+	cols := len(grid[0])
+	if cols == 0 {
+		return 0
+	}
+	var count int
+	ids := make([]int, rows*cols)
+	sz := make([]int, rows*cols)
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if grid[i][j] == '1' {
+				ids[i*cols+j] = i*cols + j
+				sz[i*cols+j] = 1
+				count++
+			}
+		}
+	}
+
+	root := func(p int) int {
+		for p != ids[p] {
+			ids[p] = ids[ids[p]]
+			p = ids[p]
+		}
+		return p
+	}
+	union := func(p, q int) {
+		i, j := root(p), root(q)
+		if i == j {
+			return
+		}
+		if sz[i] < sz[j] {
+			ids[i] = j
+			sz[j] += sz[i]
+		} else {
+			ids[j] = i
+			sz[i] += sz[j]
+		}
+		count--
+	}
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if grid[i][j] == '1' {
+				grid[i][j] = '0' // mark visited
+				if i > 0 && grid[i-1][j] == '1' {
+					union(i*cols+j, (i-1)*cols+j)
+				}
+				if i+1 < rows && grid[i+1][j] == '1' {
+					union(i*cols+j, (i+1)*cols+j)
+				}
+				if j > 0 && grid[i][j-1] == '1' {
+					union(i*cols+j, i*cols+j-1)
+				}
+				if j+1 < cols && grid[i][j+1] == '1' {
+					union(i*cols+j, i*cols+j+1)
+				}
+			}
+		}
+	}
+
+	return count
+}
--- 200_number_of_islands/islands_test.go
+++ 200_number_of_islands/islands_test.go
@@ -49,3 +49,27 @@ func TestNumOfIslands2(t *testing.T) {
 		})
 	}
 }
+
+func TestNumOfIslands3(t *testing.T) {
+	st := []struct {
+		name string
+		grid [][]byte
+		exp  int
+	}{
+		{"empty", [][]byte{}, 0},
+		{"single", [][]byte{[]byte{'1'}}, 1},
+		{"single0", [][]byte{[]byte{'0'}}, 0},
+		{"test1", [][]byte{[]byte{'1', '1', '1', '1', '0'},
+			[]byte{'1', '1', '0', '1', '0'}, []byte{'1', '1', '0', '0', '0'},
+			[]byte{'0', '0', '0', '0', '1'}}, 2},
+	}
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			ret := NumOfIslands3(c.grid)
+			if ret != c.exp {
+				t.Fatalf("expected %d but got %d with input %v",
+					c.exp, ret, c.grid)
+			}
+		})
+	}
+}
