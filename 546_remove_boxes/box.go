77614ac59542d4352464e22dfabfad57f00e796f
Catorpilor <jiaojiangonghitster@gmail.com>
2018-01-10 22:20:14 +0800

    modification

--- 546_remove_boxes/box.go
+++ 546_remove_boxes/box.go
@@ -9,11 +9,12 @@ func RemoveBoxes(boxes []int) int {
 	// ans[l][r][k] represents the largest number we can get from boxes[l:r] with k same colored boxes.
 	// for example ans[l][r][3] represents the solution for [b_l, ... b_r, b_r, b_r, b_r] (format ABDAA) => (BDAAA)
 	// The transition function is to find the maximum among all b_i==b_r for i=l,...,r-1:
-	ans := [100][100][100]int{}
-	return dfs(boxes, &ans, 0, n-1, 0)
+	ans := [101][101][101]int{}
+	// k >= 1
+	return dfs(boxes, &ans, 0, n-1, 1)
 }
 
-func dfs(boxes []int, ans *[100][100][100]int, l, r, k int) int {
+func dfs(boxes []int, ans *[101][101][101]int, l, r, k int) int {
 	if l > r {
 		return 0
 	}
@@ -27,10 +28,10 @@ func dfs(boxes []int, ans *[100][100][100]int, l, r, k int) int {
 	for l < r && boxes[l] == boxes[r] {
 		l, k = l+1, k+1
 	}
-	(*ans)[l][r][k] = dfs(boxes, ans, l, r-1, 0) + (k+1)*(k+1)
+	(*ans)[l][r][k] = dfs(boxes, ans, l, r-1, 1) + k*k
 	for i := l; i < r; i++ {
 		if boxes[i] == boxes[r] {
-			(*ans)[l][r][k] = utils.Max((*ans)[l][r][k], dfs(boxes, ans, l, i, k+1)+dfs(boxes, ans, i+1, r-1, 0))
+			(*ans)[l][r][k] = utils.Max((*ans)[l][r][k], dfs(boxes, ans, l, i, k+1)+dfs(boxes, ans, i+1, r-1, 1))
 		}
 	}
 	return (*ans)[l][r][k]
