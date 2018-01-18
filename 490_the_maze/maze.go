639f8a11d2b9486b0ccadee3c1af7cea8cf52247
Catorpilor <jiaojiangonghitster@gmail.com>
2018-01-18 21:42:14 +0800

    bfs version

--- 490_the_maze/maze.go
+++ 490_the_maze/maze.go
@@ -1,5 +1,9 @@
 package maze
 
+import (
+	"github.com/catorpilor/leetcode/utils"
+)
+
 func HasPath(maze [][]int, start, dest []int) bool {
 	m := len(maze)
 	if m == 0 {
@@ -14,16 +18,16 @@ func HasPath(maze [][]int, start, dest []int) bool {
 		visited[i] = make([]bool, n)
 	}
 	dirs := [4][2]int{[2]int{0, 1}, [2]int{0, -1}, [2]int{1, 0}, [2]int{-1, 0}}
-	return dfs(maze, start, dest, &visited, dirs)
+	return dfs2(maze, start, dest, &visited, dirs)
 
 }
 
 func dfs(maze [][]int, start, dest []int, visited *[][]bool, dirs [4][2]int) bool {
 	x, y := start[0], start[1]
-	if (*visited)[start[0]][start[1]] {
+	if (*visited)[x][y] {
 		return false
 	}
-	if start[0] == dest[0] && start[1] == dest[1] {
+	if x == dest[0] && y == dest[1] {
 		return true
 	}
 	(*visited)[x][y] = true
@@ -34,6 +38,27 @@ func dfs(maze [][]int, start, dest []int, visited *[][]bool, dirs [4][2]int) boo
 		// tempx, tempy = x+c[0], y+c[1]
 		newStart := roll(maze, x, y, c[0], c[1])
 		if dfs(maze, newStart, dest, visited, dirs) {
+			return true
+		}
+	}
+	return false
+}
+
+func dfs2(maze [][]int, start, dest []int, visited *[][]bool, dirs [4][2]int) bool {
+	if (*visited)[start[0]][start[1]] {
+		return false
+	}
+	if start[0] == dest[0] && start[1] == dest[1] {
+		return true
+	}
+	(*visited)[start[0]][start[1]] = true
+	var x, y int
+	for _, c := range dirs {
+		x, y = start[0], start[1]
+		if x+c[0] < len(maze) && x+c[0] >= 0 && y+c[1] < len(maze[0]) && y+c[1] >= 0 && maze[x+c[0]][y+c[1]] == 0 {
+			x, y = x+c[0], y+c[1]
+		}
+		if dfs2(maze, []int{x, y}, dest, visited, dirs) {
 			return true
 		}
 	}
@@ -54,4 +79,46 @@ func roll(maze [][]int, x, y, rowInc, colInc int) []int {
 		x, y = x+rowInc, y+colInc
 	}
 	return []int{x, y}
+}
+
+func HasPath2(maze [][]int, start, dest []int) bool {
+	m := len(maze)
+	if m == 0 {
+		return false
+	}
+	n := len(maze[0])
+	if n == 0 {
+		return false
+	}
+	q := utils.NewQueue()
+	visited := make([][]bool, m)
+	for i := range visited {
+		visited[i] = make([]bool, n)
+	}
+	q.Enroll(start)
+	visited[start[0]][start[1]] = true
+	dirs := [4][2]int{[2]int{1, 0}, [2]int{-1, 0}, [2]int{0, 1}, [2]int{0, -1}}
+	var x, y int
+	for !q.IsEmpty() {
+		cur := q.Pull().([]int)
+		for _, c := range dirs {
+			x, y = cur[0], cur[1]
+			for x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == 0 {
+				x, y = x+c[0], y+c[1]
+			}
+			x, y = x-c[0], y-c[1]
+			if visited[x][y] {
+				continue
+			}
+			// mark as visited
+			visited[x][y] = true
+			// desision point
+			if x == dest[0] && y == dest[1] {
+				return true
+			}
+			// put in queue
+			q.Enroll([]int{x, y})
+		}
+	}
+	return false
 }
--- 490_the_maze/maze_test.go
+++ 490_the_maze/maze_test.go
@@ -25,3 +25,27 @@ func TestHasPath(t *testing.T) {
 		})
 	}
 }
+
+func TestHasPath2(t *testing.T) {
+	st := []struct {
+		name        string
+		maze        [][]int
+		start, dest []int
+		exp         bool
+	}{
+		{"test1", [][]int{[]int{0, 0, 1, 0, 0}, []int{0, 0, 0, 0, 0}, []int{0, 0, 0, 1, 0},
+			[]int{1, 1, 0, 1, 1}, []int{0, 0, 0, 0, 0}}, []int{0, 4}, []int{4, 4}, true},
+		{"test2", [][]int{[]int{0, 0, 1, 0, 0}, []int{0, 0, 0, 0, 0}, []int{0, 0, 0, 1, 0},
+			[]int{1, 1, 0, 1, 1}, []int{0, 0, 0, 0, 0}}, []int{0, 4}, []int{3, 2}, false},
+		{"test3", [][]int{[]int{0, 0}}, []int{0, 0}, []int{0, 1}, true},
+	}
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			ret := HasPath2(c.maze, c.start, c.dest)
+			if ret != c.exp {
+				t.Fatalf("expected %t but got %t with input %v and %v and %v",
+					c.exp, ret, c.maze, c.start, c.dest)
+			}
+		})
+	}
+}
