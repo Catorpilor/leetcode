491b107f770363dec8d2fd0431ed2d89736c4b84
Catorpilor <jiaojiangonghitster@gmail.com>
2018-01-22 22:57:36 +0800

    dfs version

---
+++ 207_course_schedule/course.go
@@ -0,0 +1,39 @@
+package course
+
+func CanFinish(numCourses int, pres [][]int) bool {
+	n := len(pres)
+	if n == 0 {
+		return true
+	}
+	visited, onpath := make([]bool, numCourses), make([]bool, numCourses)
+	g := make(map[int][]int)
+	// fill g with pres
+	for _, v := range pres {
+		g[v[1]] = append(g[v[1]], v[0])
+	}
+	for i := 0; i < numCourses; i++ {
+		if visited[i] {
+			continue
+		}
+		if dfsCycle(g, &visited, &onpath, i) {
+			return false
+		}
+	}
+	return true
+}
+
+func dfsCycle(g map[int][]int, visited, onpath *[]bool, node int) bool {
+	if (*visited)[node] {
+		return false
+	}
+	// mark node as visited
+	(*visited)[node], (*onpath)[node] = true, true
+
+	for _, adj := range g[node] {
+		if (*onpath)[adj] || dfsCycle(g, visited, onpath, adj) {
+			return true
+		}
+	}
+	(*onpath)[node] = false
+	return false
+}
