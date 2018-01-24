67af5fe289cd9776beefeb489b7e8e8bb9721cd4
Catorpilor <jiaojiangonghitster@gmail.com>
2018-01-24 22:27:27 +0800

    bfs version

--- 207_course_schedule/course.go
+++ 207_course_schedule/course.go
@@ -39,3 +39,38 @@ func dfsCycle(g map[int][]int, visited, onpath *[]bool, node int) bool {
 	(*onpath)[node] = false
 	return false
 }
+
+func CanFinish2(numCourses int, pres [][]int) bool {
+	n := len(pres)
+	if n == 0 {
+		return true
+	}
+	g := make(map[int][]int)
+	for _, v := range pres {
+		g[v[1]] = append(g[v[1]], v[0])
+	}
+	// count indegress of each nodes
+	degs := make([]int, numCourses)
+	for i := 0; i < numCourses; i++ {
+		for _, v := range g[i] {
+			degs[v]++
+		}
+	}
+	var j int
+	for i := 0; i < numCourses; i++ {
+		j = 0
+		for ; j < numCourses; j++ {
+			if degs[j] == 0 {
+				break
+			}
+		}
+		if j == numCourses {
+			return false
+		}
+		degs[j] = -1
+		for _, v := range g[j] {
+			degs[v]--
+		}
+	}
+	return true
+}
