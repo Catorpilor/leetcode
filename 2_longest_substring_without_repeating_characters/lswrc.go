04b298f81f446cc04cb5cd04d81c223f0e08014f
Catorpilor <jiaojiangonghitster@gmail.com>
2019-01-24 10:14:08 +0800

    add brute force and optimized version

--- 2_longest_substring_without_repeating_characters/lswrc.go
+++ 2_longest_substring_without_repeating_characters/lswrc.go
@@ -25,3 +25,49 @@ func lengthOfLongestSubstring(s string) int {
 	}
 	return max
 }
+
+func bruteForceLength(s string) int {
+	n := len(s)
+	if n <= 1 {
+		return n
+	}
+	max := 0
+	for i := 0; i < n; i++ {
+		for j := i + 1; j <= n; j++ {
+			if allUnique(s, i, j) {
+				max = utils.Max(max, j-i)
+			}
+		}
+	}
+	return max
+}
+func allUnique(s string, l, r int) bool {
+	hset := make(map[byte]bool)
+	for i := l; i < r; i++ {
+		if hset[s[i]] {
+			return false
+		}
+		hset[s[i]] = true
+	}
+	return true
+}
+
+func lengthOfLongestSubstringOpt(s string) int {
+	n := len(s)
+	if n <= 1 {
+		return n
+	}
+	max := 0
+	hset := make(map[byte]int)
+	l, r := 0, 0
+	for r < n {
+		// expand [l,r]
+		if k, ok := hset[s[r]]; ok {
+			l = utils.Max(l, k)
+		}
+		max = utils.Max(max, r-l+1)
+		hset[s[r]] = r + 1
+		r++
+	}
+	return max
+}
--- 2_longest_substring_without_repeating_characters/lswrc_test.go
+++ 2_longest_substring_without_repeating_characters/lswrc_test.go
@@ -23,3 +23,47 @@ func TestLengthOfLongestSubstring(t *testing.T) {
 		})
 	}
 }
+
+func TestBruteForceLengthOfLongestSubstring(t *testing.T) {
+	st := []struct {
+		name, str string
+		exp       int
+	}{
+		{"empty string", "", 0},
+		{"single character", "a", 1},
+		{"duplicate charater", "aaaa", 1},
+		{"abcabcbb", "abcabcbb", 3},
+		{"pwwkew", "pwwkew", 3},
+	}
+	for _, s := range st {
+		t.Run(s.name, func(t *testing.T) {
+			ret := bruteForceLength(s.str)
+			if ret != s.exp {
+				t.Fatalf("expected %d but got %d with input string: %s",
+					s.exp, ret, s.str)
+			}
+		})
+	}
+}
+
+func TestLengthOfLongestSubstringOpt(t *testing.T) {
+	st := []struct {
+		name, str string
+		exp       int
+	}{
+		{"empty string", "", 0},
+		{"single character", "a", 1},
+		{"duplicate charater", "aaaa", 1},
+		{"abcabcbb", "abcabcbb", 3},
+		{"pwwkew", "pwwkew", 3},
+	}
+	for _, s := range st {
+		t.Run(s.name, func(t *testing.T) {
+			ret := lengthOfLongestSubstringOpt(s.str)
+			if ret != s.exp {
+				t.Fatalf("expected %d but got %d with input string: %s",
+					s.exp, ret, s.str)
+			}
+		})
+	}
+}
