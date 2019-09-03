0751d977f83e3494177b42deeadc8a88d74d9803
Catorpilor <jiaojiangonghitster@gmail.com>
2019-09-03 22:34:27 +0800

    sovled based on discussion

--- 979_distribute_coins_in_binary_tree/dis.go
+++ 979_distribute_coins_in_binary_tree/dis.go
@@ -4,5 +4,18 @@ import "github.com/catorpilor/LeetCode/utils"
 
 // DistributeCoins distribute coins
 func DistributeCoins(root *utils.TreeNode) int {
-	return 0
+	var res int
+	dfs(root, &res)
+	return res
+}
+
+func dfs(root *utils.TreeNode, res *int) int {
+	if root == nil {
+		return 0
+	}
+	// l ,r represents the total coins needs to be transfered for
+	// root's substree
+	l, r := dfs(root.Left, res), dfs(root.Right, res)
+	*res += utils.Abs(l) + utils.Abs(r)
+	return root.Val + l + r - 1
 }
