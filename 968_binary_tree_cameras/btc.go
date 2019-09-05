b73cbe29b3bf43037a3785cc69943eeb5f477183
Catorpilor <jiaojiangonghitster@gmail.com>
2019-09-05 23:05:23 +0800

    solved based on lee215's contest

---
+++ 968_binary_tree_cameras/btc.go
@@ -0,0 +1,54 @@
+package btc
+
+import "github.com/catorpilor/leetcode/utils"
+
+func MinCameras(root *utils.TreeNode) int {
+	var res int
+	state := dfs(root, &res)
+	if state < 1 {
+		// need camera
+		res++
+	}
+	return res
+}
+
+// dfs depth-first-search the tree
+// use greedy algorithem
+// put no cameras on leaves
+// return values
+// 0 means child need to be covered.
+// 1 means node with camera, covered.
+// 2 means parent node already covered by node.
+
+func dfs(root *utils.TreeNode, res *int) int {
+	var neededCamera, covered int
+	if root.Left == nil && root.Right == nil {
+		return 0
+	}
+	if root.Left != nil {
+		state := dfs(root.Left, res)
+		if state == 0 {
+			neededCamera = 1
+			covered = 1
+		} else if state == 1 {
+			covered = 1
+		}
+	}
+	if root.Right != nil {
+		state := dfs(root.Right, res)
+		if state == 0 {
+			neededCamera = 1
+			covered = 1
+		} else if state == 1 {
+			covered = 1
+		}
+	}
+	if neededCamera > 0 {
+		*res++
+		return 1
+	}
+	if covered > 0 {
+		return 2
+	}
+	return 0
+}
---
+++ 968_binary_tree_cameras/btc_test.go
@@ -0,0 +1,32 @@
+package btc
+
+import (
+	"math"
+	"testing"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
+func TestMinCameras(t *testing.T) {
+	st := []struct {
+		name  string
+		nodes []int
+		root  *utils.TreeNode
+		exp   int
+	}{
+		// {"empty tree", []int{}, nil, 0},
+		{"single node", []int{1}, utils.ConstructTree([]int{1}), 1},
+		{"testcase1", []int{0, 0, math.MinInt32, 0, 0}, utils.ConstructTree([]int{0, 0, math.MinInt32, 0, 0}), 1},
+		{"testcase2", []int{0, 0, math.MinInt32, 0, math.MinInt32, 0, math.MinInt32, math.MinInt32, 0}, utils.ConstructTree([]int{0, 0, math.MinInt32, 0, math.MinInt32, 0, math.MinInt32, math.MinInt32, 0}), 2},
+	}
+
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := MinCameras(tt.root)
+			if out != tt.exp {
+				t.Fatalf("with input node slice: %v wanted %d but got %d", tt.nodes, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
