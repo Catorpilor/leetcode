552a564e9f0fc735aa99dd7297bbe2a1ccf90d38
Catorpilor <jiaojiangonghitster@gmail.com>
2019-09-18 23:11:42 +0800

    solve 1019 based on dp

---
+++ 1019_next_greater_node_in_linked_list/ngl.go
@@ -0,0 +1,52 @@
+package ngl
+
+import (
+	"github.com/catorpilor/LeetCode/utils"
+)
+
+func nextLargeNode(head *utils.ListNode) []int {
+	if head == nil {
+		return []int{}
+	}
+	st := utils.NewStack()
+	h := head
+	for h != nil {
+		st.Push(h.Val)
+		h = h.Next
+	}
+	n := st.Len()
+	reversd := make([]int, 0, n)
+	for !st.IsEmpty() {
+		reversd = append(reversd, st.Pop().(int))
+	}
+	res := make([]int, n)
+	dp := make([]int, n)
+	dp[0] = -1
+	for i := 1; i < n; i++ {
+		if reversd[i] < reversd[i-1] {
+			dp[i] = i - 1
+		} else if reversd[i] == reversd[i-1] {
+			dp[i] = dp[i-1]
+		} else {
+			j := dp[i-1]
+			for j != -1 {
+				if reversd[j] > reversd[i] {
+					dp[i] = j
+					break
+				} else {
+					j = dp[j]
+				}
+			}
+			if j == -1 && dp[i] == 0 {
+				dp[i] = j
+			}
+		}
+	}
+	for i := n - 1; i >= 0; i-- {
+		if dp[i] != -1 {
+			res[n-1-i] = reversd[dp[i]]
+		}
+	}
+
+	return res
+}
---
+++ 1019_next_greater_node_in_linked_list/ngl_test.go
@@ -0,0 +1,34 @@
+package ngl
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/catorpilor/LeetCode/utils"
+)
+
+func TestNextLargeNode(t *testing.T) {
+	st := []struct {
+		name string
+		head *utils.ListNode
+		exp  []int
+	}{
+		{"nil head", nil, []int{}},
+		{"single node", utils.ConstructFromSlice([]int{1}), []int{0}},
+		{"identical", utils.ConstructFromSlice([]int{1, 1, 1}), []int{0, 0, 0}},
+		{"testcase1", utils.ConstructFromSlice([]int{2, 1, 5}), []int{5, 5, 0}},
+		{"testcase2", utils.ConstructFromSlice([]int{2, 7, 4, 3, 5}), []int{7, 0, 5, 5, 0}},
+		{"testcase3", utils.ConstructFromSlice([]int{1, 7, 5, 1, 9, 2, 5, 1}), []int{7, 9, 9, 9, 0, 5, 0, 0}},
+		{"failed1", utils.ConstructFromSlice([]int{4, 3, 2, 5, 1, 8, 10}), []int{5, 5, 5, 8, 8, 10, 0}},
+		{"failed2", utils.ConstructFromSlice([]int{9, 7, 6, 7, 6, 9}), []int{0, 9, 7, 9, 9, 0}},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := nextLargeNode(tt.head)
+			if !reflect.DeepEqual(out, tt.exp) {
+				t.Fatalf("with input list: %s, wanted %v but got %v", tt.head.String(), tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
