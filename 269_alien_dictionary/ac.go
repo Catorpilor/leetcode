a9c3374323115c288e7396f8ed5c287378f90e64
Catorpilor <jiaojiangonghitster@gmail.com>
2019-09-27 21:08:53 +0800

    partially done

--- 269_alien_dictionary/ac.go
+++ 269_alien_dictionary/ac.go
@@ -1,5 +1,58 @@
 package ac
 
+import "fmt"
+
 func alienOrder(words []string) string {
-    return ""
+	var ans string
+	n := len(words)
+	indegress := make(map[byte]int)
+	g := make(map[byte]map[byte]bool)
+	// init indegrees
+	for i := 0; i < n; i++ {
+		for j := 0; j < len(words[i]); j++ {
+			indegress[words[i][j]] = 0
+		}
+	}
+	// build graph and update indegress
+	for i := 0; i < n-1; i++ {
+		curW, nxtW := words[i], words[i+1]
+		sn := len(curW)
+		if sn > len(nxtW) {
+			sn = len(nxtW)
+		}
+		fmt.Printf("curW: %s, nextW: %s, sn: %d\n", curW, nxtW, sn)
+		for j := 0; j < sn; j++ {
+			if curW[j] != nxtW[j] {
+				fmt.Printf("find one: left: %s, right: %s, j: %d\n", string(curW[j]), string(nxtW[j]), j)
+				nodes := g[curW[j]]
+				if nodes == nil {
+					nodes = make(map[byte]bool)
+				}
+				nodes[nxtW[j]] = true
+				g[curW[j]] = nodes
+				indegress[nxtW[j]]++
+				break
+			}
+		}
+	}
+	fmt.Printf("graph is %v\n", g)
+	for k := range indegress {
+		if indegress[k] == 0 {
+			ans += string(k)
+		}
+	}
+	fmt.Printf("current ans: %s\n", ans)
+	for i := 0; i < len(ans); i++ {
+		nodes := g[ans[i]]
+		for k := range nodes {
+			indegress[k]--
+			if indegress[k] == 0 {
+				ans += string(k)
+			}
+		}
+	}
+	if len(ans) != len(indegress) {
+		return ""
+	}
+	return ans
 }
--- 269_alien_dictionary/ac_test.go
+++ 269_alien_dictionary/ac_test.go
@@ -3,23 +3,24 @@ package ac
 import "testing"
 
 func TestAlienOrder(t *testing.T) {
-    st := []struct {
-        name  string
-        words []string
-        exp   string
-    }{
-        {"empty words", []string{}, ""},
-        {"test1", []string{"wrt", "wrf", "er", "ett", "rftt"}, "wertf"},
-        {"test2", []string{"z", "x"}, "zx"},
-        {"test3", []string{"z", "x", "z"}, ""},
-    }
-    for _, tt := range st {
-        t.Run(tt.name, func(t *testing.T) {
-            out := alienOrder(tt.words)
-            if out != tt.exp {
-                t.Fatalf("with input words: %v wanted %s but got %s", tt.words, tt.exp, out)
-            }
-            t.Log("pass")
-        })
-    }
+	st := []struct {
+		name  string
+		words []string
+		exp   string
+	}{
+		{"empty words", []string{}, ""},
+		{"test1", []string{"wrt", "wrf", "er", "ett", "rftt"}, "wertf"},
+		{"test2", []string{"z", "x"}, "zx"},
+		{"test3", []string{"z", "x", "z"}, ""},
+		{"failed1", []string{"za", "zb", "ca", "cb"}, "abzc"},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := alienOrder(tt.words)
+			if out != tt.exp {
+				t.Fatalf("with input words: %v wanted %s but got %s", tt.words, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
 }
