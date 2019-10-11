b94d7bbef1f697ace76fdd5dc728ff6676d1e67a
Catorpilor <jiaojiangonghitster@gmail.com>
2019-10-11 19:36:16 +0800

    solve 230 use inorder traversal

--- 230_kth_smallest_element_in_a_bst/kthbst.go
+++ 230_kth_smallest_element_in_a_bst/kthbst.go
@@ -3,5 +3,9 @@ package kthbst
 import "github.com/catorpilor/leetcode/utils"
 
 func kthSmallest(root *utils.TreeNode, k int) int {
-    return 0
+	ret := utils.InorderTraversal(root)
+	if len(ret) < k || k < 1 {
+		return 0
+	}
+	return ret[k-1]
 }
