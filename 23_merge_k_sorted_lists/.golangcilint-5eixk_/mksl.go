a5e25e8a0320649ade36e84e3416407602892c05
Catorpilor <jiaojiangonghitster@gmail.com>
2019-10-16 21:04:54 +0800

    solve 23 by comparing one by one

---
+++ 23_merge_k_sorted_lists/.golangcilint-5eixk_/mksl.go
@@ -0,0 +1,29 @@
+package mksl
+
+import (
+    "sort"
+
+    "github.com/catorpilor/leetcode/utils"
+)
+
+func mergeKLists(lists []*utils.ListNode) *utils.ListNode {
+    return bruteForce(lists)
+}
+
+func bruteForce(lists []*utils.ListNode) *utils.ListNode {
+    nodes := make([]int, 0, 10000)
+    for _, l := range lists {
+        for l != nil {
+            nodes = append(nodes, l.Val)
+            l = l.Next
+        }
+    }
+    if len(nodes) > 1 {
+        sort.Slice(nodes, func(i, j int) bool {
+            return nodes[i] <= nodes[j]
+        })
+    }
+    return utils.ConstructFromSlice(nodes)
+}
+
+func compare(lists []*utils)
--- 23_merge_k_sorted_lists/mksl.go
+++ 23_merge_k_sorted_lists/mksl.go
@@ -1,13 +1,15 @@
 package mksl
 
 import (
+    "math"
     "sort"
 
     "github.com/catorpilor/leetcode/utils"
 )
 
 func mergeKLists(lists []*utils.ListNode) *utils.ListNode {
-    return bruteForce(lists)
+    // return bruteForce(lists)
+    return compare(lists)
 }
 
 func bruteForce(lists []*utils.ListNode) *utils.ListNode {
@@ -24,4 +26,30 @@ func bruteForce(lists []*utils.ListNode) *utils.ListNode {
         })
     }
     return utils.ConstructFromSlice(nodes)
+}
+
+func compare(lists []*utils.ListNode) *utils.ListNode {
+    k := len(lists)
+    pointers := make([]*utils.ListNode, k)
+    for i := range pointers {
+        pointers[i] = lists[i]
+    }
+    sortedNodes := make([]int, 0, 1000000)
+    for {
+        sm, idx := math.MaxInt32, -1
+        for i := range pointers {
+            if pointers[i] != nil && pointers[i].Val < sm {
+                sm = pointers[i].Val
+                idx = i
+            }
+        }
+        if idx == -1 {
+            // we are done
+            break
+        }
+        sortedNodes = append(sortedNodes, pointers[idx].Val)
+        // update pointers[idx]
+        pointers[idx] = pointers[idx].Next
+    }
+    return utils.ConstructFromSlice(sortedNodes)
 }
