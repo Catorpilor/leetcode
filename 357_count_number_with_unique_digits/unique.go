2941010bcd029efbb3ef0309558742556e4732bf
catorpilor <jiaojiangonghitster@gmail.com>
2019-11-02 23:39:21 +0800

    solve 357 based on disscussion

--- 357_count_number_with_unique_digits/unique.go
+++ 357_count_number_with_unique_digits/unique.go
@@ -1,5 +1,29 @@
 package unique
 
 func countUniqueNumbers(n int) int {
-	return 0
+	return dp(n)
+}
+
+// based on discussion
+// f(1) = 10  // (0..9) unique numbers
+// f(2) = 9*9 // choose i from (1..9) choose j from (0..9) except i
+// f(3) = f(2) * 8 // the third postion just have 8 choices
+// f(4) = f(3) * 7
+// ...
+// f(10) = f(9) * 1
+// f(11) = f(12) = 0
+func dp(n int) int {
+	if n == 0 {
+		return 1
+	}
+	res := 10
+	uniqueDigits := 9
+	avaliableNumber := 9
+	for n > 1 && avaliableNumber > 0 {
+		uniqueDigits *= avaliableNumber
+		res += uniqueDigits
+		avaliableNumber--
+		n--
+	}
+	return res
 }
