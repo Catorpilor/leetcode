9a9e4c09993dc938c9facda720441901fe1ca3b1
Catorpilor <jiaojiangonghitster@gmail.com>
2019-11-10 21:45:53 +0800

    optimize with memorization

--- 526_beautiful_arrangement/ba.go
+++ 526_beautiful_arrangement/ba.go
@@ -20,3 +20,32 @@ func permute(res *int, pos int, visited *[]bool, n int) {
 		}
 	}
 }
+
+func withMemorization(n int) int {
+	curState := make([]byte, n+1)
+	for i := range curState {
+		curState[i] = 'f'
+	}
+	cache := make(map[string]int)
+	return dfs(cache, &curState, 1)
+}
+
+func dfs(cache map[string]int, curState *[]byte, idx int) int {
+	if idx == len(*curState) {
+		return 1
+	}
+	ck := string(*curState)
+	if v, exists := cache[ck]; exists {
+		return v
+	}
+	var count int
+	for i := 1; i < len(*curState); i++ {
+		if (*curState)[i] == 'f' && (i%idx == 0 || idx%i == 0) {
+			(*curState)[i] = 't'
+			count += dfs(cache, curState, idx+1)
+			(*curState)[i] = 'f'
+		}
+	}
+	cache[ck] = count
+	return count
+}
--- 526_beautiful_arrangement/ba_test.go
+++ 526_beautiful_arrangement/ba_test.go
@@ -23,3 +23,24 @@ func TestBeautyArrangement(t *testing.T) {
 	}
 
 }
+func TestWithMemo(t *testing.T) {
+	st := []struct {
+		name string
+		n    int
+		exp  int
+	}{
+		{"N=1", 1, 1},
+		{"N=2", 2, 2},
+		{"N=3", 3, 3},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := withMemorization(tt.n)
+			if out != tt.exp {
+				t.Fatalf("with input n:%d wanted %d but got %d", tt.n, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+
+}
