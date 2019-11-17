36f3f3f73535921772903c5748c2b36a93878026
Catorpilor <jiaojiangonghitster@gmail.com>
2019-11-17 22:59:52 +0800

    implement ternary search trie

---
+++ utils/trie.go
@@ -0,0 +1,72 @@
+package utils
+
+import "fmt"
+
+// TSTNode represents the ternary search trie node
+type TSTNode struct {
+	value               interface{}
+	cb                  byte
+	left, middle, right *TSTNode
+}
+
+type TernarySearchTrie struct {
+	root *TSTNode
+}
+
+func NewTST() *TernarySearchTrie {
+	return &TernarySearchTrie{
+		root: &TSTNode{},
+	}
+}
+
+func (t *TernarySearchTrie) Put(key string, val interface{}) {
+	t.root = put(t.root, key, val, 0)
+}
+
+func put(node *TSTNode, key string, val interface{}, d int) *TSTNode {
+	c := key[d]
+	if node == nil {
+		node = &TSTNode{
+			cb: c,
+		}
+	}
+	if c < node.cb {
+		node.left = put(node.left, key, val, d)
+	} else if c > node.cb {
+		node.right = put(node.right, key, val, d)
+	} else if d < len(key)-1 {
+		node.middle = put(node.middle, key, val, d+1)
+	} else {
+		node.value = val
+	}
+	return node
+}
+
+func (t *TernarySearchTrie) Contains(key string) bool {
+	return t.Get(key) != nil
+}
+
+func (t *TernarySearchTrie) Get(key string) interface{} {
+	fmt.Printf("t: %v\n", t)
+	x := get(t.root, key, 0)
+	if x == nil {
+		return nil
+	}
+	return x.value
+}
+
+func get(node *TSTNode, key string, d int) *TSTNode {
+	if node == nil {
+		return nil
+	}
+	c := key[d]
+	if c < node.cb {
+		return get(node.left, key, d)
+	} else if c > node.cb {
+		return get(node.right, key, d)
+	} else if d < len(key)-1 {
+		return get(node.middle, key, d+1)
+	} else {
+		return node
+	}
+}
---
+++ utils/trie_test.go
@@ -0,0 +1,64 @@
+package utils
+
+import (
+	"testing"
+)
+
+var tst *TernarySearchTrie
+
+func setup() {
+	tst = NewTST()
+	ss := []string{"hello", "hell", "hi", "news", "newspaper"}
+	for _, s := range ss {
+		tst.Put(s, 1)
+	}
+}
+
+func TestGet(t *testing.T) {
+	st := []struct {
+		name string
+		key  string
+		exp  int
+	}{
+		{"get hello", "hello", 1},
+		{"get hell", "hell", 1},
+		{"get his", "his", 0},
+	}
+	setup()
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := tst.Get(tt.key)
+			var v int
+			var ok bool
+			if v, ok = out.(int); !ok {
+				if out != nil {
+					t.Fatalf("wanted int but got %v with type: %T", out, out)
+				}
+			}
+			if v != tt.exp {
+				t.Fatalf("with key: %s wanted %d but got %d", tt.key, tt.exp, v)
+			}
+		})
+	}
+}
+
+func TestContains(t *testing.T) {
+	st := []struct {
+		name string
+		key  string
+		exp  bool
+	}{
+		{"contains hello", "hello", true},
+		{"contains hell", "hell", true},
+		{"contains his", "his", false},
+	}
+	setup()
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := tst.Contains(tt.key)
+			if out != tt.exp {
+				t.Fatalf("with key: %s wanted %t but got %t", tt.key, tt.exp, out)
+			}
+		})
+	}
+}
