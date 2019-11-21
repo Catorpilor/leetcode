d38a1c6c1a09d000d6343ddee35091a43f7d0ff7
Catorpilor <jiaojiangonghitster@gmail.com>
2019-11-21 11:21:03 +0800

    not finished yet

--- 44_wildcard_matching/wm.go
+++ 44_wildcard_matching/wm.go
@@ -1,6 +1,6 @@
 package wm
 
-func isMatch(s, p string) bool {
+func isMatchTLE(s, p string) bool {
 	ns, np := len(s), len(p)
 	// empty case
 	if ns == 0 {
@@ -38,4 +38,37 @@ func allAsterisk(p string) bool {
 		}
 	}
 	return true
+}
+
+func isMatch(s, p string) bool {
+	var i, j int
+	ast_pos, pos := -1, 0
+	if len(p) < 1 {
+		return false
+	}
+
+	for i < len(s) {
+		if s[i] == p[j] || p[j] == '?' {
+			i++
+			j++
+			continue
+		}
+		if p[j] == '*' {
+			ast_pos = j
+			j++
+			pos = i
+			continue
+		}
+		if ast_pos != -1 {
+			j = ast_pos + 1
+			pos++
+			i = pos
+			continue
+		}
+		return false
+	}
+	for j < len(p) && p[j] == '*' {
+		j++
+	}
+	return j == len(p)
 }
