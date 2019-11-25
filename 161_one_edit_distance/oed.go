ad68d4d4315d1a716e43c8ad4662d886fdeeafaf
Catorpilor <jiaojiangonghitster@gmail.com>
2019-11-25 23:15:41 +0800

    fix: solve 161 using iteration

--- 161_one_edit_distance/oed.go
+++ 161_one_edit_distance/oed.go
@@ -30,3 +30,34 @@ func isOneEditDistance(s, t string) bool {
 	}
 	return dp[n] == 1
 }
+
+// onePass sovle using iteration
+// time complexity O(m)
+// space complexity O(1)
+func onePass(s, t string) bool {
+	m, n := len(s), len(t)
+	if m > n {
+		// always make len(s) <= len(t)
+		return onePass(t, s)
+	}
+	if n-m > 1 {
+		// no way
+		return false
+	}
+	for i := 0; i < m; i++ {
+		if s[i] != t[i] {
+			if m == n {
+				// len(s) == len(t) && s[[i] != t[i]
+				//  this can only be the difference
+				// so we have to check the remaining parts
+				return s[i+1:] == t[i+1:]
+			}
+			// len(s) != len(t) && s[i] != t[i]
+			// skip t[i] here and check s[i:] with t[i+1:]
+			return s[i:] == t[i+1:]
+		}
+	}
+	// if no difference found
+	// just make sure m+1 == n
+	return m+1 == n
+}
