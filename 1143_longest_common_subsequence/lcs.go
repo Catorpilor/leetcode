60cddb174b2caf5b88fb6d06e7c3588dd8deaf7d
Catorpilor <jiaojiangonghitster@gmail.com>
2019-11-26 21:00:50 +0800

    doc: add some comments

--- 1143_longest_common_subsequence/lcs.go
+++ 1143_longest_common_subsequence/lcs.go
@@ -2,6 +2,9 @@ package lcs
 
 import "github.com/catorpilor/LeetCode/utils"
 
+// longestCommonSeq using dp to solve the problem
+// time complexity is O(mn)
+// space compelxity is o(n)
 func longestCommonSeq(text1, text2 string) int {
 	m, n := len(text1), len(text2)
 	if text1 == text2 {
