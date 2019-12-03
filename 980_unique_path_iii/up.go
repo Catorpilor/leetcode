983ae4032d06b7af2c7a6643a2fd886219fe6504
Catorpilor <jiaojiangonghitster@gmail.com>
2019-12-03 14:57:25 +0800

    fix: solve 980 using dfs+backtrakcing

--- 980_unique_path_iii/up.go
+++ 980_unique_path_iii/up.go
@@ -1,10 +1,11 @@
 package up
 
-import "fmt"
+var (
+	sx, sy, ex, ey, cells int
+)
 
 func uniquePath(grid [][]int) int {
-	var res int
-	var sx, sy, ex, ey, cells int
+	cells = 1
 	for i := range grid {
 		for j := range grid[i] {
 			if grid[i][j] == 1 {
@@ -17,28 +18,28 @@ func uniquePath(grid [][]int) int {
 		}
 	}
 	// fmt.Printf("ex:%d, ey:%d, sx:%d, sy:%d, cells:%d\n", ex, ey, sx, sy, cells)
-	dfs(&res, grid, sx, sy, ex, ey, &cells)
+	var res int
+	dfs(&res, grid, sx, sy, cells)
 	return res
 }
 
-func dfs(res *int, grid [][]int, sx, sy, ex, ey int, empty *int) {
-	fmt.Printf("ex:%d, ey:%d, sx:%d, sy:%d, cells:%d\n", ex, ey, sx, sy, *empty)
+func dfs(res *int, grid [][]int, sx, sy, empty int) {
+	// fmt.Printf("ex:%d, ey:%d, sx:%d, sy:%d, cells:%d\n", ex, ey, sx, sy, empty)
 	if sx < 0 || sx >= len(grid) || sy < 0 || sy >= len(grid[0]) || grid[sx][sy] < 0 {
 		return
 	}
 	if sx == ex && sy == ey {
-		if (*empty) == 0 {
+		if empty == 0 {
 			(*res)++
 		}
 		return
 	}
 	grid[sx][sy] = -2
-	(*empty)--
-	dfs(res, grid, sx, sy-1, ex, ey, empty)
-	dfs(res, grid, sx, sy+1, ex, ey, empty)
-	dfs(res, grid, sx-1, sy, ex, ey, empty)
-	dfs(res, grid, sx+1, sy, ex, ey, empty)
-
-	(*empty)++
+	empty--
+	dfs(res, grid, sx, sy-1, empty)
+	dfs(res, grid, sx, sy+1, empty)
+	dfs(res, grid, sx-1, sy, empty)
+	dfs(res, grid, sx+1, sy, empty)
 	grid[sx][sy] = 0
+	empty++
 }
