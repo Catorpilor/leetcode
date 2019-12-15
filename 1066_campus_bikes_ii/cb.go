c3b8c9d2ed9a82b2a6d3c37bdba8e0047a44ef24
Catorpilor <jiaojiangonghitster@gmail.com>
2019-12-15 23:51:53 +0800

    fix: try to solve use dp

--- 1066_campus_bikes_ii/cb.go
+++ 1066_campus_bikes_ii/cb.go
@@ -1,6 +1,7 @@
 package cb
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/catorpilor/LeetCode/utils"
@@ -37,4 +38,36 @@ func dfs(visited *[]bool, bikes, workers [][]int, wid, distance int, res *int) {
 
 func dis(p1, p2 []int) int {
 	return utils.Abs(p1[0]-p2[0]) + utils.Abs(p1[1]-p2[1])
+}
+
+func useDp(workers, bikes [][]int) int {
+	nw, nb := len(workers), len(bikes)
+	if nw == 0 || nb == 0 {
+		return 0
+	}
+	res := math.MaxInt32
+	dp := make([][]int, nw+1)
+	// init dp
+	for i := range dp {
+		dp[i] = make([]int, 1<<nb)
+		for j := 0; j < (1 << nb); j++ {
+			dp[i][j] = math.MaxInt32 / 2
+		}
+	}
+	for i := 1; i <= nw; i++ {
+		for s := 1; s < (1 << nb); s++ {
+			for j := 0; j < nb; j++ {
+				if (s & (1 << j)) == 0 {
+					continue
+				}
+				prev := s ^ (1 << j)
+				dp[i][s] = utils.Min(dp[i][s], dp[i-1][prev]+dis(workers[i-1], bikes[j]))
+				if i == nw {
+					res = utils.Min(res, dp[i][s])
+					fmt.Printf("all the worker are assigned, res:%d, dp[i][s]:%d\n", res, dp[i][s])
+				}
+			}
+		}
+	}
+	return res
 }
--- 1066_campus_bikes_ii/cb_test.go
+++ 1066_campus_bikes_ii/cb_test.go
@@ -24,3 +24,26 @@ func TestAssignBikes(t *testing.T) {
 	}
 
 }
+
+func TestUseDp(t *testing.T) {
+	st := []struct {
+		name           string
+		workers, bikes [][]int
+		exp            int
+	}{
+		{"empty workers", [][]int{}, [][]int{[]int{1, 2}}, 0},
+		{"empty bikes", [][]int{[]int{0, 0}}, [][]int{}, 0},
+		{"testcase1", [][]int{[]int{0, 0}, []int{2, 1}}, [][]int{[]int{1, 2}, []int{3, 3}}, 6},
+		{"testcase2", [][]int{[]int{0, 0}, []int{1, 1}, []int{2, 0}}, [][]int{[]int{1, 0}, []int{2, 2}, []int{2, 1}}, 4},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useDp(tt.workers, tt.bikes)
+			if out != tt.exp {
+				t.Fatalf("with workers: %v and bikes %v wanted %d but got %d", tt.workers, tt.bikes, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+
+}
