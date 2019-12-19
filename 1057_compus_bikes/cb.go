99764c2a7f5c1ed5361d9132c090defed30a907d
Catorpilor <jiaojiangonghitster@gmail.com>
2019-12-19 20:40:35 +0800

    feat: use bucket sort based on discussion

--- 1057_compus_bikes/cb.go
+++ 1057_compus_bikes/cb.go
@@ -7,6 +7,10 @@ import (
 )
 
 func assignBikes(workers, bikes [][]int) []int {
+	return pq(workers, bikes)
+}
+
+func pq(workers, bikes [][]int) []int {
 	nw, nb := len(workers), len(bikes)
 	if nw == 0 || nb == 0 {
 		return []int{}
@@ -33,6 +37,41 @@ func assignBikes(workers, bikes [][]int) []int {
 		if res[tup[1]] == -1 && !set[tup[2]] {
 			res[tup[1]] = tup[2]
 			set[tup[2]] = true
+		}
+	}
+	return res
+}
+
+// bucket use bucket sort to solve the problem
+func bucket(workers, bikes [][]int) []int {
+	nw, nb := len(workers), len(bikes)
+	if nw == 0 || nb == 0 {
+		return []int{}
+	}
+	// since distance ranges from [0,2000]
+	// we create a bucket to store the (worker, bike) pairs
+	// for bucket[i] stores the distance=i's pairs
+	type pair struct {
+		w, b int
+	}
+	bkt := [2001][]pair{}
+	for i := 0; i < nw; i++ {
+		for j := 0; j < nb; j++ {
+			dis := utils.Abs(workers[i][0]-bikes[j][0]) + utils.Abs(workers[i][1]-bikes[j][1])
+			bkt[dis] = append(bkt[dis], pair{w: i, b: j})
+		}
+	}
+	res := make([]int, nb)
+	for i := range res {
+		res[i] = -1
+	}
+	set := make(map[int]bool, nb)
+	for d := 0; d <= 2000; d++ {
+		for k := 0; k < len(bkt[d]); k++ {
+			if res[bkt[d][k].w] == -1 && !set[bkt[d][k].b] {
+				set[bkt[d][k].b] = true
+				res[bkt[d][k].w] = bkt[d][k].b
+			}
 		}
 	}
 	return res
