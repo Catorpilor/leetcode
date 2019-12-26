a5d408162b996171f0292fbdcf9ca1bee8ed405a
Catorpilor <jiaojiangonghitster@gmail.com>
2019-12-26 23:18:58 +0800

    fix: make it pass

--- 381_insert_delete_getrandom_o1_duplicates_allowed/idgd.go
+++ 381_insert_delete_getrandom_o1_duplicates_allowed/idgd.go
@@ -1,22 +1,56 @@
 package idgd
 
-import "math/rand"
+import (
+	"errors"
+	"math/rand"
+)
+
+type unordered_map struct {
+	set map[int]int
+}
+
+func NewUm() unordered_map {
+	return unordered_map{set: make(map[int]int)}
+}
+
+func (u unordered_map) Exists(val int) bool {
+	_, exists := u.set[val]
+	return exists
+}
+
+func (u unordered_map) Iter() (int, error) {
+	for k := range u {
+		return k, nil
+	}
+	return -1, errors.New("empty set")
+}
+
+func (u unordered_map) Add(val int) {
+	u.set[val]++
+}
 
 type RandomizedCollection struct {
-	hashset map[int][]int
+	hashset map[int]unordered_map
 	res     []int
 }
 
 /** Initialize your data structure here. */
 func Constructor() RandomizedCollection {
-	return RandomizedCollection{hashset: make(map[int][]int)}
+	return RandomizedCollection{hashset: make(map[int]unordered_map)}
 }
 
 /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
 func (this *RandomizedCollection) Insert(val int) bool {
+	if u, exists := this.hashset[val]; !exists {
+		u = NewUm()
+		u.Add(len(this.res))
+		this.res = append(this.res, val)
+		return true
+	}
+	this.hashset[val].Add(len(this.res))
 	this.res = append(this.res, val)
-	this.hashset[val] = append(this.hashset[val], len(this.res)-1)
-	return true
+	// this.hashset[val] = append(this.hashset[val], len(this.res)-1)
+	return false
 }
 
 /** Removes a value from the collection. Returns true if the collection contained the specified element. */
