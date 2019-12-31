db30ddfe824cc0f643223b385c273293c23aff17
Catorpilor <jiaojiangonghitster@gmail.com>
2019-12-31 22:15:01 +0800

    fix: add brute force version with presum

--- 1031_maximum_sum_of_two_non_overlapping_subarrays/nos.go
+++ 1031_maximum_sum_of_two_non_overlapping_subarrays/nos.go
@@ -7,13 +7,13 @@ func maxSum(A []int, l, m int) int {
     if n < 1 {
         return 0
     }
-    presum := make([]int, n)
-    presum[0] = A[0]
-    for i := 1; i < n; i++ {
-        presum[i] = presum[i-1] + A[i]
+    presum := make([]int, n+1)
+    for i := 1; i <= n; i++ {
+        presum[i] = presum[i-1] + A[i-1]
     }
     // return withPresum(presum, l, m)
-    return utils.Max(cal(presum, l, m), cal(presum, m, l))
+    // return utils.Max(cal(presum, l, m), cal(presum, m, l))
+    return utils.Max(burteForce(presum, l, m), burteForce(presum, m, l))
 }
 
 func withPresum(preSum []int, l, m int) int {
@@ -36,6 +36,20 @@ func cal(preSum []int, l, m int) int {
         // lmax stores the max L continous subarray sum that before M
         lmax = utils.Max(lmax, preSum[i-m]-preSum[i-m-l])
         res = utils.Max(res, lmax+preSum[i]-preSum[i-m])
+    }
+    return res
+}
+
+// bruteForce with presum time complexity O(n^2), Space Complexity O(n)
+func burteForce(presum []int, l, m int) int {
+    var res int
+    n := len(presum)
+    for i := 0; i < n-1-m; i++ {
+        for j := i + l; j+m-1 < n-1; j++ {
+            suml := presum[i+l] - presum[i]
+            summ := presum[j+m] - presum[j]
+            res = utils.Max(res, suml+summ)
+        }
     }
     return res
 }
