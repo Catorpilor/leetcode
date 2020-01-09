95908ac214f43b1a25c836d7be026188c759336a
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-09 20:41:30 +0800

    fix: mine works

--- 713_subarray_product_less_than_k/splk.go
+++ 713_subarray_product_less_than_k/splk.go
@@ -1,9 +1,7 @@
 package splk
 
-import "fmt"
-
 func numsOfSubarray(nums []int, k int) int {
-	return twoPointers(nums, k)
+	return mine(nums, k)
 }
 
 func twoPointers(nums []int, k int) int {
@@ -23,6 +21,8 @@ func twoPointers(nums []int, k int) int {
 	return res
 }
 
+// mine inspired by twoPointers, did some modification
+// time complexity O(n^2), space complexity is O(n)
 func mine(nums []int, k int) int {
 	n := len(nums)
 	var res int
@@ -32,41 +32,29 @@ func mine(nums []int, k int) int {
 	// sort.Slice(nums, func(i, j int) bool { return nums[i] < nums[j] })
 	opts := make([]int, n)
 	for i := range opts {
-		if nums[i] < k {
-			opts[i] = 1
-		} else {
-			opts[i] = -1
-		}
+		opts[i] = i
 	}
-	opts[0] = 0
 	// fmt.Printf("before parse: opt: %v\n", opts)
-	prodSofar := nums[0]
-	for i := 1; i < n; i++ {
-		if nums[i] > k {
-			fmt.Printf("nums[%d] = %d, k: %d\n", i, nums[i], k)
-			prodSofar = 1
-			continue
-		}
-		temp := nums[i] * prodSofar
-		if temp < k {
-			if opts[i-1] != -1 {
+	prodSofar, l := 1, 0
+	for i := 0; i < n; i++ {
+		prodSofar *= nums[i]
+		if prodSofar < k {
+			if i > 0 && opts[i-1] != -1 {
 				opts[i] = opts[i-1]
 			}
-			prodSofar = temp
 			continue
 		}
-		fmt.Printf("current i:%d, temp: %d, k:%d \n", i, temp, k)
-		prodSofar = nums[i]
-		j := i - 1
-		for j >= 0 {
-			t := nums[j] * prodSofar
-			if t >= k {
-				break
-			}
-			prodSofar = t
-			opts[i] = j
-			j--
+		for l <= i && prodSofar >= k {
+			prodSofar /= nums[l]
+			l++
 		}
+		// fmt.Printf("current i:%d, temp: %d, k:%d \n", i, temp, k)
+		if l > i {
+			opts[i] = -1
+		} else {
+			opts[i] = l
+		}
+
 	}
 	// fmt.Printf("opts: %v\n", opts)
 	for i := range opts {
