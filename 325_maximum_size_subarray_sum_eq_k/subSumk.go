603724ceada07ac2c60bf1468004aafbf7338367
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-10 20:17:40 +0800

    fix: solve 325 using prefix sum and hash map

--- 325_maximum_size_subarray_sum_eq_k/subSumk.go
+++ 325_maximum_size_subarray_sum_eq_k/subSumk.go
@@ -3,10 +3,11 @@ package subSumk
 import "github.com/catorpilor/LeetCode/utils"
 
 func maxSubLen(nums []int, k int) int {
-	return bruteForce(nums, k)
+	return preSumWithHash(nums, k)
 }
 
-func bruteForce(nums []int, k int) int {
+// preSumWithHash time complexity O(2n) space complexity O(n)
+func preSumWithHash(nums []int, k int) int {
 	n := len(nums)
 	if n < 1 {
 		return 0
@@ -17,5 +18,18 @@ func bruteForce(nums []int, k int) int {
 		preSum[i] = preSum[i-1] + nums[i]
 	}
 	var res int
+	// key is the sum and value is the index
+	set := make(map[int]int, n)
+	// edge case,
+	// when sum is 0, the length of sub array is index - (-1)
+	set[0] = -1
+	for i := range preSum {
+		if v, exists := set[preSum[i]-k]; exists {
+			res = utils.Max(res, i-v)
+		}
+		if _, exists := set[preSum[i]]; !exists {
+			set[preSum[i]] = i
+		}
+	}
 	return res
 }
