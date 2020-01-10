a3499ec176186b813f7d2f6e0ca242beb8b8dfe4
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-10 20:34:12 +0800

    fix: brute force version

--- 325_maximum_size_subarray_sum_eq_k/subSumk.go
+++ 325_maximum_size_subarray_sum_eq_k/subSumk.go
@@ -33,3 +33,25 @@ func preSumWithHash(nums []int, k int) int {
 	}
 	return res
 }
+
+// bruteForce time complexity is O(n^2) space complexity is O(1)
+func bruteForce(nums []int, k int) int {
+	n := len(nums)
+	if n < 1 {
+		return 0
+	}
+	var res, sum int
+	for i := range nums {
+		sum = nums[i]
+		if sum == k {
+			res = utils.Max(res, 1)
+		}
+		for j := i + 1; j < n; j++ {
+			sum += nums[j]
+			if sum == k {
+				res = utils.Max(res, j-i+1)
+			}
+		}
+	}
+	return res
+}
