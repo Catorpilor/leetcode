c29ed2abdfc7d6391b3bf2e4ff413a2268292aba
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-11 20:52:28 +0800

    solve using hashmap and prefixSum

--- 523_continuous_subarray_sum/css.go
+++ 523_continuous_subarray_sum/css.go
@@ -4,12 +4,12 @@ func checkSubArraySum(nums []int, k int) bool {
 	return bruteForce(nums, k)
 }
 
+// bruteForce time complexity O(n^2) space complexity O(1)
 func bruteForce(nums []int, k int) bool {
 	n := len(nums)
 	if n < 2 {
 		return false
 	}
-	var res bool
 	var sum int
 	for i := range nums {
 		sum = 0
@@ -21,6 +21,38 @@ func bruteForce(nums []int, k int) bool {
 					return true
 				}
 			}
+		}
+	}
+	return false
+}
+
+// preSumHash time complexity o(N), space complexity O(N)
+func preSumHash(nums []int, k int) bool {
+	n := len(nums)
+	if n < 2 {
+		return false
+	}
+	// key is the sum%k, value is index
+	// based on this math equation
+	// sum1 % k = x ----- eq1
+	// sum2 % k = x ----- eq2
+	// then (sum1-sum2) % k = x - x = 0 ------ eq3
+	hash := make(map[int]int, n)
+	// edge case
+	hash[0] = -1
+	var sum int
+	for i := range nums {
+		sum += nums[i]
+		if k != 0 {
+			sum %= k
+		}
+		if v, exists := hash[sum]; exists {
+			// found one based on eq3
+			if i-v > 1 {
+				return true
+			}
+		} else {
+			hash[sum] = i
 		}
 	}
 	return false
--- 523_continuous_subarray_sum/css_test.go
+++ 523_continuous_subarray_sum/css_test.go
@@ -26,3 +26,29 @@ func TestCheckSubArraySum(t *testing.T) {
 	}
 
 }
+
+func TestPrefixSum(t *testing.T) {
+	st := []struct {
+		name string
+		nums []int
+		k    int
+		exp  bool
+	}{
+		{"empty slice", []int{}, 2, false},
+		{"not found", []int{1, 2, 3}, 4, false},
+		{"testcase1", []int{1, 1, 1}, 2, true},
+		{"testcase2", []int{23, 2, 4, 6, 7}, 6, true},
+		{"failed1", []int{0, 0}, 0, true},
+		{"failed2", []int{0, 1, 0}, 0, false},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := preSumHash(tt.nums, tt.k)
+			if out != tt.exp {
+				t.Fatalf("with nums: %v and k: %d wanted %t but got %t", tt.nums, tt.k, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+
+}
