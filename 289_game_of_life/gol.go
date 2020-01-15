e87fb8d34207672b6a05a6ca62c91e0e8d9fe416
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-15 09:39:14 +0800

    refactor: clean code

--- 289_game_of_life/gol.go
+++ 289_game_of_life/gol.go
@@ -26,10 +26,12 @@ func bruteForce(board [][]int) [][]int {
         }
     }
 
+    neighbors := [3]int{0, 1, -1}
+
     hset := make(map[pos]int, n*m)
     for i := 0; i < n; i++ {
         for j := 0; j < m; j++ {
-            numOfLives := process(bak, i, j, n, m)
+            numOfLives := process(bak, i, j, n, m, neighbors)
             if bak[i][j] == 1 {
                 if numOfLives < 2 || numOfLives > 3 {
                     hset[pos{i, j}] = 0
@@ -46,31 +48,41 @@ func bruteForce(board [][]int) [][]int {
     }
     return bak
 }
-func process(board [][]int, i, j, n, m int) int {
+func process(board [][]int, i, j, n, m int, neighbors [3]int) int {
     var res int
-    if i > 0 {
-        res += board[i-1][j]
+    for x := 0; x < 3; x++ {
+        for y := 0; y < 3; y++ {
+            if !(neighbors[x] == 0 && neighbors[y] == 0) {
+                r, c := i+neighbors[x], j+neighbors[y]
+                if (r < n && r >= 0) && (c < m && c >= 0) {
+                    res += board[r][c]
+                }
+            }
+        }
     }
-    if i < n-1 {
-        res += board[i+1][j]
-    }
-    if j > 0 {
-        res += board[i][j-1]
-    }
-    if j < m-1 {
-        res += board[i][j+1]
-    }
-    if i > 0 && j > 0 {
-        res += board[i-1][j-1]
-    }
-    if i > 0 && j < m-1 {
-        res += board[i-1][j+1]
-    }
-    if i < n-1 && j > 0 {
-        res += board[i+1][j-1]
-    }
-    if i < n-1 && j < m-1 {
-        res += board[i+1][j+1]
-    }
+    // if i > 0 {
+    //     res += board[i-1][j]
+    // }
+    // if i < n-1 {
+    //     res += board[i+1][j]
+    // }
+    // if j > 0 {
+    //     res += board[i][j-1]
+    // }
+    // if j < m-1 {
+    //     res += board[i][j+1]
+    // }
+    // if i > 0 && j > 0 {
+    //     res += board[i-1][j-1]
+    // }
+    // if i > 0 && j < m-1 {
+    //     res += board[i-1][j+1]
+    // }
+    // if i < n-1 && j > 0 {
+    //     res += board[i+1][j-1]
+    // }
+    // if i < n-1 && j < m-1 {
+    //     res += board[i+1][j+1]
+    // }
     return res
 }
