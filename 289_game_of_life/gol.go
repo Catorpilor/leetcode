22979b7a47b6b06f06805f954c050ccf4467e3ce
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-15 09:57:11 +0800

    based on discussion, space optimized

--- 289_game_of_life/gol.go
+++ 289_game_of_life/gol.go
@@ -1,5 +1,7 @@
 package gol
 
+import "github.com/catorpilor/leetcode/utils"
+
 func gameOfLife(board [][]int) [][]int {
     return bruteForce(board)
 }
@@ -84,5 +86,61 @@ func process(board [][]int, i, j, n, m int, neighbors [3]int) int {
     // if i < n-1 && j < m-1 {
     //     res += board[i+1][j+1]
     // }
+    return res
+}
+
+// spaceOptimized time complexity is O(NM), space complexity is O(1)
+func spaceOptimized(board [][]int) [][]int {
+    n := len(board)
+    if n < 1 {
+        return board
+    }
+    m := len(board[0])
+    if m < 1 {
+        return board
+    }
+    neighbors := [3]int{0, 1, -1}
+    for i := 0; i < n; i++ {
+        for j := 0; j < m; j++ {
+            numOfLives := processWithAbs(board, i, j, n, m, neighbors)
+            if board[i][j] == 1 {
+                if numOfLives < 2 || numOfLives > 3 {
+                    // trick part, -1 means prev state is a live cell
+                    // when calculate numOfLives we use abs to make sure
+                    // did not affect others
+                    board[i][j] = -1
+                }
+            } else {
+                if numOfLives == 3 {
+                    // use 2 to represent prev state is a dead cell
+                    board[i][j] = 2
+                }
+            }
+        }
+    }
+    for i := 0; i < n; i++ {
+        for j := 0; j < m; j++ {
+            if board[i][j] > 0 {
+                board[i][j] = 1
+            } else {
+                board[i][j] = 0
+            }
+        }
+    }
+    return board
+}
+
+func processWithAbs(board [][]int, i, j, n, m int, neighbors [3]int) int {
+    var res int
+    for x := 0; x < 3; x++ {
+        for y := 0; y < 3; y++ {
+            if !(neighbors[x] == 0 && neighbors[y] == 0) {
+                r, c := i+neighbors[x], j+neighbors[y]
+                if (r < n && r >= 0) && (c < m && c >= 0) && utils.Abs(board[r][c]) == 1 {
+                    res += 1
+                }
+            }
+        }
+    }
     return res
 }
--- 289_game_of_life/gol_test.go
+++ 289_game_of_life/gol_test.go
@@ -25,3 +25,24 @@ func TestGameOfLife(t *testing.T) {
         })
     }
 }
+
+func TestOptimized(t *testing.T) {
+    st := []struct {
+        name  string
+        board [][]int
+        exp   [][]int
+    }{
+        {"board is empty", [][]int{}, [][]int{}},
+        {"testcase1", [][]int{[]int{0, 1, 0}, []int{0, 0, 1}, []int{1, 1, 1}, []int{0, 0, 0}},
+            [][]int{[]int{0, 0, 0}, []int{1, 0, 1}, []int{0, 1, 1}, []int{0, 1, 0}}},
+    }
+    for _, tt := range st {
+        t.Run(tt.name, func(t *testing.T) {
+            out := spaceOptimized(tt.board)
+            if !reflect.DeepEqual(tt.exp, out) {
+                t.Fatalf("with board: %v wanted %v but got %v", tt.board, tt.exp, out)
+            }
+            t.Log("pass")
+        })
+    }
+}
