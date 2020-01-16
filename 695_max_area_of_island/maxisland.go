8c35739591184215ba64b2833e90c341ed77cad9
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-16 11:11:11 +0800

    solve 695 using dfs

--- 695_max_area_of_island/maxisland.go
+++ 695_max_area_of_island/maxisland.go
@@ -1,5 +1,52 @@
 package maxisland
 
 func maxArea(grid [][]int) int {
-    return 0
+    return dfs(grid)
+}
+
+// dfs time complexity is O(M*N), space complexity is O(1)
+func dfs(grid [][]int) int {
+    m := len(grid)
+    if m < 1 {
+        return 0
+    }
+    n := len(grid[0])
+    if n < 1 {
+        return 0
+    }
+    var res int
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if grid[i][j] == 1 {
+                cval := helper(grid, i, j, m, n)
+                if cval > res {
+                    res = cval
+                }
+            }
+        }
+    }
+    return res
+}
+
+func helper(grid [][]int, i, j, m, n int) int {
+    var res int
+    if grid[i][j] == 1 {
+        res += 1
+        grid[i][j] = 0
+    } else {
+        return res
+    }
+    if i > 0 {
+        res += helper(grid, i-1, j, m, n)
+    }
+    if i < m-1 {
+        res += helper(grid, i+1, j, m, n)
+    }
+    if j > 0 {
+        res += helper(grid, i, j-1, m, n)
+    }
+    if j < n-1 {
+        res += helper(grid, i, j+1, m, n)
+    }
+    return res
 }
--- 695_max_area_of_island/maxisland_test.go
+++ 695_max_area_of_island/maxisland_test.go
@@ -11,7 +11,7 @@ func TestMaxArea(t *testing.T) {
         {"empty grid", [][]int{}, 0},
         {"all zeros", [][]int{[]int{0, 0, 0, 0}}, 0},
         {"all ones", [][]int{[]int{1, 1, 1, 1}}, 4},
-        {"testcases1", [][]int{[]int{1, 0}, []int{0, 1}}, 0},
+        {"testcases1", [][]int{[]int{1, 0}, []int{0, 1}}, 1},
         {"testcase2", [][]int{[]int{0, 1, 0}, []int{1, 1, 1}, []int{0, 1, 1}}, 6},
     }
     for _, tt := range st {
