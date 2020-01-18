8a46fe2e0928f214e43443cf5c4e241c763d58a2
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-18 14:46:46 +0800

    solve 695 using union find

--- 695_max_area_of_island/maxisland.go
+++ 695_max_area_of_island/maxisland.go
@@ -1,5 +1,7 @@
 package maxisland
 
+import "github.com/catorpilor/leetcode/utils/uf"
+
 func maxArea(grid [][]int) int {
     return dfs(grid)
 }
@@ -47,6 +49,55 @@ func helper(grid [][]int, i, j, m, n int) int {
     }
     if j < n-1 {
         res += helper(grid, i, j+1, m, n)
+    }
+    return res
+}
+
+func withUnionFind(grid [][]int) int {
+    m := len(grid)
+    if m < 1 {
+        return 0
+    }
+    n := len(grid[0])
+    if n < 1 {
+        return 0
+    }
+    wqc := uf.NewWQUPC(m * n)
+    var idx int
+    dirs := [4][2]int{[2]int{0, 1}, [2]int{0, -1}, [2]int{1, 0}, [2]int{-1, 0}}
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if grid[i][j] == 1 {
+                idx = i*n + j
+                for _, v := range dirs {
+                    ni, nj := i+v[0], i+v[1]
+                    nidx := ni*n + nj
+                    if ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == 1 {
+                        wqc.Union(idx, nidx)
+                    }
+                }
+            }
+        }
+    }
+    hset := make(map[int]int, n*m)
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if grid[i][j] == 1 {
+                idx = i*n + j
+                root := wqc.Root(idx)
+                if _, exists := hset[root]; !exists {
+                    hset[root] = 1
+                } else {
+                    hset[root]++
+                }
+            }
+        }
+    }
+    var res int
+    for _, v := range hset {
+        if v > res {
+            res = v
+        }
     }
     return res
 }
