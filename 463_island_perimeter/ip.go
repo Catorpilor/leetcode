14663db22c39932b0ee052bb2a49c0b70202d625
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-19 16:23:41 +0800

    solve 463 using equation sizeOfIsland*4 - neighbors*2

--- 463_island_perimeter/ip.go
+++ 463_island_perimeter/ip.go
@@ -1,5 +1,35 @@
 package ip
 
 func islandPerimeter(grid [][]int) int {
-    return 0
+    return countWithEqu(grid)
+}
+
+// countWithEqu time complexity O(MN), space complexity O(1)
+func countWithEqu(grid [][]int) int {
+    m := len(grid)
+    if m < 1 {
+        return 0
+    }
+    n := len(grid[0])
+    if n < 1 {
+        return 0
+    }
+    var islands, neighbors int
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if grid[i][j] == 1 {
+                islands++
+                if j < n-1 && grid[i][j+1] == 1 {
+                    // right neighbors
+                    neighbors++
+                }
+                if i < m-1 && grid[i+1][j] == 1 {
+                    // down neighbors
+                    neighbors++
+                }
+            }
+        }
+    }
+    // fmt.Printf("res: %d\n", res)
+    return islands*4 - neighbors*2
 }
