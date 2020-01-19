03e3d13096b6a1eeef19668c6957e44dd49d846e
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-19 16:34:12 +0800

    solve 463 using iteration by couting edges

--- 463_island_perimeter/ip.go
+++ 463_island_perimeter/ip.go
@@ -33,3 +33,36 @@ func countWithEqu(grid [][]int) int {
     // fmt.Printf("res: %d\n", res)
     return islands*4 - neighbors*2
 }
+
+// iterator count the num of edges of island, time complexity O(MN)
+func iterator(grid [][]int) int {
+    m := len(grid)
+    if m < 1 {
+        return 0
+    }
+    n := len(grid[0])
+    if n < 1 {
+        return 0
+    }
+    var res int
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            // if grid[i][j] == 1 we were supposed to add 4 edges, but we've to check its neighbors
+            if grid[i][j] == 1 {
+                if i == 0 || grid[i-1][j] == 0 {
+                    res++
+                }
+                if i == m-1 || grid[i+1][j] == 0 {
+                    res++
+                }
+                if j == 0 || grid[i][j-1] == 0 {
+                    res++
+                }
+                if j == n-1 || grid[i][j+1] == 0 {
+                    res++
+                }
+            }
+        }
+    }
+    return res
+}
--- 463_island_perimeter/ip_test.go
+++ 463_island_perimeter/ip_test.go
@@ -24,3 +24,26 @@ func TestIslandPerimeter(t *testing.T) {
         })
     }
 }
+
+func TestIslandPerimeterWithIter(t *testing.T) {
+    st := []struct {
+        name string
+        grid [][]int
+        exp  int
+    }{
+        {"empty grid", [][]int{}, 0},
+        {"testcase1", [][]int{[]int{1, 0, 0, 0}}, 4},
+        {"testcase2", [][]int{[]int{1, 1, 0, 0}}, 6},
+        {"testcase3", [][]int{[]int{1, 1, 1, 1}}, 10},
+        {"testcase3", [][]int{[]int{0, 1, 0, 0}, []int{1, 1, 1, 0}, []int{0, 1, 0, 0}, []int{1, 1, 0, 0}}, 16},
+    }
+    for _, tt := range st {
+        t.Run(tt.name, func(t *testing.T) {
+            out := iterator(tt.grid)
+            if out != tt.exp {
+                t.Fatalf("with grid: %v wanted %d but got %d", tt.grid, tt.exp, out)
+            }
+            t.Log("pass")
+        })
+    }
+}
