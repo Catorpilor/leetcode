289c2df981b5b4064388d9df93a4e1bad41ed4e2
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-20 10:52:13 +0800

    solve 1325 using recursion

--- 1325_delete_leaves_with_a_given_value/leave.go
+++ 1325_delete_leaves_with_a_given_value/leave.go
@@ -3,5 +3,18 @@ package leave
 import "github.com/catorpilor/leetcode/utils"
 
 func removeLeafNodes(root *utils.TreeNode, target int) *utils.TreeNode {
-    return nil
+    return recur(root, target)
+}
+
+// recur time comlexity O(N) N nodes, space complexity O(lgn)
+func recur(root *utils.TreeNode, target int) *utils.TreeNode {
+    if root == nil {
+        return root
+    }
+    root.Left = recur(root.Left, target)
+    root.Right = recur(root.Right, target)
+    if root.Left == nil && root.Right == nil && root.Val == target {
+        root = nil
+    }
+    return root
 }
