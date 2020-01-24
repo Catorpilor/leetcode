03d5d03a87c98f5bff0dfcb745aa8f8ea2b83695
Catorpilor <jiaojiangonghitster@gmail.com>
2020-01-24 10:19:52 +0800

    faster bfs version solve 127

--- 127_word_ladder/wl.go
+++ 127_word_ladder/wl.go
@@ -3,77 +3,119 @@ package wl
 import "fmt"
 
 func ladderLength(beg, end string, lists []string) int {
-    return bfs(beg, end, lists)
+	return bfs(beg, end, lists)
 }
 
 // bfs time complexity O(N^2*M) where M is the length of lists[i]
 // space complexity is O(N)
 func bfs(beg, end string, lists []string) int {
-    n := len(lists)
-    if n < 1 {
-        return 0
-    }
-    hset := make(map[string][]string, len(lists))
-    helper(hset, lists)
-    if _, exists := hset[end]; !exists {
-        return 0
-    }
-    // level 1
-    level := 1
-    visited := make(map[string]int, len(lists))
-    leveld := make([]string, 0, len(lists))
-    for i := range lists {
-        if diffOne(beg, lists[i]) {
-            leveld = append(leveld, lists[i])
-            visited[lists[i]] = level + 1
-        }
-        if beg == lists[i] {
-            visited[beg] = level
-        }
-    }
-    level++
-    fmt.Printf("beg's diffOne resule: %v\n", leveld)
-    for len(leveld) != 0 {
-        next := make([]string, 0, len(lists))
-        for i := range leveld {
-            if leveld[i] == end {
-                return level
-            }
-            for _, str := range hset[leveld[i]] {
-                if _, exists := visited[str]; exists {
-                    continue
-                }
-                next = append(next, str)
-                visited[str] = level + 1
-            }
-        }
-        leveld = next
-        level++
-        fmt.Printf("next level:%d, with vals: %v\n", level, leveld)
-    }
-    return 0
+	n := len(lists)
+	if n < 1 {
+		return 0
+	}
+	hset := make(map[string][]string, len(lists))
+	helper(hset, lists)
+	// withPattern(hset, lists, len(beg))
+	if _, exists := hset[end]; !exists {
+		return 0
+	}
+	// level 1
+	level := 1
+	visited := make(map[string]int, len(lists))
+	leveld := make([]string, 0, len(lists))
+	for i := range lists {
+		if diffOne(beg, lists[i]) {
+			leveld = append(leveld, lists[i])
+			visited[lists[i]] = level + 1
+		}
+		if beg == lists[i] {
+			visited[beg] = level
+		}
+	}
+	level++
+	fmt.Printf("beg's diffOne resule: %v\n", leveld)
+	for len(leveld) != 0 {
+		next := make([]string, 0, len(lists))
+		for i := range leveld {
+			if leveld[i] == end {
+				return level
+			}
+			for _, str := range hset[leveld[i]] {
+				if _, exists := visited[str]; exists {
+					continue
+				}
+				next = append(next, str)
+				visited[str] = level + 1
+			}
+		}
+		leveld = next
+		level++
+		fmt.Printf("next level:%d, with vals: %v\n", level, leveld)
+	}
+	return 0
+}
+
+func withPattern(hset map[string][]string, lists []string, l int) {
+	for _, str := range lists {
+		for i := 0; i < l; i++ {
+			key := str[:i] + "*" + str[i+1:]
+			hset[key] = append(hset[key], str)
+		}
+	}
 }
 
 func helper(hset map[string][]string, lists []string) {
-    for i := range lists {
-        hset[lists[i]] = make([]string, 0, len(lists))
-        for j := range lists {
-            if i == j {
-                continue
-            }
-            if diffOne(lists[i], lists[j]) {
-                hset[lists[i]] = append(hset[lists[i]], lists[j])
-            }
-        }
-    }
+	for i := range lists {
+		hset[lists[i]] = make([]string, 0, len(lists))
+		for j := range lists {
+			if i == j {
+				continue
+			}
+			if diffOne(lists[i], lists[j]) {
+				hset[lists[i]] = append(hset[lists[i]], lists[j])
+			}
+		}
+	}
 }
 
 func diffOne(a, b string) bool {
-    diffs := 0
-    for i := 0; i < len(a); i++ {
-        if a[i] != b[i] {
-            diffs++
-        }
-    }
-    return diffs == 1
+	diffs := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			diffs++
+		}
+	}
+	return diffs == 1
+}
+
+func bfsWithPattern(beg, end string, lists []string) int {
+	hset := make(map[string][]string, len(lists))
+	withPattern(hset, lists, len(beg))
+	visited := make(map[string]bool, len(lists))
+	leveld := make([]string, 0, len(lists))
+	for i := 0; i < len(beg); i++ {
+		key := beg[:i] + "*" + beg[i+1:]
+		leveld = append(leveld, hset[key]...)
+	}
+	visited[beg] = true
+	level := 2
+	for len(leveld) != 0 {
+		next := make([]string, 0, len(lists))
+		for _, str := range leveld {
+			if visited[str] {
+				continue
+			}
+			visited[str] = true
+			if str == end {
+				return level
+			}
+			for i := 0; i < len(beg); i++ {
+				key := str[:i] + "*" + str[i+1:]
+				next = append(next, hset[key]...)
+			}
+		}
+		level++
+		leveld = next
+	}
+	return 0
 }
--- 127_word_ladder/wl_test.go
+++ 127_word_ladder/wl_test.go
@@ -3,50 +3,77 @@ package wl
 import "testing"
 
 func TestLadderLength(t *testing.T) {
-    st := []struct {
-        name       string
-        begin, end string
-        wordList   []string
-        exp        int
-    }{
-        {"empty wordList", "abc", "cde", []string{}, 0},
-        {"testcase1", "hit", "cog", []string{"hot", "dot", "dog", "log", "cog"}, 5},
-        {"testcase2", "hit", "cog", []string{"hot", "dot", "dog", "log"}, 0},
-        {"failed1", "a", "c", []string{"a", "b", "c"}, 2},
-        {"failed2", "hot", "dot", []string{"hot", "dot", "dog"}, 2},
-        {"failed3", "leet", "code", []string{"lest", "leet", "lose", "code", "lode", "robe", "lost"}, 6},
-        {"failed4", "hog", "cog", []string{"cog"}, 2},
-    }
-    for _, tt := range st {
-        t.Run(tt.name, func(t *testing.T) {
-            out := ladderLength(tt.begin, tt.end, tt.wordList)
-            if out != tt.exp {
-                t.Fatalf("with worldList: %v and begin:%s and end: %s wanted %d but got %d",
-                    tt.wordList, tt.begin, tt.end, tt.exp, out)
-            }
-            t.Log("pass")
-        })
-    }
+	st := []struct {
+		name       string
+		begin, end string
+		wordList   []string
+		exp        int
+	}{
+		{"empty wordList", "abc", "cde", []string{}, 0},
+		{"testcase1", "hit", "cog", []string{"hot", "dot", "dog", "log", "cog"}, 5},
+		{"testcase2", "hit", "cog", []string{"hot", "dot", "dog", "log"}, 0},
+		{"failed1", "a", "c", []string{"a", "b", "c"}, 2},
+		{"failed2", "hot", "dot", []string{"hot", "dot", "dog"}, 2},
+		{"failed3", "leet", "code", []string{"lest", "leet", "lose", "code", "lode", "robe", "lost"}, 6},
+		{"failed4", "hog", "cog", []string{"cog"}, 2},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := ladderLength(tt.begin, tt.end, tt.wordList)
+			if out != tt.exp {
+				t.Fatalf("with worldList: %v and begin:%s and end: %s wanted %d but got %d",
+					tt.wordList, tt.begin, tt.end, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
 }
 
 func TestDiffOne(t *testing.T) {
-    st := []struct {
-        name string
-        a, b string
-        exp  bool
-    }{
-        {"testcase1", "abc", "abd", true},
-        {"testcase2", "aac", "aab", true},
-        {"testcase3", "abc", "bdc", true},
-        {"testcase4", "abc", "bfe", false},
-    }
-    for _, tt := range st {
-        t.Run(tt.name, func(t *testing.T) {
-            out := diffOne(tt.a, tt.b)
-            if out != tt.exp {
-                t.Fatalf("with a:%s and b:%s wanted %t but got %t", tt.a, tt.b, tt.exp, out)
-            }
-            t.Log("pass")
-        })
-    }
+	st := []struct {
+		name string
+		a, b string
+		exp  bool
+	}{
+		{"testcase1", "abc", "abd", true},
+		{"testcase2", "aac", "aab", true},
+		{"testcase3", "abc", "bdc", false},
+		{"testcase4", "abc", "bfe", false},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := diffOne(tt.a, tt.b)
+			if out != tt.exp {
+				t.Fatalf("with a:%s and b:%s wanted %t but got %t", tt.a, tt.b, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
+
+func TestBfsWithPattern(t *testing.T) {
+	st := []struct {
+		name       string
+		begin, end string
+		wordList   []string
+		exp        int
+	}{
+		{"empty wordList", "abc", "cde", []string{}, 0},
+		{"testcase1", "hit", "cog", []string{"hot", "dot", "dog", "log", "cog"}, 5},
+		{"testcase2", "hit", "cog", []string{"hot", "dot", "dog", "log"}, 0},
+		{"failed1", "a", "c", []string{"a", "b", "c"}, 2},
+		{"failed2", "hot", "dot", []string{"hot", "dot", "dog"}, 2},
+		{"failed3", "leet", "code", []string{"lest", "leet", "lose", "code", "lode", "robe", "lost"}, 6},
+		{"failed4", "hog", "cog", []string{"cog"}, 2},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := bfsWithPattern(tt.begin, tt.end, tt.wordList)
+			if out != tt.exp {
+				t.Fatalf("with worldList: %v and begin:%s and end: %s wanted %d but got %d",
+					tt.wordList, tt.begin, tt.end, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
 }
