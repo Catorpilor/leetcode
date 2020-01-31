c969f746517cf94a222360b812d69a16218242d9
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-01 00:02:43 +0800

    solve 417 using bfs

--- 417_pacific_atlantic_water_flow/pawf.go
+++ 417_pacific_atlantic_water_flow/pawf.go
@@ -57,3 +57,83 @@ func helper(matrix [][]int, visited [][]bool, height, x, y, m, n int) {
         helper(matrix, visited, matrix[x][y], nx, ny, m, n)
     }
 }
+
+// bfs time complexity O(MN), space complexity O(MN)
+func bfs(matrix [][]int) [][]int {
+    var res [][]int
+    m := len(matrix)
+    if m < 1 {
+        return res
+    }
+    n := len(matrix[0])
+    if n < 1 {
+        return res
+    }
+    pacific, atlantic := make([][]bool, m), make([][]bool, m)
+    for i := 0; i < m; i++ {
+        pacific[i] = make([]bool, n)
+        atlantic[i] = make([]bool, n)
+    }
+    type pair struct {
+        x, y int
+    }
+    plevel, alevel := make([]pair, 0, m*n), make([]pair, 0, m*n)
+    // start from the boarder
+    for i := 0; i < m; i++ {
+        // left boader with pacific ocean
+        pacific[i][0] = true
+        plevel = append(plevel, pair{i, 0})
+        // right boarder with atlantic ocean
+        atlantic[i][n-1] = true
+        alevel = append(alevel, pair{i, n - 1})
+    }
+
+    for j := 0; j < n; j++ {
+        // top boarder with pacific ocean
+        pacific[0][j] = true
+        plevel = append(plevel, pair{0, j})
+        // bottom boarder with atlantic ocean
+        atlantic[m-1][j] = true
+        alevel = append(alevel, pair{m - 1, j})
+    }
+    dirs := [5]int{-1, 0, 1, 0, -1}
+    for len(plevel) != 0 {
+        next := make([]pair, 0, m*n)
+        for _, p := range plevel {
+            for i := 0; i < 4; i++ {
+                nx, ny := p.x+dirs[i], p.y+dirs[i+1]
+                if nx < 0 || nx >= m || ny < 0 || ny >= n || pacific[nx][ny] || matrix[nx][ny] < matrix[p.x][p.y] {
+                    continue
+                }
+                pacific[nx][ny] = true
+                next = append(next, pair{nx, ny})
+            }
+        }
+        plevel = next
+    }
+
+    for len(alevel) != 0 {
+        next := make([]pair, 0, m*n)
+        for _, p := range alevel {
+            for i := 0; i < 4; i++ {
+                nx, ny := p.x+dirs[i], p.y+dirs[i+1]
+                if nx < 0 || nx >= m || ny < 0 || ny >= n || atlantic[nx][ny] || matrix[nx][ny] < matrix[p.x][p.y] {
+                    continue
+                }
+                atlantic[nx][ny] = true
+                next = append(next, pair{nx, ny})
+            }
+        }
+        alevel = next
+    }
+
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if pacific[i][j] && atlantic[i][j] {
+                res = append(res, []int{i, j})
+            }
+        }
+    }
+
+    return res
+}
--- 417_pacific_atlantic_water_flow/pawf_test.go
+++ 417_pacific_atlantic_water_flow/pawf_test.go
@@ -25,3 +25,24 @@ func TestWaterFlow(t *testing.T) {
         })
     }
 }
+
+func TestWaterFlowBfs(t *testing.T) {
+    st := []struct {
+        name    string
+        maxtrix [][]int
+        exp     [][]int
+    }{
+        {"empty matrix", [][]int{}, [][]int{}},
+        {"testcase1", [][]int{[]int{1, 2, 2, 3, 5}, []int{3, 2, 3, 4, 4}, []int{2, 4, 5, 3, 1}, []int{6, 7, 1, 4, 5}, []int{5, 1, 1, 2, 4}},
+            [][]int{[]int{0, 4}, []int{1, 3}, []int{1, 4}, []int{2, 2}, []int{3, 0}, []int{3, 1}, []int{4, 0}}},
+    }
+    for _, tt := range st {
+        t.Run(tt.name, func(t *testing.T) {
+            out := bfs(tt.maxtrix)
+            if !reflect.DeepEqual(tt.exp, out) {
+                t.Fatalf("with matrix: %v wanted %v but got %v", tt.maxtrix, tt.exp, out)
+            }
+            t.Log("pass")
+        })
+    }
+}
