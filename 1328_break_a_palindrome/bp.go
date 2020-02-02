1449da1c8dc738e742aa91b1e3c5bf1f61b159be
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-02 12:07:29 +0800

    solve 1328 using brute force

--- 1328_break_a_palindrome/bp.go
+++ 1328_break_a_palindrome/bp.go
@@ -1,5 +1,42 @@
 package bp
 
 func breakPalindrome(s string) string {
-	return ""
+	return bruteForce(s)
+}
+
+// bruteForce time complexity O(N^2)
+func bruteForce(s string) string {
+	n := len(s)
+	var res string
+	if n == 1 {
+		return res
+	}
+	for i := range s {
+		for c := 'a'; c <= 'z'; c++ {
+			sb := []byte(s)
+			if sb[i] != byte(c) {
+				sb[i] = byte(c)
+				tmpsb := string(sb)
+				if !isPalindrome(tmpsb) {
+					if res == "" {
+						res = tmpsb
+					} else {
+						if tmpsb < res {
+							res = tmpsb
+						}
+					}
+				}
+			}
+		}
+	}
+	return res
+}
+
+func isPalindrome(s string) bool {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
 }
