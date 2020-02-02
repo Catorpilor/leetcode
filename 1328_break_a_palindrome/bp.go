e4cb9f100fc9275173d2ed4301d984a5711729ea
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-02 11:50:56 +0800

    add testcases for 1328

---
+++ 1328_break_a_palindrome/bp.go
@@ -0,0 +1,5 @@
+package bp
+
+func breakPalindrome(s string) string {
+	return ""
+}
---
+++ 1328_break_a_palindrome/bp_test.go
@@ -0,0 +1,23 @@
+package bp
+
+import "testing"
+
+func TestBreakPalindrome(t *testing.T) {
+	st := []struct {
+		name, s, exp string
+	}{
+		{"single character", "a", ""},
+		{"aa", "aa", "ab"},
+		{"aba", "aba", "abb"},
+		{"testcase1", "abccba", "aaccba"},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := breakPalindrome(tt.s)
+			if out != tt.exp {
+				t.Fatalf("with s:%s wanted %s but got %s", tt.s, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
