71c5056d8b343616f75d3c99c87ac99db8d7fa90
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-03 11:24:48 +0800

    solve 64 using dp with less space

--- 64_min_path_sum/mps.go
+++ 64_min_path_sum/mps.go
@@ -1,9 +1,14 @@
 package mps
 
-import "github.com/catorpilor/leetcode/utils"
+import (
+	"fmt"
+
+	"github.com/catorpilor/leetcode/utils"
+)
 
 func minPathSum(grid [][]int) int {
-	return useDp(grid)
+	// return useDp(grid)
+	return useDpWithLessSpace(grid)
 }
 
 // useDp time complexity O(MN), space complexity O(MN)
@@ -36,4 +41,30 @@ func useDp(grid [][]int) int {
 		}
 	}
 	return dp[m-1][n-1]
+}
+
+// useDpWithLessSpace time complexity O(MN), space complexity O(N)
+func useDpWithLessSpace(grid [][]int) int {
+	m := len(grid)
+	if m < 1 {
+		return 0
+	}
+	n := len(grid[0])
+	if n < 1 {
+		return 0
+	}
+	dp := make([]int, n)
+	dp[0] = grid[0][0]
+	for j := 1; j < n; j++ {
+		dp[j] = dp[j-1] + grid[0][j]
+	}
+	for i := 1; i < m; i++ {
+		dp[0] += grid[i][0]
+		for j := 1; j < n; j++ {
+			up, left := dp[j], dp[j-1]
+			dp[j] = utils.Min(up+grid[i][j], left+grid[i][j])
+		}
+		fmt.Printf("row:%d, dp:%v\n", i, dp)
+	}
+	return dp[n-1]
 }
