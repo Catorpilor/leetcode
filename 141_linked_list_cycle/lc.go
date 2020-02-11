ecf95807412ecc5e220867e886d452c06171567b
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-11 23:42:04 +0800

    solve 141 using two pointers

--- 141_linked_list_cycle/lc.go
+++ 141_linked_list_cycle/lc.go
@@ -3,5 +3,25 @@ package lc
 import "github.com/catorpilor/leetcode/utils"
 
 func hasCycle(head *utils.ListNode) bool {
+	return twoPointers(head)
+}
+
+// twoPointers time complexity O(N), space complexity O(1)
+func twoPointers(head *utils.ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow := head
+	fast := slow.Next
+	for slow != nil && fast != nil {
+		if slow == fast {
+			return true
+		}
+		slow = slow.Next
+		fast = fast.Next
+		if fast.Next != nil {
+			fast = fast.Next
+		}
+	}
 	return false
 }
