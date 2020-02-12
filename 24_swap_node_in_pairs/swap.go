22ab32bd2ae747c078ace9c896b272d3a0c9a2ba
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-12 12:09:38 +0800

    solve 24 by swaping the nodes

--- 24_swap_node_in_pairs/swap.go
+++ 24_swap_node_in_pairs/swap.go
@@ -3,6 +3,11 @@ package swap
 import "github.com/catorpilor/leetcode/utils"
 
 func swapPairs(head *utils.ListNode) *utils.ListNode {
+	return swapNodes(head)
+}
+
+// swapByValue just swap values instead, which is not allowed
+func swapByValue(head *utils.ListNode) *utils.ListNode {
 	if head == nil {
 		return nil
 	}
@@ -25,4 +30,22 @@ func swapPairs(head *utils.ListNode) *utils.ListNode {
 	}
 
 	return head
+}
+
+// swapNodes time complexity O(N), space complexity O(1)
+func swapNodes(head *utils.ListNode) *utils.ListNode {
+	if head == nil {
+		return head
+	}
+	dummy := &utils.ListNode{Next: head}
+	prev := dummy
+	for head != nil && head.Next != nil {
+		first, second := head, head.Next
+		head = second.Next
+		first.Next = head
+		prev.Next = second
+		second.Next = first
+		prev = first
+	}
+	return dummy.Next
 }
