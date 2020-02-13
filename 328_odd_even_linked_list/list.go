a6d14f7c18a9ab5e8073935d061cf8b1bd10426d
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-13 20:12:19 +0800

    solve 328 using dummy head

--- 328_odd_even_linked_list/list.go
+++ 328_odd_even_linked_list/list.go
@@ -4,6 +4,29 @@ import (
 	"github.com/catorpilor/leetcode/utils"
 )
 
-func oddEvenList(head *utils.ListNode) *utils.ListNode{
-	return head
+func oddEvenList(head *utils.ListNode) *utils.ListNode {
+	return iterator(head)
+}
+
+// iterator iterates the list, time complexity O(N), space complexity O(1)
+func iterator(head *utils.ListNode) *utils.ListNode {
+	if head == nil {
+		return head
+	}
+	dummy := &utils.ListNode{Next: head}
+	oddHead, evenHead := head, head.Next
+	cur := oddHead
+	// head point to evenHead
+	head = head.Next
+	for cur != nil && head != nil {
+		cur.Next = head.Next
+		if cur.Next == nil {
+			break
+		}
+		cur = cur.Next
+		head.Next = cur.Next
+		head = head.Next
+	}
+	cur.Next = evenHead
+	return dummy.Next
 }
