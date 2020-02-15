50839be44ec2186b0ba2d98162f25a32b4d2d46f
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-15 19:15:26 +0800

    solve 445 use array, without modify the original list

--- 445_add_two_numbers_ii/add.go
+++ 445_add_two_numbers_ii/add.go
@@ -55,3 +55,47 @@ func reverse(head *utils.ListNode) *utils.ListNode {
 	dummy.Next = pre
 	return dummy.Next
 }
+
+// useStack time complexity O(M+N), space complexity O(M+N)
+func useStack(l1, l2 *utils.ListNode) *utils.ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	a1, a2 := make([]int, 0, 65536), make([]int, 0, 65536)
+	for l1 != nil {
+		a1 = append(a1, l1.Val)
+		l1 = l1.Next
+	}
+	for l2 != nil {
+		a2 = append(a2, l2.Val)
+		l2 = l2.Next
+	}
+	n1, n2 := len(a1), len(a2)
+	sum := 0
+	dummy := &utils.ListNode{}
+	pre := dummy.Next
+	for n1 != 0 || n2 != 0 || sum != 0 {
+		if n1 > 0 {
+			sum += a1[n1-1]
+			a1 = a1[:n1-1]
+		}
+		if n2 > 0 {
+			sum += a2[n2-1]
+			a2 = a2[:n2-1]
+		}
+		node := &utils.ListNode{Val: sum % 10, Next: pre}
+		pre = node
+		n1, n2 = len(a1), len(a2)
+		sum /= 10
+	}
+	// skip 0 at the highest bit
+	// if pre.Val == 0 {
+	// 	pre = pre.Next
+	// }
+	dummy.Next = pre
+	return dummy.Next
+
+}
--- 445_add_two_numbers_ii/add_test.go
+++ 445_add_two_numbers_ii/add_test.go
@@ -45,3 +45,27 @@ func TestReverse(t *testing.T) {
 		})
 	}
 }
+
+func TestUseStack(t *testing.T) {
+	st := []struct {
+		name        string
+		l1, l2, exp *utils.ListNode
+	}{
+		{"both empty", nil, nil, nil},
+		{"l1 is empty", nil, utils.ConstructFromSlice([]int{1, 2, 3}), utils.ConstructFromSlice([]int{1, 2, 3})},
+		{"testcase1", utils.ConstructFromSlice([]int{7, 2, 4, 3}), utils.ConstructFromSlice([]int{5, 6, 4}), utils.ConstructFromSlice([]int{7, 8, 0, 7})},
+		{"testcase3", utils.ConstructFromSlice([]int{0, 2, 4, 3}), utils.ConstructFromSlice([]int{5, 6, 4}), utils.ConstructFromSlice([]int{8, 0, 7})},
+		{"testcase2", utils.ConstructFromSlice([]int{9, 9, 9, 9, 9}), utils.ConstructFromSlice([]int{1}), utils.ConstructFromSlice([]int{1, 0, 0, 0, 0, 0})},
+		{"failed1", utils.ConstructFromSlice([]int{0}), utils.ConstructFromSlice([]int{0}), utils.ConstructFromSlice([]int{0})},
+		{"failed2", utils.ConstructFromSlice([]int{0, 0, 1}), utils.ConstructFromSlice([]int{1}), utils.ConstructFromSlice([]int{2})},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useStack(tt.l1, tt.l2)
+			if !utils.IsEqualList(out, tt.exp) {
+				t.Fatalf("with input l1: %s and l2: %s wanted %s but got %s", tt.l1, tt.l2, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
