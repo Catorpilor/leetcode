4c3e6eff46639017a0f5063d9aebfedb6a04adfe
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-15 19:51:33 +0800

    add testcases for 143

---
+++ 143_reorder_list/reorder.go
@@ -0,0 +1,7 @@
+package reorder
+
+import "github.com/catorpilor/leetcode/utils"
+
+func reorderList(head *utils.ListNode) *utils.ListNode {
+	return head
+}
---
+++ 143_reorder_list/reorder_test.go
@@ -0,0 +1,29 @@
+package reorder
+
+import (
+	"testing"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
+func TestReorderList(t *testing.T) {
+	st := []struct {
+		name      string
+		head, exp *utils.ListNode
+	}{
+		{"empty list", nil, nil},
+		{"single node", utils.ConstructFromSlice([]int{1}), utils.ConstructFromSlice([]int{1})},
+		{"two nodes", utils.ConstructFromSlice([]int{1, 3}), utils.ConstructFromSlice([]int{1, 3})},
+		{"testcase1", utils.ConstructFromSlice([]int{1, 2, 3, 4}), utils.ConstructFromSlice([]int{1, 4, 2, 3})},
+		{"testcase2", utils.ConstructFromSlice([]int{1, 2, 3, 4, 5}), utils.ConstructFromSlice([]int{1, 5, 2, 4, 3})},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := reorderList(tt.head)
+			if !utils.IsEqualList(tt.exp, out) {
+				t.Fatalf("with input list: %s wanted %s but got %s", tt.head, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
