0331b01d4436edda5f3b6dbc94bb47912abcef26
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-17 12:15:30 +0800

    solve 342 using precomputing

--- 342_is_power_of_four/pf.go
+++ 342_is_power_of_four/pf.go
@@ -1,9 +1,11 @@
 package pf
 
 func isPowerOfFour(num int) bool {
-	return useLoops(num)
+	// return useLoops(num)
+	return preCompute(num)
 }
 
+// useLoops time complexity O(lgN), space complexity O(1)
 func useLoops(num int) bool {
 	if num <= 0 {
 		return false
@@ -15,4 +17,24 @@ func useLoops(num int) bool {
 		num >>= 2
 	}
 	return true
+}
+
+var results map[int]bool
+
+func init() {
+	if results == nil {
+		results = make(map[int]bool)
+	}
+	// since num <= 2^31 - 1, so the max power of 4 is 4^15
+	results[1] = true
+	base := 1
+	for i := 1; i <= 15; i++ {
+		base *= 4
+		results[base] = true
+	}
+}
+
+// preCompute time complexity O(1), space complexity O(1)
+func preCompute(num int) bool {
+	return results[num]
 }
