09f66a3fac85c648d3bb32bbbf35713efd297f3a
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-17 12:25:58 +0800

    solve 342 using bit manipulation

--- 342_is_power_of_four/pf.go
+++ 342_is_power_of_four/pf.go
@@ -2,7 +2,8 @@ package pf
 
 func isPowerOfFour(num int) bool {
 	// return useLoops(num)
-	return preCompute(num)
+	// return preCompute(num)
+	return useBit(num)
 }
 
 // useLoops time complexity O(lgN), space complexity O(1)
@@ -37,4 +38,20 @@ func init() {
 // preCompute time complexity O(1), space complexity O(1)
 func preCompute(num int) bool {
 	return results[num]
+}
+
+// useBit time complexity O(1) space complexity O(1)
+func useBit(num int) bool {
+	// is power of two is (num > 0) && (num & (num-1) == 0 )
+	// we have to eliminate that is power of two but not power of four
+	// num = 1 	0000,0001  1-bit at 0
+	// num = 4  0000,0100  1-bit at 2
+	// num = 16 0001,0000  1-bit at 4
+	// num = 64 0100,0000  1-bit at 6
+	// num = 2  0000,0010  1-bit at 1
+	// num = 8  0000,1000  1-bit at 3
+	// num = 32 0010,0000  1-bit at 5
+	// num =128 1000,0000  1-bit at 7
+	// so that (num & (0xaaaaaaaa) == 0 )
+	return (num > 0) && ((num & (num - 1)) == 0) && ((num & 0xaaaaaaaa) == 0)
 }
