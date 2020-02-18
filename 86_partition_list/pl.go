15383181fb1e9bbafa5cccde64120933f81fc4ce
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-18 14:56:18 +0800

    add testcases for 86

---
+++ 86_partition_list/pl.go
@@ -0,0 +1,7 @@
+package pl
+
+import "github.com/catorpilor/leetcode/utils"
+
+func partition(head *utils.ListNode, x int) *utils.ListNode {
+	return head
+}
---
+++ 86_partition_list/pl_test.go
@@ -0,0 +1,29 @@
+package pl
+
+import (
+	"testing"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
+func TestPartition(t *testing.T) {
+	st := []struct {
+		name      string
+		head, exp *utils.ListNode
+		x         int
+	}{
+		{"empty list", nil, 3, nil},
+		{"single node", utils.ConstructFromSlice([]int{1}), 2, utils.ConstructFromSlice([]int{1})},
+		{"all identical", utils.ConstructFromSlice([]int{2, 2, 2, 2, 2}), 3, utils.ConstructFromSlice([]int{2, 2, 2, 2, 2})},
+		{"testcase1", utils.ConstructFromSlice([]int{1, 4, 3, 2, 5, 2}), 3, utils.ConstructFromSlice([]int{1, 2, 2, 4, 3, 5})},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := partition(tt.head, tt.x)
+			if !utils.IsEqualList(tt.exp, out) {
+				t.Fatalf("with input list: %s and x: %d wanted %s but got %s", tt.head, tt.x, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
