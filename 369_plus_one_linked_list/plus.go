36348f1bee5394b755dc2b4b40878d6170e30235
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-19 12:43:39 +0800

    solve 369 by adding a dummy node

--- 369_plus_one_linked_list/plus.go
+++ 369_plus_one_linked_list/plus.go
@@ -6,6 +6,7 @@ func plusOne(head *utils.ListNode) *utils.ListNode {
 	return useReverse(head)
 }
 
+// useReverse time complexity O(N), space complexity O(1)
 func useReverse(head *utils.ListNode) *utils.ListNode {
 	if head == nil {
 		return head
@@ -28,5 +29,32 @@ func useReverse(head *utils.ListNode) *utils.ListNode {
 		cur.Next = &utils.ListNode{Val: 1}
 	}
 	dummy.Next = utils.ReverseList(dummy.Next)
+	return dummy.Next
+}
+
+// useDummy time complexity O(N), space complexit O(1)
+func useDummy(head *utils.ListNode) *utils.ListNode {
+	if head == nil {
+		return head
+	}
+	dummy := &utils.ListNode{Next: head}
+	notNine := dummy
+	// find the rightmost not 9 node
+	for head != nil {
+		if head.Val != 9 {
+			notNine = head
+		}
+		head = head.Next
+	}
+	notNine.Val++
+	notNine = notNine.Next
+	// update the following 9 to 0
+	for notNine != nil {
+		notNine.Val = 0
+		notNine = notNine.Next
+	}
+	if dummy.Val != 0 {
+		return dummy
+	}
 	return dummy.Next
 }
--- 369_plus_one_linked_list/plus_test.go
+++ 369_plus_one_linked_list/plus_test.go
@@ -26,3 +26,24 @@ func TestPlusOne(t *testing.T) {
 		})
 	}
 }
+
+func TestPlusOneUseDummy(t *testing.T) {
+	st := []struct {
+		name      string
+		head, exp *utils.ListNode
+	}{
+		{"head is nil", nil, nil},
+		{"single node val < 9", utils.ConstructFromSlice([]int{3}), utils.ConstructFromSlice([]int{4})},
+		{"single node val=9", utils.ConstructFromSlice([]int{9}), utils.ConstructFromSlice([]int{1, 0})},
+		{"testcase1", utils.ConstructFromSlice([]int{1, 2, 3}), utils.ConstructFromSlice([]int{1, 2, 4})},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useDummy(tt.head)
+			if !utils.IsEqualList(tt.exp, out) {
+				t.Fatalf("with input list:%s wanted %s but got %s", tt.head, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
