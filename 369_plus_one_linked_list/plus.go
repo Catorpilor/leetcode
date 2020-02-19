f3a07f47714fe5fb38a2d95a56874b554d78d498
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-19 12:19:29 +0800

    add testcases for 369

---
+++ 369_plus_one_linked_list/plus.go
@@ -0,0 +1,7 @@
+package plus
+
+import "github.com/catorpilor/leetcode/utils"
+
+func plusOne(head *utils.ListNode) *utils.ListNode {
+	return head
+}
---
+++ 369_plus_one_linked_list/plus_test.go
@@ -0,0 +1,28 @@
+package plus
+
+import (
+	"testing"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
+func TestPlusOne(t *testing.T) {
+	st := []struct {
+		name      string
+		head, exp *utils.ListNode
+	}{
+		{"head is nil", nil, nil},
+		{"single node val < 9", utils.ConstructFromSlice([]int{3}), utils.ConstructFromSlice([]int{4})},
+		{"single node val=9", utils.ConstructFromSlice([]int{9}), utils.ConstructFromSlice([]int{1, 0})},
+		{"testcase1", utils.ConstructFromSlice([]int{1, 2, 3}), utils.ConstructFromSlice([]int{1, 2, 4})},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := plusOne(tt.head)
+			if !utils.IsEqualList(tt.exp, out) {
+				t.Fatalf("with input list:%s wanted %s but got %s", tt.head, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
