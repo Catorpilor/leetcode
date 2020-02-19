4ae795eb9c5b20098a47ce048ad285818a9c6fbd
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-19 12:34:51 +0800

    solve 369 by reverse the list twice

--- 369_plus_one_linked_list/plus.go
+++ 369_plus_one_linked_list/plus.go
@@ -3,5 +3,30 @@ package plus
 import "github.com/catorpilor/leetcode/utils"
 
 func plusOne(head *utils.ListNode) *utils.ListNode {
-	return head
+	return useReverse(head)
+}
+
+func useReverse(head *utils.ListNode) *utils.ListNode {
+	if head == nil {
+		return head
+	}
+	head = utils.ReverseList(head)
+	dummy := &utils.ListNode{Next: head}
+	cur := dummy
+	flag := false
+	for cur.Next != nil {
+		cur = cur.Next
+		if cur.Val < 9 {
+			cur.Val++
+			flag = false
+			break
+		}
+		cur.Val = 0
+		flag = true
+	}
+	if flag {
+		cur.Next = &utils.ListNode{Val: 1}
+	}
+	dummy.Next = utils.ReverseList(dummy.Next)
+	return dummy.Next
 }
