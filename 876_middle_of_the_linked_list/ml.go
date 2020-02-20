6aefb8243572176a66cc2f36684dd5bdfcca64c0
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-20 14:04:04 +0800

    solve 876 using slow&fast pointers

--- 876_middle_of_the_linked_list/ml.go
+++ 876_middle_of_the_linked_list/ml.go
@@ -3,5 +3,20 @@ package ml
 import "github.com/catorpilor/leetcode/utils"
 
 func middleNode(head *utils.ListNode) *utils.ListNode {
-	return head
+	return twoPointers(head)
+}
+
+func twoPointers(head *utils.ListNode) *utils.ListNode {
+	if head == nil {
+		return head
+	}
+	slow, fast := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	if fast.Next != nil {
+		return slow.Next
+	}
+	return slow
 }
