6e8a844016bb01006f71a0f077f296b985247ed3
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-20 14:15:33 +0800

    add testcases for 61

---
+++ 61_rotate_list/rl.go
@@ -0,0 +1,7 @@
+package rl
+
+import "github.com/catorpilor/leetcode/utils"
+
+func rotateRight(head *utils.ListNode, k int) *utils.ListNode {
+	return head
+}
---
+++ 61_rotate_list/rl_test.go
@@ -0,0 +1,31 @@
+package rl
+
+import (
+	"testing"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
+func TestRotateRight(t *testing.T) {
+	st := []struct {
+		name string
+		head *utils.ListNode
+		k    int
+		exp  *utils.ListNode
+	}{
+		{"empty list", nil, 4, nil},
+		{"single node", utils.ConstructFromSlice([]int{1}), 4, utils.ConstructFromSlice([]int{1})},
+		{"testcase1", utils.ConstructFromSlice([]int{1, 2, 3, 4, 5}), 2, utils.ConstructFromSlice([]int{4, 5, 1, 2, 3})},
+		{"testcase2", utils.ConstructFromSlice([]int{0, 1, 2}), 4, utils.ConstructFromSlice([]int{2, 0, 1})},
+	}
+
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := rotateRight(tt.head, tt.k)
+			if !utils.IsEqualList(tt.exp, out) {
+				t.Fatalf("with input list: %s and k: %d wanted %s but got %s", tt.head, tt.k, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
