4e229100ec7c2e2dc5acb05f3711e7d331f687c8
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-21 15:25:27 +0800

    solve 1171 using preSum and hashmap

--- 1171_remove_zero_sum_consecutive_nodes_from_linked_list/zerosum.go
+++ 1171_remove_zero_sum_consecutive_nodes_from_linked_list/zerosum.go
@@ -3,5 +3,34 @@ package zerosum
 import "github.com/catorpilor/leetcode/utils"
 
 func zeroSum(head *utils.ListNode) *utils.ListNode {
-	return head
+	return preSum(head)
+}
+
+// preSum time complexity O(N), space complexity O(N)
+func preSum(head *utils.ListNode) *utils.ListNode {
+	if head == nil {
+		return head
+	}
+	dummy := &utils.ListNode{Next: head}
+	cur := dummy
+	pmap := make(map[int]*utils.ListNode)
+	prefix := 0
+	for cur != nil {
+		prefix += cur.Val
+		if node, exists := pmap[prefix]; exists {
+			beg := node.Next
+			p := prefix + beg.Val
+			for p != prefix {
+				// cause we are going to delete this node, pmap has to be updated
+				delete(pmap, p)
+				beg = beg.Next
+				p += beg.Val
+			}
+			node.Next = cur.Next
+		} else {
+			pmap[prefix] = cur
+		}
+		cur = cur.Next
+	}
+	return dummy.Next
 }
--- 1171_remove_zero_sum_consecutive_nodes_from_linked_list/zerosum_test.go
+++ 1171_remove_zero_sum_consecutive_nodes_from_linked_list/zerosum_test.go
@@ -17,6 +17,7 @@ func TestZeroSum(t *testing.T) {
 		{"testcase1", utils.ConstructFromSlice([]int{1, 2, -3, 3, 1}), utils.ConstructFromSlice([]int{3, 1})},
 		{"testcase2", utils.ConstructFromSlice([]int{1, 2, 3, -3, 4}), utils.ConstructFromSlice([]int{1, 2, 4})},
 		{"testcase3", utils.ConstructFromSlice([]int{1, 2, 3, -3, -2}), utils.ConstructFromSlice([]int{1})},
+		{"testcase4", utils.ConstructFromSlice([]int{1, 3, 2, -3, -2, 5, 100, -100, 1}), utils.ConstructFromSlice([]int{1, 5, 1})},
 	}
 	for _, tt := range st {
 		t.Run(tt.name, func(t *testing.T) {
