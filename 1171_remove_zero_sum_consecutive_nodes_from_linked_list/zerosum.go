09e6e6e0345843dca02a8530323bb6bb3ee788f1
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-21 15:41:24 +0800

    solve 1171 by two passes preSum

--- 1171_remove_zero_sum_consecutive_nodes_from_linked_list/zerosum.go
+++ 1171_remove_zero_sum_consecutive_nodes_from_linked_list/zerosum.go
@@ -6,7 +6,7 @@ func zeroSum(head *utils.ListNode) *utils.ListNode {
 	return preSum(head)
 }
 
-// preSum time complexity O(N), space complexity O(N)
+// preSum one pass, time complexity O(N), space complexity O(N)
 func preSum(head *utils.ListNode) *utils.ListNode {
 	if head == nil {
 		return head
@@ -31,6 +31,29 @@ func preSum(head *utils.ListNode) *utils.ListNode {
 			pmap[prefix] = cur
 		}
 		cur = cur.Next
+	}
+	return dummy.Next
+}
+
+// pprefixSum two passes, time complexity O(N), space complexity O(N)
+func prefixSum(head *utils.ListNode) *utils.ListNode {
+	if head == nil {
+		return head
+	}
+	dummy := &utils.ListNode{Next: head}
+	preMap := make(map[int]*utils.ListNode)
+	preMap[0] = dummy
+	pSum := 0
+	// calculate the preSum, and update preMap to the latest node
+	for c := dummy; c != nil; c = c.Next {
+		pSum += c.Val
+		preMap[pSum] = c
+	}
+	pSum = 0
+	// iterator again, calculate the preSum and directly skip to the last occurrence of that pSum
+	for c := dummy; c != nil; c = c.Next {
+		pSum += c.Val
+		c.Next = preMap[pSum].Next
 	}
 	return dummy.Next
 }
--- 1171_remove_zero_sum_consecutive_nodes_from_linked_list/zerosum_test.go
+++ 1171_remove_zero_sum_consecutive_nodes_from_linked_list/zerosum_test.go
@@ -29,3 +29,26 @@ func TestZeroSum(t *testing.T) {
 		})
 	}
 }
+func TestZeroSumTwoPass(t *testing.T) {
+	st := []struct {
+		name      string
+		head, exp *utils.ListNode
+	}{
+		{"empty list", nil, nil},
+		{"single node not eq 0", utils.ConstructFromSlice([]int{1}), utils.ConstructFromSlice([]int{1})},
+		{"single node eq 0", utils.ConstructFromSlice([]int{0}), nil},
+		{"testcase1", utils.ConstructFromSlice([]int{1, 2, -3, 3, 1}), utils.ConstructFromSlice([]int{3, 1})},
+		{"testcase2", utils.ConstructFromSlice([]int{1, 2, 3, -3, 4}), utils.ConstructFromSlice([]int{1, 2, 4})},
+		{"testcase3", utils.ConstructFromSlice([]int{1, 2, 3, -3, -2}), utils.ConstructFromSlice([]int{1})},
+		{"testcase4", utils.ConstructFromSlice([]int{1, 3, 2, -3, -2, 5, 100, -100, 1}), utils.ConstructFromSlice([]int{1, 5, 1})},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := prefixSum(tt.head)
+			if !utils.IsEqualList(tt.exp, out) {
+				t.Fatalf("with input list: %s wanted %s but got %s", tt.head, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
