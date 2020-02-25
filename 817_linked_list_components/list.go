bb1a2cd3055888a5ccd981a4b65ee7e9a24bd4d3
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-25 17:39:50 +0800

    solve 817 based on dicussion

--- 817_linked_list_components/list.go
+++ 817_linked_list_components/list.go
@@ -3,5 +3,21 @@ package list
 import "github.com/catorpilor/leetcode/utils"
 
 func numComps(head *utils.ListNode, g []int) int {
-	return 0
+	return iterator(head, g)
+}
+
+// iterator time complexity O(N+len(g)), space compleixty O(len(g))
+func iterator(head *utils.ListNode, g []int) int {
+	gset := make(map[int]bool, len(g))
+	for i := range g {
+		gset[g[i]] = true
+	}
+	var ret int
+	for head != nil {
+		if gset[head.Val] && (head.Next == nil || !gset[head.Next.Val]) {
+			ret++
+		}
+		head = head.Next
+	}
+	return ret
 }
