74bd71a521412d0641c0276c2879fe12c26c2dba
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-27 22:06:27 +0800

    solve 100 use dfs

--- 100_same_tree/sametree.go
+++ 100_same_tree/sametree.go
@@ -3,5 +3,13 @@ package sametree
 import "github.com/catorpilor/leetcode/utils"
 
 func isSameTree(p, q *utils.TreeNode) bool {
+	return dfs(p, q)
+}
+
+// dfs time complexity O(N), space complexity O(lgN)
+func dfs(p, q *utils.TreeNode) bool {
+	if (p == nil && q == nil) || (p != nil && q != nil && p.Val == q.Val && dfs(p.Left, q.Left) && dfs(p.Right, q.Right)) {
+		return true
+	}
 	return false
 }
