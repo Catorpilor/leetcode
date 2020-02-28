c84edc14074af9eee10e2ff9069fdfb164cd6a1b
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-28 20:25:08 +0800

    solve 104 using recursive dfs

--- 104_max_depth_of_binary_tree/maxd.go
+++ 104_max_depth_of_binary_tree/maxd.go
@@ -3,5 +3,15 @@ package maxd
 import "github.com/catorpilor/leetcode/utils"
 
 func maxDepth(root *utils.TreeNode) int {
-	return 0
+	return useDfs(root)
+}
+
+// useDfs time complexity O(N), space complexity O(lgN)
+func useDfs(root *utils.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := useDfs(root.Left)
+	r := useDfs(root.Right)
+	return 1 + utils.Max(l, r)
 }
