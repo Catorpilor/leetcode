458e3a6fa4118085b5c954b40beeeea197925c62
Catorpilor <jiaojiangonghitster@gmail.com>
2020-02-29 21:55:12 +0800

    solve 34 using lowerBound

--- 34_find_first_last_postion_of_element_in_sorted_array/pos.go
+++ 34_find_first_last_postion_of_element_in_sorted_array/pos.go
@@ -1,5 +1,25 @@
 package pos
 
 func searchRange(arr []int, target int) []int {
-	return nil
+	l := lowerBound(arr, target)
+	r := lowerBound(arr, target+1)
+	if l < r {
+		return []int{l, r - 1}
+	}
+	return []int{-1, -1}
+}
+
+// lowberBound time complexity O(lgN), space complexity O(1)
+func lowerBound(arr []int, target int) int {
+	n := len(arr)
+	low, hi := 0, n-1
+	for low <= hi {
+		mid := low + (hi-low)>>1
+		if arr[mid] >= target {
+			hi = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return low
 }
