130fb8d90846fc29d0ba1498c904edf477d3ce3d
Catorpilor <jiaojiangonghitster@gmail.com>
2020-03-09 22:48:25 +0800

    solve 105 using dfs

--- 105_construct_binary_tree_from_preorder_and_inorder_traversal/tree.go
+++ 105_construct_binary_tree_from_preorder_and_inorder_traversal/tree.go
@@ -3,5 +3,28 @@ package tree
 import "github.com/catorpilor/leetcode/utils"
 
 func buildTree(preorder, inorder []int) *utils.TreeNode {
-	return nil
+	return useDFS(preorder, inorder)
+}
+
+// useDFS time complexity O(N) space complexity  O(1)
+func useDFS(preorder, inorder []int) *utils.TreeNode {
+	np, ni := len(preorder), len(inorder)
+	if np < 1 && ni < 1 {
+		return nil
+	}
+	val := preorder[0] // root val
+	root := &utils.TreeNode{Val: val}
+	if np == 1 && ni == 1 {
+		return root
+	}
+	var pos int // find root's pos in inorder array
+	for pos < ni {
+		if inorder[pos] == val {
+			break
+		}
+		pos++
+	}
+	root.Left = useDFS(preorder[1:1+pos], inorder[:pos])
+	root.Right = useDFS(preorder[1+pos:], inorder[pos+1:])
+	return root
 }
