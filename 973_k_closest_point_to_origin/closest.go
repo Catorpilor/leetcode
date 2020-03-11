fdc5fa325b911e35c7b771abfe761b330fd1fd1d
Catorpilor <jiaojiangonghitster@gmail.com>
2020-03-11 22:13:36 +0800

    add testcases for 973

---
+++ 973_k_closest_point_to_origin/closest.go
@@ -0,0 +1,5 @@
+package closest
+
+func kClosest(points [][]int, k int) [][]int {
+	return points
+}
---
+++ 973_k_closest_point_to_origin/closest_test.go
@@ -0,0 +1,28 @@
+package closest
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestKClosest(t *testing.T) {
+	st := []struct {
+		name   string
+		points [][]int
+		k      int
+		exp    [][]int
+	}{
+		{"just one point", [][]int{[]int{3, 4}}, 1, [][]int{[]int{3, 4}}},
+		{"testcase1", [][]int{[]int{1, 3}, []int{-2, 2}}, 1, [][]int{[]int{-2, 2}}},
+		{"testcase2", [][]int{[]int{3, 3}, []int{5, -1}, []int{-2, 4}}, 2, [][]int{[]int{-2, 4}, []int{3, 3}}},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := kClosest(tt.points, tt.k)
+			if !reflect.DeepEqual(tt.exp, out) {
+				t.Fatalf("with input points: %v and k:%d wanted %v but got %v", tt.points, tt.k, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
