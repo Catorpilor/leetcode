2f03afb45c181bbcf1f2807485475b8723c58a16
Catorpilor <jiaojiangonghitster@gmail.com>
2020-03-15 12:34:16 +0800

    solve 97 by recursion

--- 97_interleaving_string/interleave.go
+++ 97_interleaving_string/interleave.go
@@ -1,5 +1,27 @@
 package interleave
 
 func isInterLeaving(s1, s2, s3 string) bool {
-	return false
+	return useRecursion(s1, s2, s3, 0, 0, 0)
+}
+
+// useRecursion time complexity O(2^(M+N)) space complexity O(M+N)
+func useRecursion(s1, s2, s3 string, p1, p2, p3 int) bool {
+	n1, n2, n3 := len(s1), len(s2), len(s3)
+	if n1 + n2 != n3 {
+		return false
+	}
+	if p1 == n1 && p2 == n2 && p3 == n3 {
+		return true
+	}
+	// if p3 == n3 {
+	// 	return false
+	// }
+	var ret bool
+	if p1 < n1 && s1[p1] == s3[p3] {
+		ret = useRecursion(s1, s2, s3, p1+1, p2, p3+1) || ret
+	}
+	if p2 < n2 && s2[p2] == s3[p3] {
+		ret = useRecursion(s1, s2, s3, p1, p2+1, p3+1) || ret
+	}
+	return ret
 }
