95ec61100365f6f1fa40e6a941b6685d8fbe7d38
Catorpilor <jiaojiangonghitster@gmail.com>
2020-03-21 19:38:46 +0800

    solve numbers with two equal digit sum with straight forward traversal

--- oa/ms/numbers_with_two_equal_digit_sum/sum.go
+++ oa/ms/numbers_with_two_equal_digit_sum/sum.go
@@ -1,5 +1,48 @@
 package sum
 
+import (
+	"sort"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
 func digitSum(nums []int) int {
-	return -1
+	return useBruteForce(nums)
+}
+
+// useBruteForce time complexity O(K*N), space complexity O(N)
+func useBruteForce(nums []int) int {
+	n := len(nums)
+	if n <= 1 {
+		return -1
+	}
+	hset := make(map[int][]int, n)
+	for _, num := range nums {
+		ds := calculateSum(num)
+		hset[ds] = append(hset[ds], num)
+	}
+	ans := -1
+	for _, v := range hset {
+		nv := len(v)
+		if nv < 2 {
+			continue
+		} else if nv == 2 {
+			ans = utils.Max(ans, v[0]+v[1])
+		} else {
+			sort.Ints(v)
+			ans = utils.Max(ans, v[nv-2]+v[nv-1])
+		}
+	}
+	return ans
+
+}
+
+func calculateSum(num int) int {
+	var ans int
+	num = utils.Abs(num)
+	for num != 0 {
+		ans += num % 10
+		num /= 10
+	}
+	return ans
 }
