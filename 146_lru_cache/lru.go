c2dd65d026b7f77bb85ca883d4119282ca59b1cd
Catorpilor <jiaojiangonghitster@gmail.com>
2020-03-25 18:37:19 +0800

    fix moveFront when calling get to solve 146

--- 146_lru_cache/lru.go
+++ 146_lru_cache/lru.go
@@ -1,5 +1,10 @@
 package lru
 
+import (
+	"fmt"
+	"strings"
+)
+
 type DBNode struct {
 	Key, Val   int
 	Prev, Next *DBNode
@@ -25,41 +30,68 @@ func Constructor(d int) *LRUCache {
 
 func (cache *LRUCache) Get(key int) int {
 	if v, exists := cache.set[key]; exists {
+		// fmt.Printf("get key: %d, found\n", key)
 		cache.moveFront(v)
+		// fmt.Println(cache.String())
 		return v.Key
 	}
+	// fmt.Printf("get key: %d, not found\n", key)
+	// fmt.Println(cache.String())
 	return -1
 }
 
 func (cache *LRUCache) Put(key, val int) {
 	if old, exists := cache.set[key]; !exists {
+		if cache.n == cache.cap {
+			ct := cache.dummyTail.Prev
+			// fmt.Printf("befor delete: %s, prepare to delete node: %+v\n", cache.String(), ct)
+			tk := ct.Key
+			delete(cache.set, tk)
+			// fmt.Printf("deleting node: %p, prev(%p):%+v, next(%p):%+v\n", ct, ct.Prev, ct.Prev, ct.Next, ct.Next)
+			ct.Prev.Next = ct.Next
+			ct.Next.Prev = ct.Prev
+			cache.n--
+			// fmt.Println(cache.String())
+		}
 		oldNext := cache.dummyHead.Next
 		node := &DBNode{Val: val, Key: key, Prev: cache.dummyHead, Next: oldNext}
 		cache.dummyHead.Next = node
 		oldNext.Prev = node
-		if cache.n == cache.cap {
-			ct := cache.dummyTail.Prev
-			tk := ct.Key
-			delete(cache.set, tk)
-			ct.Prev.Next = ct.Next
-			ct.Next.Prev = ct.Prev
-			cache.n--
-		}
 		cache.n++
 		cache.set[key] = node
+		// fmt.Printf("insert key:%d, not found\n", key)
+		// fmt.Println(cache.String())
 	} else {
 		old.Val = val
 		cache.moveFront(old)
+		// fmt.Printf("insert key:%d, found\n", key)
+		// fmt.Println(cache.String())
 	}
+	// fmt.Println(cache.String())
 	return
 }
 
 func (cache *LRUCache) moveFront(node *DBNode) {
+	// fmt.Printf("before move node(%p):%+v, cache:%s", node, node, cache.String())
 	if node != cache.dummyHead.Next {
 		node.Prev.Next = node.Next
 		node.Next.Prev = node.Prev
 		node.Prev = cache.dummyHead
 		node.Next = cache.dummyHead.Next
+		cache.dummyHead.Next.Prev = node
 		cache.dummyHead.Next = node
 	}
+	// fmt.Printf("after move node(%p):%+v, cache:%s", node, node, cache.String())
+}
+
+func (cache *LRUCache) String() string {
+	var sb strings.Builder
+	fmt.Fprintf(&sb, "cap:%d, n:%d, set:%v, list:0=", cache.cap, cache.n, cache.set)
+	node := cache.dummyHead.Next
+	for node != cache.dummyTail {
+		fmt.Fprintf(&sb, "%d=", node.Val)
+		node = node.Next
+	}
+	sb.WriteString("0")
+	return sb.String()
 }
--- 146_lru_cache/lru_test.go
+++ 146_lru_cache/lru_test.go
@@ -24,27 +24,27 @@ func TestEvict(t *testing.T) {
 }
 
 func TestWithBunchCommands(t *testing.T) {
-	f := setup(3)
+	f := setup(2)
 	defer f()
 	obj.Put(1, 1)
+	obj.Put(2, 2)
 	if obj.Get(1) != 1 {
 		t.Fatalf("after put (1,1), get should return 1 but got:%d", obj.Get(1))
-	}
-	obj.Put(2, 2)
-	if obj.Get(2) != 2 {
-		t.Fatalf("after put (2,2), get should return 2 but got:%d", obj.Get(2))
 	}
 	obj.Put(3, 3)
-	if obj.Get(3) != 3 {
-		t.Fatalf("after put (3,3), get should return 3 but got:%d", obj.Get(3))
-	}
-	if obj.Get(1) != 1 {
-		t.Fatalf("after put (1,1), get should return 1 but got:%d", obj.Get(1))
+	if obj.Get(2) != -1 {
+		t.Fatalf("after put (3,3), get should return -1 but got:%d", obj.Get(2))
 	}
 	obj.Put(4, 4)
+	if obj.Get(1) != -1 {
+		t.Fatalf("after put (4,4), get should return -1 but got:%d", obj.Get(1))
+	}
 	// 2,2 is the oldest data now, should be evicted
-	if obj.Get(2) != -1 {
-		t.Fatalf("after put (4,4), get should return -1 but got:%d", obj.Get(2))
+	if obj.Get(3) != 3 {
+		t.Fatalf("call get(3), get should return 3 but got:%d", obj.Get(3))
+	}
+	if obj.Get(4) != 4 {
+		t.Fatalf("call get(4), get should return 4 but got:%d", obj.Get(4))
 	}
 	t.Log("pass")
 }
