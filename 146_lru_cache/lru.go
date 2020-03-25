1379c8c65e7317c8672b8ef6124f38e17aa79988
Catorpilor <jiaojiangonghitster@gmail.com>
2020-03-25 16:41:03 +0800

    add testcases for 146

---
+++ 146_lru_cache/lru.go
@@ -0,0 +1,15 @@
+package lru
+
+type LRUCache struct{}
+
+func Constructor(d int) *LRUCache {
+	return nil
+}
+
+func (cache *LRUCache) Get(key int) int {
+	return -1
+}
+
+func (cache *LRUCache) Put(key, val int) {
+	return
+}
---
+++ 146_lru_cache/lru_test.go
@@ -0,0 +1,50 @@
+package lru
+
+import "testing"
+
+var obj *LRUCache
+
+func setup(cap int) func() {
+	if obj == nil {
+		obj = Constructor(cap)
+	}
+	return func() { obj = nil }
+}
+
+func TestEvict(t *testing.T) {
+	f := setup(2)
+	defer f()
+	obj.Put(1, 1)
+	obj.Put(2, 2)
+	obj.Put(3, 3) // 1,1 should evicted
+	if obj.Get(1) != -1 {
+		t.Fatal("with cap(2) insert the 3rd element should evict the oldest one")
+	}
+	t.Log("pass")
+}
+
+func TestWithBunchCommands(t *testing.T) {
+	f := setup(3)
+	defer f()
+	obj.Put(1, 1)
+	if obj.Get(1) != 1 {
+		t.Fatalf("after put (1,1), get should return 1 but got:%d", obj.Get(1))
+	}
+	obj.Put(2, 2)
+	if obj.Get(2) != 2 {
+		t.Fatalf("after put (2,2), get should return 2 but got:%d", obj.Get(2))
+	}
+	obj.Put(3, 3)
+	if obj.Get(3) != 3 {
+		t.Fatalf("after put (3,3), get should return 3 but got:%d", obj.Get(3))
+	}
+	if obj.Get(1) != 1 {
+		t.Fatalf("after put (1,1), get should return 1 but got:%d", obj.Get(1))
+	}
+	obj.Put(4, 4)
+	// 2,2 is the oldest data now, should be evicted
+	if obj.Get(2) != -1 {
+		t.Fatalf("after put (4,4), get should return -1 but got:%d", obj.Get(2))
+	}
+	t.Log("pass")
+}
