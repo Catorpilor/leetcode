80c177a599452b5ca81f730e13de6cd9ba822c01
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-08 17:04:03 +0800

    solve counting elements by sorting first

--- counting_elements/count.go
+++ counting_elements/count.go
@@ -1,9 +1,12 @@
 package count
 
+import "sort"
+
 func countElements(arr []int) int {
 	return useHashmap(arr)
 }
 
+// useHashmap time complexity O(N), space complexity O(1)
 func useHashmap(arr []int) int {
 	n := len(arr)
 	if n <= 1 {
@@ -17,6 +20,29 @@ func useHashmap(arr []int) int {
 	for _, num := range arr {
 		if set[num+1] {
 			ans++
+		}
+	}
+	return ans
+}
+
+// useSort time complexity O(NlgN), space complexity O(1)
+func useSort(arr []int) int {
+	n := len(arr)
+	if n <= 1 {
+		return 0
+	}
+	sort.Ints(arr)
+	prev, prevCount := -1, 0
+	var ans int
+	for _, num := range arr {
+		if num == prev {
+			prevCount++
+		} else {
+			if num == prev+1 {
+				ans += prevCount
+			}
+			prev = num
+			prevCount = 1
 		}
 	}
 	return ans
--- counting_elements/count_test.go
+++ counting_elements/count_test.go
@@ -23,3 +23,25 @@ func TestCounting(t *testing.T) {
 		})
 	}
 }
+
+func TestCountingUseSort(t *testing.T) {
+	st := []struct {
+		name string
+		arr  []int
+		exp  int
+	}{
+		{"empty arr", nil, 0},
+		{"single element", []int{1}, 0},
+		{"all identical", []int{1, 1, 1}, 0},
+		{"testcase1", []int{1, 2, 3}, 2},
+		{"testcsae2", []int{1, 1, 1, 1, 1, 2}, 5},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useSort(tt.arr)
+			if out != tt.exp {
+				t.Fatalf("with input arr:%v wanted %d but got %d", tt.arr, tt.exp, out)
+			}
+		})
+	}
+}
