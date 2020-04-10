806634240ed572628110c4b2173ad07d0e5f1d45
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-10 18:50:20 +0800

    solve 215 using sort

--- 215_kth_largest_element_in_an_array/kth.go
+++ 215_kth_largest_element_in_an_array/kth.go
@@ -1,5 +1,17 @@
 package kth
 
+import "sort"
+
 func kthLargest(nums []int, k int) int {
-	return 0
+	return useSort(nums, k)
+}
+
+// useSort time complexity O(nlgn), space complexity O(1)
+func useSort(nums []int, k int) int {
+	n := len(nums)
+	if n < 1 || k > n {
+		return 0
+	}
+	sort.Ints(nums)
+	return nums[n-k]
 }
