500dddb86e8eada99292e84b1fd3bc43fe72b4c4
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-11 15:34:22 +0800

    solve 26 by just traverse the array

--- 26_remove_duplicates_from_sorted_array/rd.go
+++ 26_remove_duplicates_from_sorted_array/rd.go
@@ -1,5 +1,22 @@
 package rd
 
 func removeDups(nums []int) int {
-	return 0
+	return useTraverse(nums)
+}
+
+// useTraverse time complexity O(N), space complexity O(1)
+func useTraverse(nums []int) int {
+	n := len(nums)
+	if n <= 1 {
+		return n
+	}
+	prev, ans := nums[0], 1
+	for i := 1; i < n; i++ {
+		if nums[i] != prev {
+			prev = nums[i]
+			nums[ans], nums[i] = nums[i], nums[ans]
+			ans++
+		}
+	}
+	return ans
 }
