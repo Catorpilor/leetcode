e682c48e98ba0295ed8ac7492c415ddeefb2d071
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-12 11:20:59 +0800

    sovle 135 using brute force

--- 135_candy/candy.go
+++ 135_candy/candy.go
@@ -1,5 +1,36 @@
 package candy
 
 func numOfCandies(ratings []int) int {
-	return 0
+	return useBruteForce(ratings)
+}
+
+// useBruteForce time complexity O(N^2), space complexity O(N)
+func useBruteForce(ratings []int) int {
+	n := len(ratings)
+	if n <= 1 {
+		return n
+	}
+	flag := true
+	store := make([]int, n)
+	for i := range store {
+		store[i] = 1
+	}
+	for flag {
+		flag = false
+		for i := 0; i < n; i++ {
+			if i > 0 && ratings[i] > ratings[i-1] && store[i] <= store[i-1] {
+				store[i] = store[i-1] + 1 // update need to validate
+				flag = true
+			}
+			if i < n-1 && ratings[i] > ratings[i+1] && store[i] <= store[i+1] {
+				store[i] = store[i+1] + 1 // update need to validate
+				flag = true
+			}
+		}
+	}
+	var ans int
+	for i := range store {
+		ans += store[i]
+	}
+	return ans
 }
