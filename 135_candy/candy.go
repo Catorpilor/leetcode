b0aafcc205d13081e54dab150145985433aaaf50
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-12 11:43:36 +0800

    solve 135 by use two arrays

--- 135_candy/candy.go
+++ 135_candy/candy.go
@@ -1,7 +1,12 @@
 package candy
 
+import (
+	"github.com/catorpilor/leetcode/utils"
+)
+
 func numOfCandies(ratings []int) int {
-	return useBruteForce(ratings)
+	// return useBruteForce(ratings)
+	return useTwoArray(ratings)
 }
 
 // useBruteForce time complexity O(N^2), space complexity O(N)
@@ -31,6 +36,35 @@ func useBruteForce(ratings []int) int {
 	var ans int
 	for i := range store {
 		ans += store[i]
+	}
+	return ans
+}
+
+// useTwoArray time complexity O(N), space complexity O(N)
+func useTwoArray(ratings []int) int {
+	n := len(ratings)
+	if n <= 1 {
+		return n
+	}
+	leftTurn, rightTurn := make([]int, n), make([]int, n)
+	leftTurn[0], rightTurn[n-1] = 1, 1
+	for i := 1; i < n; i++ {
+		if ratings[i] <= ratings[i-1] {
+			leftTurn[i] = 1
+		} else {
+			leftTurn[i] = leftTurn[i-1] + 1
+		}
+	}
+	for i := n - 2; i >= 0; i-- {
+		if ratings[i] <= ratings[i+1] {
+			rightTurn[i] = 1
+		} else {
+			rightTurn[i] = rightTurn[i+1] + 1
+		}
+	}
+	var ans int
+	for i := 0; i < n; i++ {
+		ans += utils.Max(leftTurn[i], rightTurn[i])
 	}
 	return ans
 }
