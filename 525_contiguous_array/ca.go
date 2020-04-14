e4e9adc284db0cfbf4c755ee233889df315cc0cc
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-14 11:16:38 +0800

    solve 525 use brute force

--- 525_contiguous_array/ca.go
+++ 525_contiguous_array/ca.go
@@ -1,5 +1,24 @@
 package ca
 
+import "github.com/catorpilor/leetcode/utils"
+
 func maxContiguousArray(nums []int) int {
-    return 0
+    return useBruteForce(nums)
+}
+
+// useBruteForce time complexity O(N^2), space complexity O(1)
+func useBruteForce(nums []int) int {
+    n := len(nums)
+    var ans int
+    count := make([]int, 2)
+    for i := 0; i < n-1; i++ {
+        count[nums[i]] = 1
+        for j := i + 1; j < n; j++ {
+            count[nums[j]]++
+            if count[0] == count[1] {
+                ans = utils.Max(ans, j-i+1)
+            }
+        }
+    }
+    return ans
 }
