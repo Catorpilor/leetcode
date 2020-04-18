5b82d6feca297918a2573bd107894c134e414fb2
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-18 13:34:58 +0800

    solve 1026 using dfs

--- 1026_max_difference_between_node_and_ancestor/maxdiffs.go
+++ 1026_max_difference_between_node_and_ancestor/maxdiffs.go
@@ -3,5 +3,23 @@ package maxdiffs
 import "github.com/catorpilor/leetcode/utils"
 
 func maxDiff(root *utils.TreeNode) int {
-    return 0
+    return useDfs(root)
+}
+
+// useDfs time complexity O(N), space complexity O(N)
+func useDfs(root *utils.TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    return helper(root, root.Val, root.Val)
+
+}
+
+func helper(root *utils.TreeNode, min, max int) int {
+    if root == nil {
+        return max - min
+    }
+    max = utils.Max(max, root.Val)
+    min = utils.Min(min, root.Val)
+    return utils.Max(helper(root.Left, min, max), helper(root.Right, min, max))
 }
