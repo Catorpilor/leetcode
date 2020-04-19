5b07240bd1338018c52573204253ca80bdc8f823
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-19 17:25:57 +0800

    solve 33 by using binary search with one pass

--- 33_search_in_rotated_sorted_array/search.go
+++ 33_search_in_rotated_sorted_array/search.go
@@ -29,3 +29,38 @@ func IsExist(nums []int, target int) int {
 	}
 	return -1
 }
+
+func search(nums []int, target int) int {
+	return useBinarySearchWithOnePass(nums, target)
+}
+
+// useBinarySearchWithOnePass time complexity O(lgN), space complexity O(1)
+func useBinarySearchWithOnePass(nums []int, target int) int {
+	n := len(nums)
+	if n < 1 {
+		return -1
+	}
+	if target > nums[0] && target < nums[n-1] {
+		return -1
+	}
+	left, right := 0, n-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] >= nums[left] {
+			if target >= nums[left] && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target <= nums[right] && target > nums[mid] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
--- 33_search_in_rotated_sorted_array/search_test.go
+++ 33_search_in_rotated_sorted_array/search_test.go
@@ -22,3 +22,25 @@ func TestIsExist(t *testing.T) {
 		})
 	}
 }
+
+func TestSearch(t *testing.T) {
+	st := []struct {
+		name        string
+		nums        []int
+		target, exp int
+	}{
+		{"empty slice", []int{}, 5, -1},
+		{"single slice", []int{1}, 2, -1},
+		{"test 1", []int{4, 5, 6, 7, 0, 1, 2, 3}, 7, 3},
+		{"testcase2", []int{80, 4, 5, 17, 19, 20, 50}, 30, -1},
+	}
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			ret := search(c.nums, c.target)
+			if ret != c.exp {
+				t.Fatalf("expected %d but got %d, with input %v and %d",
+					c.exp, ret, c.nums, c.target)
+			}
+		})
+	}
+}
