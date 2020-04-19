c9fae480b18477b4bebf35ba73911b8ec3d5d0a0
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-19 17:41:00 +0800

    sovle 33 by use binary search with two passes

--- 33_search_in_rotated_sorted_array/search.go
+++ 33_search_in_rotated_sorted_array/search.go
@@ -64,3 +64,47 @@ func useBinarySearchWithOnePass(nums []int, target int) int {
 	}
 	return -1
 }
+
+// useBinarySearchWithTwoPasses time complexity O(lgN), space complexity O(1)
+func useBinarySearchWithTwoPasses(nums []int, target int) int {
+	n := len(nums)
+	if n < 1 || (target > nums[0] && target < nums[n-1]) {
+		return -1
+	}
+	// the following piece of code is to find the real start
+	// which means the smallest element in the array.
+	// for example
+	// with rotated nums:                         [4,5,6,7,0,1,2], we can split the array into two parts.
+	// with condition nums[i] > nums[n-1],we got   T,T,T,T,F,F,F
+	// so the mission here is to find the first `F`
+	initStart := 0
+	left, right := 0, n-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] > nums[n-1] {
+			left = mid + 1
+		} else {
+			initStart = mid
+			right = mid - 1
+		}
+	}
+	// the second pass
+	left, right = 0, n-1
+	if target > nums[n-1] {
+		right = initStart - 1
+	}
+	if target < nums[0] {
+		left = initStart
+	}
+	for left <= right {
+		mid := left + (right-left)/2
+		if target == nums[mid] {
+			return mid
+		} else if target < nums[mid] {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return -1
+}
