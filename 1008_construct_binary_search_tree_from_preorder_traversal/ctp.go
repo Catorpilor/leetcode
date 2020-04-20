46ba78433ae89f0c2eb9ea85664d771f6f70b016
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-20 19:48:09 +0800

    solve 1008 use dfs

--- 1008_construct_binary_search_tree_from_preorder_traversal/ctp.go
+++ 1008_construct_binary_search_tree_from_preorder_traversal/ctp.go
@@ -3,5 +3,24 @@ package ctp
 import "github.com/catorpilor/leetcode/utils"
 
 func constructTree(order []int) *utils.TreeNode {
-    return nil
+    return useDFS(order)
+}
+
+// useDFS time complexity O(N), space complexity O(N)
+func useDFS(order []int) *utils.TreeNode {
+    n := len(order)
+    if n == 0 {
+        return nil
+    }
+    root := &utils.TreeNode{Val: order[0]}
+    pos := 1
+    for pos < n {
+        if order[pos] > order[0] {
+            break
+        }
+        pos++
+    }
+    root.Left = useDFS(order[1:pos])
+    root.Right = useDFS(order[pos:])
+    return root
 }
--- 1008_construct_binary_search_tree_from_preorder_traversal/ctp_test.go
+++ 1008_construct_binary_search_tree_from_preorder_traversal/ctp_test.go
@@ -1,7 +1,6 @@
 package ctp
 
 import (
-    "math"
     "reflect"
     "testing"
 
@@ -16,8 +15,8 @@ func TestConstructTree(t *testing.T) {
     }{
         {"empty order", nil, nil},
         {"single element", []int{1}, []int{1}},
-        {"testcase1", []int{8, 5, 1, 7, 10, 12}, []int{8, 5, 10, 1, 7, math.MinInt32, 12}},
-        {"testcase2", []int{5, 3, 2, 4}, []int{5, 3, math.MinInt32, 2, 4}},
+        {"testcase1", []int{8, 5, 1, 7, 10, 12}, []int{8, 5, 10, 1, 7, 12}},
+        {"testcase2", []int{5, 3, 2, 4}, []int{5, 3, 2, 4}},
     }
     for _, tt := range st {
         t.Run(tt.name, func(t *testing.T) {
