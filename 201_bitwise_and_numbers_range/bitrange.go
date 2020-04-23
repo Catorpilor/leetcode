ed92bd78e8554e1eb370a004b406c0b86ac0d55b
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-23 21:35:34 +0800

    solve 201 by finding the common prefix of m and n

--- 201_bitwise_and_numbers_range/bitrange.go
+++ 201_bitwise_and_numbers_range/bitrange.go
@@ -1,5 +1,16 @@
 package bitrange
 
 func rangeBitwiseAnd(m, n int) int {
-    return 0
+    return commonPrefix(m, n)
+}
+
+// commonPrefix time complexity O(1), space complexity O(1)
+func commonPrefix(m, n int) int {
+    var shift int
+    for m < n {
+        m >>= 1
+        n >>= 1
+        shift++
+    }
+    return m << shift
 }
