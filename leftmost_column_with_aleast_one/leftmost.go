0b2bb1f6e40ac90a29d87fc19163fd40f3168b61
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-24 21:54:06 +0800

    solve leftmost column with one use binary search

--- leftmost_column_with_aleast_one/leftmost.go
+++ leftmost_column_with_aleast_one/leftmost.go
@@ -1,5 +1,50 @@
 package leftmost
 
+import "github.com/catorpilor/leetcode/utils"
+
+// board is at most 100*100
+// restricions: calling board[i][j] no more than 1000 times.
+
 func leftMostColumnWithOne(board [][]int) int {
-    return -1
+    return useBinarySearch(board)
+}
+
+// useBinarySearch time complexity O(M*lgN), space complexity O(1)
+func useBinarySearch(board [][]int) int {
+    // board's row element is non-decresing order.
+    m := len(board)
+    ans := -1
+    if m < 1 {
+        return ans
+    }
+    n := len(board[0])
+    if n < 1 {
+        return ans
+    }
+    for i := 0; i < m; i++ {
+        tmp := lowerBoundOfOne(board[i])
+        if tmp != -1 {
+            if ans != -1 {
+                ans = utils.Min(ans, tmp)
+            } else {
+                ans = tmp
+            }
+        }
+    }
+    return ans
+}
+
+func lowerBoundOfOne(nums []int) int {
+    ans := -1
+    l, r := 0, len(nums)-1
+    for l <= r {
+        mid := l + (r-l)/2
+        if nums[mid] != 1 {
+            l = mid + 1
+        } else {
+            ans = mid
+            r = mid - 1
+        }
+    }
+    return ans
 }
--- leftmost_column_with_aleast_one/leftmost_test.go
+++ leftmost_column_with_aleast_one/leftmost_test.go
@@ -12,7 +12,7 @@ func TestLeftmostColumnWithOne(t *testing.T) {
         {"all zeors", [][]int{[]int{0, 0}, []int{0, 0}}, -1},
         {"all ones", [][]int{[]int{1, 1}, []int{1, 1}}, 0},
         {"testcase1", [][]int{[]int{0, 0}, []int{0, 1}}, 1},
-        {"testcsae2", [][]int{[]int{0, 0, 0}, []int{0, 0, 1}, []int{1, 0, 0}}, 1},
+        {"testcsae2", [][]int{[]int{0, 0, 0}, []int{0, 0, 1}, []int{0, 1, 1}}, 1},
     }
     for _, tt := range st {
         t.Run(tt.name, func(t *testing.T) {
