3d375c377e72a8bc3bf86cb917a3c8015019c28d
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-24 22:25:20 +0800

    solve leftmost column with one use traverse

--- leftmost_column_with_aleast_one/leftmost.go
+++ leftmost_column_with_aleast_one/leftmost.go
@@ -6,7 +6,8 @@ import "github.com/catorpilor/leetcode/utils"
 // restricions: calling board[i][j] no more than 1000 times.
 
 func leftMostColumnWithOne(board [][]int) int {
-    return useBinarySearch(board)
+    // return useBinarySearch(board)
+    return useTraverse(board)
 }
 
 // useBinarySearch time complexity O(M*lgN), space complexity O(1)
@@ -44,6 +45,34 @@ func lowerBoundOfOne(nums []int) int {
         } else {
             ans = mid
             r = mid - 1
+        }
+    }
+    return ans
+}
+
+// useTraverse time complexity O(M+N), space complexity O(1)
+func useTraverse(board [][]int) int {
+    m := len(board)
+    ans := -1
+    if m < 1 {
+        return ans
+    }
+    n := len(board[0])
+    if n < 1 {
+        return ans
+    }
+    var i, j int
+    j = n - 1
+    for i < m && j >= 0 {
+        if board[i][j] == 1 {
+            if ans != -1 {
+                ans = utils.Min(ans, j)
+            } else {
+                ans = j
+            }
+            j--
+        } else {
+            i++
         }
     }
     return ans
