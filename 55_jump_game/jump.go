89b0020edbaeea20aa167ebc6bd5a3c1d1f7d699
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-25 17:05:30 +0800

    solve 55 use greedy

--- 55_jump_game/jump.go
+++ 55_jump_game/jump.go
@@ -1,5 +1,25 @@
 package jump
 
+import "github.com/catorpilor/leetcode/utils"
+
 func canJump(arr []int) bool {
+    return useGreedyForward(arr)
+}
+
+// useGreedyForward time complexity O(N), space complexity O(1)
+func useGreedyForward(nums []int) bool {
+    n := len(nums)
+    if n < 1 {
+        return false
+    }
+    // the maxPos can get so far
+    maxPos := 0
+    for i := 0; i <= maxPos; i++ {
+        // update the right boundary if necessary.
+        maxPos = utils.Max(maxPos, i+nums[i])
+        if maxPos >= n-1 {
+            return true
+        }
+    }
     return false
 }
