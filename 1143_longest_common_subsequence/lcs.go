c4d8ae4982e0a83b450fe89fe943cefae1a839fd
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-26 19:27:39 +0800

    solve 1143 use dp with less space

--- 1143_longest_common_subsequence/lcs.go
+++ 1143_longest_common_subsequence/lcs.go
@@ -2,11 +2,43 @@ package lcs
 
 import "github.com/catorpilor/leetcode/utils"
 
-// longestCommonSeq using dp to solve the problem
-// time complexity is O(mn)
-// space compelxity is o(n)
 func longestCommonSeq(text1, text2 string) int {
+	return useDPLessSpace(text1, text2)
+}
+
+// useDP time complexity O(MN), space complexity O(MN)
+func useDP(text1, text2 string) int {
+	n1, n2 := len(text1), len(text2)
+	if n1 == 0 || n2 == 0 {
+		return 0
+	}
+	if n1 > n2 {
+		text1, text2 = text2, text1
+		n1, n2 = n2, n1
+	}
+	dp := make([][]int, n1+1)
+	for i := range dp {
+		dp[i] = make([]int, n2+1)
+	}
+	for i := 1; i <= n1; i++ {
+		for j := 1; j <= n2; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = utils.Max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[n1][n2]
+}
+
+// useDPLessSpace time complexity is O(mn)
+// space compelxity is O(n)
+func useDPLessSpace(text1, text2 string) int {
 	m, n := len(text1), len(text2)
+	if m == 0 || n == 0 {
+		return 0
+	}
 	if text1 == text2 {
 		return m
 	}
@@ -25,4 +57,5 @@ func longestCommonSeq(text1, text2 string) int {
 		}
 	}
 	return dp[n]
+
 }
