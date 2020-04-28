76720c13f9764da6923862843efbc488c4471d0e
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-28 23:41:28 +0800

    solve first_unique_number with hashmap and doubly linked list

--- first_unique_number/fun.go
+++ first_unique_number/fun.go
@@ -1,7 +1,70 @@
 package fun
 
-type FU struct{}
+type Node struct {
+    Val        int
+    Prev, Next *Node
+}
+
+type FU struct {
+    store      []int
+    head, tail *Node
+    dupSet     map[int]int
+    curUniqSet map[int]*Node
+}
+
+func constructor(nums []int) *FU {
+    set := make(map[int]int)
+    curSet := make(map[int]*Node)
+    for i := range nums {
+        set[nums[i]]++
+    }
+    head, tail := &Node{Val: -1}, &Node{Val: -1}
+    head.Next, tail.Prev = tail, head
+    cur := head
+    for i := range nums {
+        if set[nums[i]] == 1 {
+            node := &Node{Val: nums[i]}
+            tmp := cur.Next
+            cur.Next = node
+            node.Prev = cur
+            node.Next = tmp
+            tmp.Prev = node
+            curSet[nums[i]] = node
+            cur = cur.Next
+        }
+    }
+    return &FU{
+        store:  nums,
+        head:   head,
+        tail:   tail,
+        dupSet: set,
+    }
+}
 
 func (fu FU) showFirstUnique() int {
-	return -1
+    if fu.head.Next != fu.tail {
+        return fu.head.Next.Val
+    }
+    return -1
+}
+
+// add time complexity O(N)
+func (fu *FU) add(val int) {
+    fu.store = append(fu.store, val)
+    fu.dupSet[val]++
+    count := fu.dupSet[val]
+    if count > 2 {
+        return
+    } else if count == 2 {
+        node := fu.curUniqSet[val]
+        node.Prev.Next = node.Next
+        node.Next.Prev = node.Prev
+        delete(fu.curUniqSet, val)
+    } else {
+        node := &Node{Val: val, Prev: fu.tail.Prev, Next: fu.tail}
+        fu.tail.Prev.Next = node
+        fu.tail.Prev = node
+        fu.curUniqSet[val] = node
+    }
+    return
 }
--- first_unique_number/fun_test.go
+++ first_unique_number/fun_test.go
@@ -2,15 +2,26 @@ package fun
 
 import "testing"
 
+func initFuWithTest(nums []int, t *testing.T) *FU {
+	fu := constructor(nums)
+	t.Cleanup(func() {
+		fu = nil
+	})
+	return fu
+}
+
 func TestShowFirstUnique(t *testing.T) {
 	st := []struct {
 		name string
 		nums []int
 		exp  int
-	}{}
+	}{
+		{"testcase1", []int{7, 7, 7, 7}, -1},
+		{"testcase2", []int{2, 3, 5}, 2},
+	}
 	for _, tt := range st {
 		t.Run(tt.name, func(t *testing.T) {
-			fu := FU{}
+			fu := initFuWithTest(tt.nums, t)
 			out := fu.showFirstUnique()
 			if out != tt.exp {
 				t.Fatalf("with input nums: %v wanted %d but got %d", tt.nums, tt.exp, out)
