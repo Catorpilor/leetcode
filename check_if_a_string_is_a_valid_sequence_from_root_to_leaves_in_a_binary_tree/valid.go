214882c92424a9f200ac1abe89c695d80337b8f6
Catorpilor <jiaojiangonghitster@gmail.com>
2020-04-30 21:09:11 +0800

    solve valid sequence by dfs

--- check_if_a_string_is_a_valid_sequence_from_root_to_leaves_in_a_binary_tree/valid.go
+++ check_if_a_string_is_a_valid_sequence_from_root_to_leaves_in_a_binary_tree/valid.go
@@ -3,5 +3,19 @@ package valid
 import "github.com/catorpilor/leetcode/utils"
 
 func isValid(root *utils.TreeNode, arr []int) bool {
+    return useDFS(root, arr, 0, len(arr))
+}
+
+// useDFS time complexity O(N), space complexity O(lgN)
+func useDFS(root *utils.TreeNode, arr []int, pos, l int) bool {
+    if root == nil {
+        return l == 0
+    }
+    if pos == l-1 && root.Left == nil && root.Right == nil && root.Val == arr[pos] {
+        return true
+    }
+    if pos < l-1 {
+        return useDFS(root.Left, arr, pos+1, l) || useDFS(root.Right, arr, pos+1, l)
+    }
     return false
 }
