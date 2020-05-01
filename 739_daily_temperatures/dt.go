b4ac1a22c3d16e8c2d83a85880b4b74ad823b231
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-01 15:54:22 +0800

    solve 739 use stack

--- 739_daily_temperatures/dt.go
+++ 739_daily_temperatures/dt.go
@@ -1,5 +1,27 @@
 package dt
 
 func dailyTemps(temps []int) []int {
-    return nil
+    return useStack(temps)
+}
+
+// useStack time complexity O(N), space complexity O(N)
+func useStack(temps []int) []int {
+    n := len(temps)
+    ans := make([]int, n)
+    if n <= 1 {
+        return ans
+    }
+    st := make([]int, 0, n)
+    for i := range temps {
+        nst := len(st)
+        for nst > 0 && temps[st[nst-1]] < temps[i] {
+            // pop from stack
+            top := st[nst-1]
+            ans[top] = i - top
+            st = st[:nst-1]
+            nst--
+        }
+        st = append(st, i)
+    }
+    return ans
 }
