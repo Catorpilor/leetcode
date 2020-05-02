74465ea385357ba8b264f3f5ba8429ab0bc92aa4
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-02 21:40:28 +0800

    solve 771 use hashmap

--- 771_jewels_and_stones/jewel.go
+++ 771_jewels_and_stones/jewel.go
@@ -1,5 +1,24 @@
 package jewel
 
 func numOfStones(j, s string) int {
-    return 0
+    return useHashmap(j, s)
+}
+
+// useHashmap time complexity O(M+N), space complexity O(M)
+func useHashmap(j, s string) int {
+    n := len(j)
+    if n < 1 {
+        return n
+    }
+    hset := make(map[byte]bool, n)
+    for i := range j {
+        hset[j[i]] = true
+    }
+    var ans int
+    for i := range s {
+        if hset[s[i]] {
+            ans++
+        }
+    }
+    return ans
 }
