7b9b726af66a9b61a88f573b7db94ef66ce3b927
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-03 18:49:55 +0800

    solve 1436 use hash

--- 1436_destination_city/dc.go
+++ 1436_destination_city/dc.go
@@ -1,5 +1,26 @@
 package dc
 
 func destCity(paths [][]string) string {
-    return ""
+    return useHashmap(paths)
+}
+
+func useHashmap(paths [][]string) string {
+    n := len(paths)
+    if n < 1 {
+        return ""
+    }
+    in, out := make(map[string]int, n), make(map[string]int, n)
+    for i := range paths {
+        out[paths[i][0]]++
+        in[paths[i][1]]++
+    }
+    var ans string
+    for k := range in {
+        if _, exists := out[k]; exists {
+            continue
+        }
+        ans = k
+        break
+    }
+    return ans
 }
