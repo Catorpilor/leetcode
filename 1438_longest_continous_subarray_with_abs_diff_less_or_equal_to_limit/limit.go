32705a52654a11009403892b4a16d3797106b5cf
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-04 15:44:37 +0800

    solve 1438 use brute force but got TLE

--- 1438_longest_continous_subarray_with_abs_diff_less_or_equal_to_limit/limit.go
+++ 1438_longest_continous_subarray_with_abs_diff_less_or_equal_to_limit/limit.go
@@ -1,5 +1,36 @@
 package limit
 
 func longestSubarray(nums []int, limit int) int {
-    return 0
+    return usebf(nums, limit)
+}
+
+// usebf time complexity O(N^3), space complexity O(1)
+func usebf(nums []int, limit int) int {
+    ans := 1
+    n := len(nums)
+    for k := 2; k <= n; k++ {
+        for i := 0; i+k <= n; i++ {
+            // subarray nums[i:i+k]
+            lmin, lmax := nums[i], nums[i]
+            for j := i + 1; j < i+k; j++ {
+                if nums[j] > lmax {
+                    lmax = nums[j]
+                }
+                if nums[j] < lmin {
+                    lmin = nums[j]
+                }
+            }
+            if lmax-lmin <= limit {
+                ans = max(ans, k)
+            }
+        }
+    }
+    return ans
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
 }
