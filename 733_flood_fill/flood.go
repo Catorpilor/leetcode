00d5d1bf9b3f7f44755aa5f4be154121d221ff99
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-11 18:52:17 +0800

    solve 733 use dfs

--- 733_flood_fill/flood.go
+++ 733_flood_fill/flood.go
@@ -1,5 +1,31 @@
 package flood
 
 func floodFill(image [][]int, x, y, newColor int) [][]int {
+	m := len(image)
+	if m < 1 {
+		return image
+	}
+	n := len(image[0])
+	if n < 1 {
+		return image
+	}
+	old := image[x][y]
+	if old == newColor {
+		return image
+	}
+	useDfs(image, m, n, x, y, newColor, old)
 	return image
+}
+
+// useDfs time complexity O(N), space complexity O(N)
+func useDfs(image [][]int, m, n, x, y, newColor, old int) {
+	if x < 0 || x >= m || y < 0 || y >= n || image[x][y] != old {
+		return
+	}
+	image[x][y] = newColor
+	dirs := [5]int{1, 0, -1, 0, 1}
+	for i := 0; i < 4; i++ {
+		nx, ny := x+dirs[i], y+dirs[i+1]
+		useDfs(image, m, n, nx, ny, newColor, old)
+	}
 }
--- 733_flood_fill/flood_test.go
+++ 733_flood_fill/flood_test.go
@@ -15,6 +15,7 @@ func TestFloodFill(t *testing.T) {
 		{"empty image", nil, 0, 0, 2, nil},
 		{"singe point", [][]int{[]int{0, 0, 0}, []int{0, 1, 0}, []int{0, 0, 0}}, 1, 1, 2, [][]int{[]int{0, 0, 0}, []int{0, 2, 0}, []int{0, 0, 0}}},
 		{"testcase1", [][]int{[]int{1, 1, 0}, []int{1, 1, 1}, []int{1, 0, 1}}, 1, 1, 2, [][]int{[]int{2, 2, 0}, []int{2, 2, 2}, []int{2, 0, 2}}},
+		{"failed1", [][]int{[]int{0, 0, 0}, []int{0, 1, 1}}, 1, 1, 1, [][]int{[]int{0, 0, 0}, []int{0, 1, 1}}},
 	}
 	for _, tt := range st {
 		t.Run(tt.name, func(t *testing.T) {
