ce9b95e3a8cb92630620ac7ced7f3893c4b06e72
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-12 23:01:45 +0800

    solve 540 use a simpler binary search

--- 540_single_element_in_a_sorted_array/single.go
+++ 540_single_element_in_a_sorted_array/single.go
@@ -4,13 +4,11 @@ func singleDup(nums []int) int {
 	return useBinarySearch(nums)
 }
 
+// useBinarySearch time complexity O(lgN), space complexity O(1)
 func useBinarySearch(nums []int) int {
 	n := len(nums)
 	l, r := 0, n-1
-	for l <= r {
-		if l == r {
-			break
-		}
+	for l < r {
 		mid := l + (r-l)/2
 		if mid > 0 && mid < n-1 && nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1] {
 			// find the one
@@ -27,6 +25,28 @@ func useBinarySearch(nums []int) int {
 			} else {
 				r = mid - 1
 			}
+		}
+	}
+	return nums[l]
+}
+
+// useBSSimple time complexity O(lgN), space complexity O(1)
+func useBSSimple(nums []int) int {
+	n := len(nums)
+	l, r := 0, n-1
+	for l < r {
+		mid := l + (r-l)/2
+		// mid^1 is brilliant and elegent.
+		// for example with nums = [1,1,2,2,3]
+		//                          0,1,2,3,4
+		// mid = 2, mid^1=3
+		// nums[mid] == nums[mid^1] then l = 3
+		// mid = 3, mid^i = 2
+		// nums[mid] == nums[mid^1] then i=4 break the loop.
+		if nums[mid] == nums[mid^1] {
+			l = mid + 1
+		} else {
+			r = mid
 		}
 	}
 	return nums[l]
--- 540_single_element_in_a_sorted_array/single_test.go
+++ 540_single_element_in_a_sorted_array/single_test.go
@@ -24,3 +24,26 @@ func TestSingleDup(t *testing.T) {
 		})
 	}
 }
+
+func TestSingleDupSimple(t *testing.T) {
+	st := []struct {
+		name string
+		nums []int
+		exp  int
+	}{
+		{"single element", []int{1}, 1},
+		{"testcase1", []int{1, 1, 2}, 2},
+		{"testcase2", []int{1, 2, 2}, 1},
+		{"testcase3", []int{1, 1, 2, 2, 3}, 3},
+		{"testcase4", []int{1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 7}, 5},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useBSSimple(tt.nums)
+			if out != tt.exp {
+				t.Fatalf("with input nums:%v wanted %d but got %d", tt.nums, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
