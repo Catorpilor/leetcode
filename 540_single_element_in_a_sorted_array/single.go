7f6c9f69d888b9ce53b95cfec0bb0934ba0d2ba8
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-12 22:16:45 +0800

    add testcases for 540

---
+++ 540_single_element_in_a_sorted_array/single.go
@@ -0,0 +1,5 @@
+package single
+
+func singleDup(nums []int) int {
+	return 0
+}
---
+++ 540_single_element_in_a_sorted_array/single_test.go
@@ -0,0 +1,25 @@
+package single
+
+import "testing"
+
+func TestSingleDup(t *testing.T) {
+	st := []struct {
+		name string
+		nums []int
+		exp  int
+	}{
+		{"single element", []int{1}, 1},
+		{"testcase1", []int{1, 1, 2}, 2},
+		{"testcase2", []int{1, 2, 2}, 1},
+		{"testcase3", []int{1, 1, 2, 2, 3}, 3},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := singleDup(tt.nums)
+			if out != tt.exp {
+				t.Fatalf("with input nums:%v wanted %d but got %d", tt.nums, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
