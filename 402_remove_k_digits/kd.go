d6d65297dae981d193c5d75215a41c79316c6c10
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-13 20:35:51 +0800

    solve 402 use greedy

--- 402_remove_k_digits/kd.go
+++ 402_remove_k_digits/kd.go
@@ -1,5 +1,35 @@
 package kd
 
+import "github.com/catorpilor/LeetCode/utils"
+
 func removeKDigits(num string, k int) string {
-	return num
+	return useTraverse(num, k, 0)
+}
+
+func useTraverse(num string, k, peak int) string {
+	n := len(num)
+	if k == 0 {
+		idx := 0
+		for ; idx < len(num); idx++ {
+			if num[idx] == '0' {
+				continue
+			}
+			break
+		}
+		if idx == len(num) {
+			return "0"
+		}
+		return num[idx:]
+	}
+	if n == k {
+		return "0"
+	}
+
+	for ; peak < n-1; peak++ {
+		if num[peak] <= num[peak+1] {
+			continue
+		}
+		break
+	}
+	return useTraverse(num[:peak]+num[peak+1:], k-1, utils.Max(peak-1, 0))
 }
