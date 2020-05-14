bc1a5481e77af69c9085f0ea485afcb77999d468
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-14 15:51:13 +0800

    solve 403 use greedy with stack

--- 402_remove_k_digits/kd.go
+++ 402_remove_k_digits/kd.go
@@ -1,11 +1,14 @@
 package kd
 
-import "github.com/catorpilor/LeetCode/utils"
+import (
+	"github.com/catorpilor/LeetCode/utils"
+)
 
 func removeKDigits(num string, k int) string {
 	return useTraverse(num, k, 0)
 }
 
+// useTraverse time complexity O(KN), space complexity O(KN)
 func useTraverse(num string, k, peak int) string {
 	n := len(num)
 	if k == 0 {
@@ -32,4 +35,33 @@ func useTraverse(num string, k, peak int) string {
 		break
 	}
 	return useTraverse(num[:peak]+num[peak+1:], k-1, utils.Max(peak-1, 0))
+}
+
+// useStack time complexity O(N), space complexity O(N)
+func useStack(num string, k int) string {
+	n := len(num)
+	st := make([]byte, 0, n)
+	left := n - k
+	for _, c := range num {
+		nst := len(st)
+		for nst > 0 && st[nst-1] > byte(c) && k > 0 {
+			st = st[:nst-1]
+			nst--
+			k--
+		}
+		st = append(st, byte(c))
+	}
+	var idx int
+	// fmt.Println(string(st))
+	for ; idx < left; idx++ {
+		if st[idx] == '0' {
+			continue
+		}
+		break
+	}
+	if idx == left {
+		return "0"
+	}
+	// fmt.Println(idx, left)
+	return string(st[idx:left])
 }
