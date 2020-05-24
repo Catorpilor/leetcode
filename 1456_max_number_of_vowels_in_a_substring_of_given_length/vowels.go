2ed1875bb4ded45e1686293a09e1592882f7828c
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-24 22:03:12 +0800

    solve 1456 use slide window

--- 1456_max_number_of_vowels_in_a_substring_of_given_length/vowels.go
+++ 1456_max_number_of_vowels_in_a_substring_of_given_length/vowels.go
@@ -1,5 +1,43 @@
 package vowels
 
+import (
+	"strings"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
 func maxVowels(s string, k int) int {
-	return 0
+	return useSlideWindow(s, k)
+}
+
+// useSlideWindow time complexity O(N), space complexity O()
+func useSlideWindow(s string, k int) int {
+	n := len(s)
+	if n < 1 {
+		return 0
+	}
+	if k == 1 {
+		return 1
+	}
+	var cur int
+	for i := 0; i < k; i++ {
+		if isVowel(s[i]) {
+			cur++
+		}
+	}
+	ans := cur
+	for j := k; j < n; j++ {
+		if isVowel(s[j]) {
+			cur++
+		}
+		if isVowel(s[j-k]) {
+			cur--
+		}
+		ans = utils.Max(ans, cur)
+	}
+	return ans
+}
+
+func isVowel(b byte) bool {
+	return strings.IndexByte("aeiou", b) != -1
 }
