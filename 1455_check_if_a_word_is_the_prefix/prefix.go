6059546afb6a33e62912952527a6ae350aed3eda
Catorpilor <jiaojiangonghitster@gmail.com>
2020-05-24 21:48:26 +0800

    solve 1455 use onepass with strings.Fields and strings.HasPrefix

--- 1455_check_if_a_word_is_the_prefix/prefix.go
+++ 1455_check_if_a_word_is_the_prefix/prefix.go
@@ -1,5 +1,21 @@
 package prefix
 
+import "strings"
+
 func isPrefix(s, word string) int {
-	return -1
+	return useOnePass(s, word)
+}
+
+// useOnePass time complexity O(NM), space complexity O(1)
+// N represents the number of strings. M means the length of word
+func useOnePass(str, word string) int {
+	ss := strings.Fields(str)
+	res := -1
+	for i := range ss { // O(N)
+		if strings.HasPrefix(ss[i], word) { // O(M)
+			res = i + 1
+			break
+		}
+	}
+	return res
 }
