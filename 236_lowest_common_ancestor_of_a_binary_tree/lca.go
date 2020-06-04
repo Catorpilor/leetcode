40bac2eb4dbc139240e1f1242a065b3fbe4fbdda
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-04 22:46:14 +0800

    solve 239 use dfs

--- 236_lowest_common_ancestor_of_a_binary_tree/lca.go
+++ 236_lowest_common_ancestor_of_a_binary_tree/lca.go
@@ -3,5 +3,21 @@ package lca
 import "github.com/catorpilor/leetcode/utils"
 
 func lowsetCommonAncestor(root, p, q *utils.TreeNode) *utils.TreeNode {
-	return root
+	return useDfs(root, p, q)
+}
+
+// useDfs time complexity O(N), space complexity O(lgN)
+func useDfs(root, p, q *utils.TreeNode) *utils.TreeNode {
+	if root == nil || p == root || q == root {
+		return root
+	}
+	leftCommon := useDfs(root.Left, p, q)
+	rightCommon := useDfs(root.Right, p, q)
+	if leftCommon != nil && rightCommon != nil {
+		return root
+	}
+	if leftCommon != nil {
+		return leftCommon
+	}
+	return rightCommon
 }
