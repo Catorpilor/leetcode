408a0a25608eacda14ba4d0ff2b9147f3d9775b9
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-11 19:39:36 +0800

    solve 74  use one pass

--- 74_sort_colors/sort.go
+++ 74_sort_colors/sort.go
@@ -24,3 +24,22 @@ func useCountSort(nums []int) {
 		prev += count
 	}
 }
+
+// useOnePass time complexity O(N), space complexity O(1)
+func useOnePass(nums []int) {
+	redIdx, whiteIdx, blueIdx := 0, 0, len(nums)-1
+	for whiteIdx <= blueIdx {
+		if nums[whiteIdx] == 0 {
+			// it's red
+			nums[redIdx], nums[whiteIdx] = nums[whiteIdx], nums[redIdx]
+			redIdx++
+			whiteIdx++
+		} else if nums[whiteIdx] == 1 {
+			// it's white just increase whiteIdx
+			whiteIdx++
+		} else {
+			nums[whiteIdx], nums[blueIdx] = nums[blueIdx], nums[whiteIdx]
+			blueIdx--
+		}
+	}
+}
--- 74_sort_colors/sort_test.go
+++ 74_sort_colors/sort_test.go
@@ -26,3 +26,24 @@ func TestSortColors(t *testing.T) {
 		})
 	}
 }
+
+func TestSortColorsUseOnePass(t *testing.T) {
+	st := []struct {
+		name string
+		nums []int
+		exp  []int
+	}{
+		{"single color", []int{0}, []int{0}},
+		{"all identical", []int{1, 1, 1, 1}, []int{1, 1, 1, 1}},
+		{"testcase1", []int{2, 0, 2, 1, 1, 0}, []int{0, 0, 1, 1, 2, 2}},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			useOnePass(tt.nums)
+			if diff := cmp.Diff(tt.nums, tt.exp); diff != "" {
+				t.Fatalf("mismatch (-want +got):\n%s", diff)
+			}
+			t.Log("pass")
+		})
+	}
+}
