db446fd5bf610a9ed48cf8482f4ef4975242d5ab
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-11 19:32:47 +0800

    solve 74 use counting sort

--- 74_sort_colors/sort.go
+++ 74_sort_colors/sort.go
@@ -1,5 +1,26 @@
 package sort
 
 func sortColors(nums []int) {
-	return
+	useCountSort(nums)
+}
+
+// useCountSort time complexity O(N), space complexity O(1)
+func useCountSort(nums []int) {
+	n := len(nums)
+	if n <= 1 {
+		return
+	}
+	counts := [3]int{}
+	for _, num := range nums {
+		counts[num]++
+	}
+	prev := 0
+	for i, count := range counts {
+		if count != 0 {
+			for j := prev; j < prev+count; j++ {
+				nums[j] = i
+			}
+		}
+		prev += count
+	}
 }
