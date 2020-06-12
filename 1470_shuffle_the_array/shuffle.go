7e5e95789f04fec3a9d6604d61669cbeffe3f9b4
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-12 22:06:42 +0800

    solve use regular slice tricks

--- 1470_shuffle_the_array/shuffle.go
+++ 1470_shuffle_the_array/shuffle.go
@@ -1,5 +1,14 @@
 package shuffle
 
 func shuf(nums []int, n int) []int {
-	return nums
+	return useSlice(nums, n)
+}
+
+func useSlice(nums []int, n int) []int {
+	res := make([]int, 0, n)
+	prev, last := nums[:n], nums[n:]
+	for i := 0; i < n; i++ {
+		res = append(res, prev[i], last[i])
+	}
+	return res
 }
