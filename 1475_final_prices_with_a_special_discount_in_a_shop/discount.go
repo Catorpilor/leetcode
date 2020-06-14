fcf6fe62a3ab51d7a61324ee32a82e35cebb5257
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-14 09:49:52 +0800

    solve 1475 use stack

--- 1475_final_prices_with_a_special_discount_in_a_shop/discount.go
+++ 1475_final_prices_with_a_special_discount_in_a_shop/discount.go
@@ -1,5 +1,24 @@
 package discount
 
 func finalPrices(prices []int) []int {
+	return useStack(prices)
+}
+
+// useStack time complexity O(N), space complexity O(N)
+func useStack(prices []int) []int {
+	n := len(prices)
+	if n <= 1 {
+		return prices
+	}
+	st := make([]int, 0, n)
+	for i := range prices {
+		nst := len(st)
+		for nst > 0 && prices[st[nst-1]] >= prices[i] {
+			prices[st[nst-1]] -= prices[i]
+			st = st[:nst-1]
+			nst--
+		}
+		st = append(st, i)
+	}
 	return prices
 }
