6ef518452bb18c15ee1bd30d55ec0a1194f890b8
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-19 14:58:13 +0800

    solve 275 use binary search

--- 275_h_index_ii/hindex.go
+++ 275_h_index_ii/hindex.go
@@ -1,5 +1,22 @@
 package hindex
 
 func getHIndex(citations []int) int {
-	return -1
+	return useBinarySearch(citations)
+}
+
+// useBinarySearch time complexity O(log(N)), space complexity O(1)
+func useBinarySearch(citations []int) int {
+	n := len(citations)
+	low, hi := 0, n-1
+	for low <= hi {
+		mid := low + (hi-low)/2
+		if n-mid == citations[mid] {
+			return citations[mid]
+		} else if n-mid < citations[mid] {
+			hi = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return n - low
 }
