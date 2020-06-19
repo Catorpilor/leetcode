cea079adf280e535ee5355eaa9afa8e98bc4ac5d
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-19 18:07:59 +0800

    solve 1482 use binary search

--- 1482_minimum_number_of_days_to_make_m_bouquets/bouquet.go
+++ 1482_minimum_number_of_days_to_make_m_bouquets/bouquet.go
@@ -1,5 +1,35 @@
 package bouquet
 
 func minDays(bloomDay []int, m, k int) int {
-	return -1
+	return useBinarySearch(bloomDay, m, k)
+}
+
+// useBinarySearch time complexity O(N*log(K)), space complexity O(1)
+func useBinarySearch(bloomDay []int, m, k int) int {
+	n := len(bloomDay)
+	if m*k > n {
+		return -1
+	}
+	low, hi := 1, int(1e9)
+	for low < hi {
+		mid := low + (hi-low)/2
+		var bloomedFlower, bouq int
+		for j := 0; j < n; j++ {
+			if bloomDay[j] > mid {
+				bloomedFlower = 0
+			} else {
+				bloomedFlower++
+				if bloomedFlower >= k {
+					bouq++
+					bloomedFlower = 0
+				}
+			}
+		}
+		if bouq < m {
+			low = mid + 1
+		} else {
+			hi = mid
+		}
+	}
+	return low
 }
