957ba9dc927238493911160a7b359d52f6728724
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-20 11:23:01 +0800

    solve 1483 use binary lifting

--- 1483_Kth_Ancestor_of_a_Tree_Node/kth.go
+++ 1483_Kth_Ancestor_of_a_Tree_Node/kth.go
@@ -1,16 +1,56 @@
 package kth
 
+import (
+	"math"
+)
+
 type TreeAncestor struct {
 	// for now just use brute force
-	n      int
-	parent []int
+	n         int
+	avgHeight int
+	parent    []int
+	dp        [][]int
 }
 
 func Construct(n int, parent []int) *TreeAncestor {
-	return &TreeAncestor{
-		n:      n,
-		parent: parent,
+	h := int(math.Log2(float64(n)))
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, h+1)
+		for j := range dp[i] {
+			dp[i][j] = -1
+		}
+		dp[i][0] = parent[i]
 	}
+	for l := 1; l <= h; l++ {
+		for i := 1; i < n; i++ {
+			if dp[i][l-1] != -1 {
+				dp[i][l] = dp[dp[i][l-1]][l-1]
+			}
+		}
+	}
+
+	return &TreeAncestor{
+		n:         n,
+		parent:    parent,
+		avgHeight: h,
+		dp:        dp,
+	}
+}
+
+// useBinaryLifting time complexity O(logN)
+func (this *TreeAncestor) useBinaryLifting(node, k int) int {
+	cur := node
+	for i := this.avgHeight; i >= 0; i-- {
+		if this.dp[cur][i] != -1 && (1<<i) <= k {
+			cur = this.dp[cur][i]
+			k -= 1 << i
+		}
+	}
+	if k > 0 {
+		return -1
+	}
+	return cur
 }
 
 func (this *TreeAncestor) GetKthAncestor(node, k int) int {
--- 1483_Kth_Ancestor_of_a_Tree_Node/kth_test.go
+++ 1483_Kth_Ancestor_of_a_Tree_Node/kth_test.go
@@ -3,11 +3,32 @@ package kth
 import "testing"
 
 func constructWithTesting(t *testing.T) *TreeAncestor {
-	ta := Construct(6, []int{-1, 0, 0, 1, 1, 2, 2})
+	ta := Construct(7, []int{-1, 0, 0, 1, 1, 2, 2})
 	t.Cleanup(func() {
 		ta = nil
 	})
 	return ta
+}
+
+func TestKthAncestorUseBf(t *testing.T) {
+	ta := constructWithTesting(t)
+	st := []struct {
+		name         string
+		node, k, exp int
+	}{
+		{"testcase1", 3, 1, 1},
+		{"testcase2", 5, 2, 0},
+		{"testcase3", 6, 3, -1},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := ta.useBinaryLifting(tt.node, tt.k)
+			if out != tt.exp {
+				t.Fatalf("with input node:%d and k:%d wanted %d but got %d", tt.node, tt.k, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
 }
 
 func TestKthAncestor(t *testing.T) {
