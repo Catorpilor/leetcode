7bb4c101970ad95e4399cd1e67f76a3d1e784404
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-24 20:05:28 +0800

    sovle 222 use just dfs

--- 222_count_complete_tree_nodes/count.go
+++ 222_count_complete_tree_nodes/count.go
@@ -3,5 +3,15 @@ package count
 import "github.com/catorpilor/LeetCode/utils"
 
 func countNodes(root *utils.TreeNode) int {
-	return 0
+	return useDfs(root)
+}
+
+// useDfs time complexity O(N), space complexity O(log(N))
+func useDfs(root *utils.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	lc := useDfs(root.Left)
+	rc := useDfs(root.Right)
+	return 1 + lc + rc
 }
