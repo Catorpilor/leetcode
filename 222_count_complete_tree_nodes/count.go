78e94d29e7a2b8f0612d71f2d736baf963c34455
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-25 13:45:12 +0800

    solve 222 use full binary tree properties

--- 222_count_complete_tree_nodes/count.go
+++ 222_count_complete_tree_nodes/count.go
@@ -15,3 +15,24 @@ func useDfs(root *utils.TreeNode) int {
 	rc := useDfs(root.Right)
 	return 1 + lc + rc
 }
+
+// useDfsWithCBSProp time compleixty O(log(N)*log(N)), space complexity O(logN)
+func useDfsWithCBSProp(root *utils.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	lr, rr := root, root
+	var lh, rh int
+	for lr != nil {
+		lr = lr.Left
+		lh++
+	}
+	for rr != nil {
+		rr = rr.Right
+		rh++
+	}
+	if lh == rh {
+		return 1<<lh - 1
+	}
+	return 1 + useDfsWithCBSProp(root.Left) + useDfsWithCBSProp(root.Right)
+}
--- 222_count_complete_tree_nodes/count_test.go
+++ 222_count_complete_tree_nodes/count_test.go
@@ -27,3 +27,25 @@ func TestCountNodes(t *testing.T) {
 		})
 	}
 }
+
+func TestCountNodesWithCBSProp(t *testing.T) {
+	st := []struct {
+		name string
+		root *utils.TreeNode
+		exp  int
+	}{
+		{"emtpty tree", nil, 0},
+		{"single node", utils.ConstructTree([]int{1}), 1},
+		{"full complete tree", utils.ConstructTree([]int{1, 2, 3, 4, 5, 6, 7}), 7},
+		{"testcase1", utils.ConstructTree([]int{1, 2, 3, 4, 5, 6}), 6},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useDfsWithCBSProp(tt.root)
+			if out != tt.exp {
+				t.Fatalf("with input tree wanted %d but got %d", tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
