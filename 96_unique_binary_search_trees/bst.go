d132c0e8bb56ee6eb89079d50b0dae603f2045b5
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-26 10:53:24 +0800

    solve 96 use mathmatical dp

--- 96_unique_binary_search_trees/bst.go
+++ 96_unique_binary_search_trees/bst.go
@@ -1,5 +1,18 @@
 package bst
 
 func numOfBst(n int) int {
-	return n
+	return useDP(n)
+}
+
+// useDP time complexity O(N^2), space complexity O(N)
+func useDP(n int) int {
+	dp := make([]int, n+1)
+	dp[1] = 1
+	dp[0] = 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
 }
