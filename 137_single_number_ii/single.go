27e43da2bb809c9055288666b065cc885eb65a13
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-26 11:22:01 +0800

    solve 137 use bit operation

--- 137_single_number_ii/single.go
+++ 137_single_number_ii/single.go
@@ -1,9 +1,11 @@
 package single
 
 func singleNumber(nums []int) int {
-	return useExtra(nums)
+	// return useExtra(nums)
+	return useBitOp(nums)
 }
 
+// useExtra time complexity O(N), space complexity O(N)
 func useExtra(nums []int) int {
 	n := len(nums)
 	hset := make(map[int]int, n)
@@ -16,4 +18,21 @@ func useExtra(nums []int) int {
 		}
 	}
 	return -1
+}
+
+// useBitOp time complexity O(N), space complexity O(1)
+func useBitOp(nums []int) int {
+	// k = 3 => 11 => k1 = 1, k2 = 1
+	// m = logk = 2 use two variables
+	// m > logk so we use a mask
+	// mask = ^(y1 & y2) where y1=x1 if k1==1, y2=~x2 if k2 == 0
+	var x1, x2, mask int
+	for _, num := range nums {
+		x2 ^= (x1 & num)
+		x1 ^= num
+		mask = ^(x1 & x2)
+		x1 &= mask
+		x2 &= mask
+	}
+	return x1
 }
