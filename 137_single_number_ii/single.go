54909af706cd5b457ef9d6e1ce01ed58012bbd6c
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-26 11:08:05 +0800

    solve 137 use extra space

--- 137_single_number_ii/single.go
+++ 137_single_number_ii/single.go
@@ -1,5 +1,19 @@
 package single
 
 func singleNumber(nums []int) int {
+	return useExtra(nums)
+}
+
+func useExtra(nums []int) int {
+	n := len(nums)
+	hset := make(map[int]int, n)
+	for _, num := range nums {
+		hset[num]++
+	}
+	for k, v := range hset {
+		if v == 1 {
+			return k
+		}
+	}
 	return -1
 }
