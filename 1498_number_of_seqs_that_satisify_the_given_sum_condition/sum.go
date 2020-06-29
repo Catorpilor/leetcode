43671f8b5b43673b7fb517f3c1ec6ebba0a3f829
Catorpilor <jiaojiangonghitster@gmail.com>
2020-06-29 17:27:53 +0800

    solve 1498 use two pointers

--- 1498_number_of_seqs_that_satisify_the_given_sum_condition/sum.go
+++ 1498_number_of_seqs_that_satisify_the_given_sum_condition/sum.go
@@ -1,5 +1,32 @@
 package sum
 
+import "sort"
+
 func numSubSeq(nums []int, target int) int {
-	return -1
+	return useTwoPointers(nums, target)
+}
+
+// useTwoPointers time complexity O(N*Log(N)), space complexity O(N)
+func useTwoPointers(nums []int, target int) int {
+	n := len(nums)
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	mod := 1e9 + 7
+	power := make([]int, n)
+	power[0] = 1
+	for i := 1; i < n; i++ {
+		power[i] = (power[i-1] * 2) % int(mod)
+	}
+	l, r := 0, n-1
+	var ans int
+	for l <= r {
+		if nums[l]+nums[r] > target {
+			r--
+		} else {
+			ans = (ans + power[r-l]) % int(mod)
+			l++
+		}
+	}
+	return ans
 }
--- 1498_number_of_seqs_that_satisify_the_given_sum_condition/sum_test.go
+++ 1498_number_of_seqs_that_satisify_the_given_sum_condition/sum_test.go
@@ -9,11 +9,11 @@ func TestNumSubSeqs(t *testing.T) {
 		target int
 		exp    int
 	}{
-		{"testcase1", []int{1, 1, 1, 1}, 2, 7},
+		{"testcase1", []int{1, 1, 1, 1}, 2, 15},
 		{"testcase2", []int{3, 5, 6, 7}, 9, 4},
 		{"testcase3", []int{3, 3, 6, 8}, 10, 6},
 		{"testcase4", []int{2, 3, 3, 4, 6, 7}, 12, 61},
-		{"testcase5", []int{5, 2, 4, 1, 7, 6, 8}, 16, 128},
+		{"testcase5", []int{5, 2, 4, 1, 7, 6, 8}, 16, 127},
 	}
 	for _, tt := range st {
 		t.Run(tt.name, func(t *testing.T) {
