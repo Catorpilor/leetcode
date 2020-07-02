7cb1ef60719d19374b3516e659b2e8c19d242ea3
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-02 21:10:31 +0800

    add testcases for 1499

---
+++ 1499_max_value_of_equation/mvoe.go
@@ -0,0 +1,5 @@
+package mvoe
+
+func findMaxValueOfEquation(points [][]int, k int) int {
+	return -1
+}
---
+++ 1499_max_value_of_equation/mvoe_test.go
@@ -0,0 +1,23 @@
+package mvoe
+
+import "testing"
+
+func TestFindMaxValueOfEquation(t *testing.T) {
+	st := []struct {
+		name   string
+		points [][]int
+		k, exp int
+	}{
+		{"testcase1", [][]int{[]int{1, 3}, []int{2, 0}, []int{5, 10}, []int{6, -10}}, 1, 4},
+		{"testcase2", [][]int{[]int{0, 0}, []int{3, 0}, []int{9, 2}}, 3, 3},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := findMaxValueOfEquation(tt.points, tt.k)
+			if out != tt.exp {
+				t.Fatalf("with input points:%v and k:%d wanted %d but got %d", tt.points, tt.k, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
