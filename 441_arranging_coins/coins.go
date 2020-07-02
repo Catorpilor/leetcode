0180d05a03fbee0b7ec5f1fbd2f09fcd3cda83d0
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-02 09:25:37 +0800

    solve 441 use binary search and math

--- 441_arranging_coins/coins.go
+++ 441_arranging_coins/coins.go
@@ -18,3 +18,21 @@ func useTraverse(n int) int {
 	}
 	return ans
 }
+
+// useBinarySearch time complexity O(lgN), space complexity O(1)
+func useBinarySearch(n int) int {
+	l, r := 0, n
+	var ans int
+	for l <= r {
+		ans = l + (r-l)/2
+		total := ans * (ans + 1) / 2
+		if total == n {
+			return ans
+		} else if total < n {
+			l = ans + 1
+		} else {
+			r = ans - 1
+		}
+	}
+	return r
+}
--- 441_arranging_coins/coins_test.go
+++ 441_arranging_coins/coins_test.go
@@ -22,3 +22,24 @@ func TestArrangeCoins(t *testing.T) {
 		})
 	}
 }
+
+func TestArrangeCoinsUseBs(t *testing.T) {
+	st := []struct {
+		name string
+		n    int
+		exp  int
+	}{
+		{"n=1", 1, 1},
+		{"n=0", 0, 0},
+		{"n=5", 5, 2},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useBinarySearch(tt.n)
+			if out != tt.exp {
+				t.Fatalf("with input n:%d wanted %d but got %d", tt.n, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
