146f81304b6a4926c90d13d9b0887edac3fa0468
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-08 21:57:45 +0800

    solve 15 use two pointers

--- 15_3_sum/sum.go
+++ 15_3_sum/sum.go
@@ -12,13 +12,13 @@ func threeSum(nums []int) [][]int {
 func useHashMap(nums []int) [][]int {
 	n := len(nums)
 	if n < 3 {
-		return [][]int{}
+		return nil
 	}
 	sort.Slice(nums, func(i, j int) bool {
 		return nums[i] < nums[j]
 	})
 	if nums[0] > 0 || nums[n-1] < 0 {
-		return [][]int{}
+		return nil
 	}
 	ans := make([][]int, 0, n)
 	set := make(map[int][]int, n)
@@ -51,4 +51,38 @@ func clear(set map[int][]int) {
 	for k := range set {
 		delete(set, k)
 	}
+}
+
+// useTwoPointers time complexity O(N^2), space complexity O(1)
+func useTwoPointers(nums []int) [][]int {
+	n := len(nums)
+	if n < 3 {
+		return nil
+	}
+	sort.Ints(nums)
+	if nums[0] > 0 && nums[n-1] < 0 {
+		return nil
+	}
+	ans := make([][]int, 0, n)
+	for i := 0; i < n; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, n-1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum < 0 {
+				l++
+			} else if sum > 0 {
+				r--
+			} else {
+				ans = append(ans, []int{nums[i], nums[l], nums[r]})
+				l++
+				for l < r && nums[l] == nums[l-1] {
+					l++
+				}
+			}
+		}
+	}
+	return ans
 }
--- 15_3_sum/sum_test.go
+++ 15_3_sum/sum_test.go
@@ -26,3 +26,24 @@ func TestThreeSum(t *testing.T) {
 		})
 	}
 }
+
+func TestThreeSumWithTwoPointers(t *testing.T) {
+	st := []struct {
+		name string
+		nums []int
+		exp  [][]int
+	}{
+		{"all positive", []int{1, 2, 3}, [][]int{}},
+		{"all negative", []int{-1, -2, -3}, [][]int{}},
+		{"testcase1", []int{-1, 0, 1, 2, -1, -4}, [][]int{[]int{-1, -1, 2}, []int{-1, 0, 1}}},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useTwoPointers(tt.nums)
+			if diff := cmp.Diff(tt.exp, out); diff != "" {
+				t.Fatalf("mismatch (-wanted, +got) %s", diff)
+			}
+			t.Log("pass")
+		})
+	}
+}
