f16239caa0fa543841d6998cbe8e48a9b2ad93f1
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-08 16:44:52 +0800

    solve 1499 use monotonic stack

--- 1499_max_value_of_equation/mvoe.go
+++ 1499_max_value_of_equation/mvoe.go
@@ -1,17 +1,23 @@
 package mvoe
 
-import "math"
+import (
+	"math"
+
+	"github.com/catorpilor/leetCode/utils"
+)
 
 func findMaxValueOfEquation(points [][]int, k int) int {
 	return useBruteForce(points, k)
+	// return useDeque(points, k)
+}
+
+type pair struct {
+	x, y int
 }
 
 // useBruteForce time complexity O(N^2), space complexity O(N)
 // got tle
 func useBruteForce(points [][]int, k int) int {
-	type pair struct {
-		x, y int
-	}
 	n := len(points)
 	pairs := make([]pair, 0, n)
 	for i := 0; i < n-1; i++ {
@@ -32,4 +38,25 @@ func useBruteForce(points [][]int, k int) int {
 		}
 	}
 	return ans
+}
+
+// useDeque time complexity O(N), space complexity O(N)
+func useDeque(points [][]int, k int) int {
+	// deq is a monotonic decreasing stack, dep.Front() is the largest one.
+	deq := utils.NewDeque()
+	res := math.MinInt32
+	for _, point := range points {
+		x, y := point[0], point[1]
+		for !deq.IsEmpty() && deq.Front().(pair).y < x-k {
+			deq.PopFront()
+		}
+		if !deq.IsEmpty() {
+			res = utils.Max(res, deq.Front().(pair).x+x+y)
+		}
+		for !deq.IsEmpty() && deq.Back().(pair).x <= y-x {
+			deq.PopBack()
+		}
+		deq.PushBack(pair{y - x, x})
+	}
+	return res
 }
