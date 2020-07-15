a0a3b655c498951f5421c58271876dab3e2ec515
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-15 16:17:07 +0800

    sovle 151 use built in functions

--- 151_reverse_words_in_a_string/words.go
+++ 151_reverse_words_in_a_string/words.go
@@ -1,5 +1,25 @@
 package words
 
+import (
+	"strings"
+)
+
 func reverseWords(s string) string {
-	return s
+	return useStraight(s)
+}
+
+// useStraight time complexity O(N), space complexity O(N)
+func useStraight(s string) string {
+	n := len(s)
+	if n < 1 {
+		return s
+	}
+	segs := strings.Fields(s)
+	l, r := 0, len(segs)-1
+	for l < r {
+		segs[l], segs[r] = segs[r], segs[l]
+		l++
+		r--
+	}
+	return strings.Join(segs, " ")
 }
