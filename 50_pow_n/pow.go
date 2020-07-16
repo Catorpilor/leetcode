3fafc09eb442dce43414cfd1c68ac0ff5e3a32a1
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-16 16:34:59 +0800

    solve 50 use equation x^n = x^n/2 * x^n/2

--- 50_pow_n/pow.go
+++ 50_pow_n/pow.go
@@ -1,5 +1,29 @@
 package pow
 
 func powN(x float64, n int) float64 {
-	return x
+	return useRecursion(x, n)
+}
+
+func useRecursion(x float64, n int) float64 {
+	return helper(x, n)
+}
+
+// helper time complexity O(logN) space complexity O(1)
+func helper(x float64, n int) float64 {
+	if x == 0 {
+		return 0
+	}
+	ans := 1.0
+	for n != 0 {
+		if n&1 != 0 {
+			if n < 0 {
+				ans /= x
+			} else {
+				ans *= x
+			}
+		}
+		x *= x
+		n /= 2
+	}
+	return ans
 }
