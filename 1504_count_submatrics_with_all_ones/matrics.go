fc2d995d619a7918cdb2faa26ae5de4f43e8f106
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-17 22:10:36 +0800

    solve 1504 use stack

--- 1504_count_submatrics_with_all_ones/matrics.go
+++ 1504_count_submatrics_with_all_ones/matrics.go
@@ -44,3 +44,54 @@ func countOneRow(arr []int) int {
 	}
 	return ans
 }
+
+// useStack time complexity O(MN), space complexity O(N)
+func useStack(mat [][]int) int {
+	m := len(mat)
+	if m < 1 {
+		return 0
+	}
+	n := len(mat[0])
+	if n < 1 {
+		return 0
+	}
+	var res int
+	h := make([]int, n)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if mat[i][j] != 0 {
+				h[j] = h[j] + 1
+			} else {
+				h[j] = 0
+			}
+		}
+		res += helper(h)
+	}
+	return res
+}
+
+func helper(arr []int) int {
+	n := len(arr)
+	local := make([]int, n)
+	st := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nst := len(st)
+		for nst > 0 && arr[st[nst-1]] >= arr[i] {
+			nst--
+			st = st[:nst]
+		}
+		if nst > 0 {
+			preIndex := st[nst-1]
+			local[i] = arr[preIndex]
+			local[i] += arr[i] * (i - preIndex)
+		} else {
+			local[i] = arr[i] * (i + 1)
+		}
+		st = append(st, i)
+	}
+	var res int
+	for _, c := range local {
+		res += c
+	}
+	return res
+}
--- 1504_count_submatrics_with_all_ones/matrics_test.go
+++ 1504_count_submatrics_with_all_ones/matrics_test.go
@@ -23,3 +23,25 @@ func TestCountMatrics(t *testing.T) {
 		})
 	}
 }
+
+func TestCountMatricsUseStack(t *testing.T) {
+	st := []struct {
+		name string
+		mat  [][]int
+		exp  int
+	}{
+		{"empty mat", nil, 0},
+		{"testcase1", [][]int{[]int{1, 1, 1, 1, 1, 1}}, 21},
+		{"testcase2", [][]int{[]int{1, 0, 1}, []int{1, 1, 0}, []int{1, 1, 0}}, 13},
+		{"testcase3", [][]int{[]int{0, 1, 1, 0}, []int{0, 1, 1, 1}, []int{1, 1, 1, 0}}, 24},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useStack(tt.mat)
+			if out != tt.exp {
+				t.Fatalf("with input mat:%v, wanted %d, but got %d", tt.mat, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
