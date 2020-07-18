41decff1207010f2cae1a27bcc04a11ba30ea1b3
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-18 22:09:45 +0800

    sovle 1510 use dp

--- 1510_stone_game_iv/stone.go
+++ 1510_stone_game_iv/stone.go
@@ -1,5 +1,23 @@
 package stone
 
 func winnerSquareGame(n int) bool {
-	return false
+	return useDP(n)
+}
+
+// useDP time complexity O(N^3/2), space complexity O(N)
+func useDP(n int) bool {
+	// dp[i] means the result for n = i.
+	// if there is any k that dp[i - k * k] == false,
+	// it means we can make the other lose the game,
+	// so we can win the game  dp[i] = true.
+	dp := make([]bool, n+1)
+	for i := 1; i <= n; i++ {
+		for k := 1; k*k <= i; k++ {
+			if !dp[i-k*k] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[n]
 }
