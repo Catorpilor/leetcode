d31e2d5af502fac47f6467422c552ca93b1f538d
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-20 19:44:31 +0800

    solve 1518 use math

--- 1518_watter_bottles/wb.go
+++ 1518_watter_bottles/wb.go
@@ -1,5 +1,18 @@
 package wb
 
 func watterBottles(numBottles, numExchange int) int {
-	return numBottles
+	return useMath(numBottles, numExchange)
+}
+
+// useMath time complexity O(1), space compelxity O(1)
+func useMath(numBottles, numExchange int) int {
+	ans := numBottles
+	var rem int
+	for numBottles >= numExchange {
+		rem = numBottles % numExchange
+		numBottles /= numExchange
+		ans += numBottles
+		numBottles += rem
+	}
+	return ans
 }
