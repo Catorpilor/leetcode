e9271d76dafbda34675de471919b224a837e5ee3
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-21 22:49:38 +0800

    my original solution which was wrong

--- 1494_parallel_courses_ii/course.go
+++ 1494_parallel_courses_ii/course.go
@@ -1,5 +1,55 @@
 package course
 
+import "fmt"
+
 func minSems(n int, deps [][]int, k int) int {
-	return n
+	return useTopSort(n, deps, k)
+}
+
+// useTopSort
+func useTopSort(n int, dependencies [][]int, k int) int {
+	nd := len(dependencies)
+	if nd == 0 {
+		return n/k + n%k
+	}
+	adj := make([][]int, n+1)
+	indgree := make([]int, n+1)
+	// we need to calculagte the out degrees
+	for _, dep := range dependencies {
+		p, q := dep[0], dep[1]
+		adj[p] = append(adj[p], q)
+		indgree[q]++
+	}
+	visited := make([]bool, n+1)
+	var ans, count int
+	// find ingree == 0
+	for {
+		starts := make([]int, 0, n+1)
+		for i := 1; i <= n; i++ {
+			if indgree[i] == 0 && !visited[i] {
+				starts = append(starts, i)
+				visited[i] = true
+				count++
+			}
+		}
+		fmt.Printf("pick starts: %v, count: %d\n", starts, count)
+		// fmt.Println(starts)
+		// this logic is wrong.....
+		ns := len(starts)
+		if ns <= k {
+			ans++
+		} else {
+			ans += ns/k + ns%k
+		}
+		for i := range starts {
+			child := adj[starts[i]]
+			for j := range child {
+				indgree[child[j]]--
+			}
+		}
+		if count == n {
+			break
+		}
+	}
+	return ans
 }
--- 1494_parallel_courses_ii/course_test.go
+++ 1494_parallel_courses_ii/course_test.go
@@ -13,6 +13,9 @@ func TestMinimumSems(t *testing.T) {
 		{"no deps", 5, nil, 2, 3},
 		{"testcase1", 4, [][]int{[]int{2, 1}, []int{3, 1}, []int{1, 4}}, 2, 3},
 		{"testcase2", 5, [][]int{[]int{2, 1}, []int{3, 1}, []int{4, 1}, []int{1, 5}}, 2, 4},
+		{"failed1", 5, [][]int{[]int{5, 3}, []int{2, 3}, []int{5, 1}, []int{4, 1}, []int{2, 4}, []int{3, 1}}, 5, 3},
+		{"failed2", 12, [][]int{[]int{1, 2}, []int{1, 3}, []int{7, 5}, []int{7, 6}, []int{4, 8}, []int{8, 9}, []int{9, 10},
+			[]int{10, 11}, []int{11, 12}}, 2, 6},
 	}
 
 	for _, tt := range st {
