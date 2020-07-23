87fced39a44c02f7e650ac65cc3d41968a727b44
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-23 16:47:17 +0800

    solve 260 use bit operation

--- 260_single_number_iii/number.go
+++ 260_single_number_iii/number.go
@@ -1,5 +1,24 @@
 package number
 
 func singleNumber(nums []int) []int {
-	return []int{0, 0}
+	return useBit(nums)
+}
+
+// useBit time complexity O(N), space complexity O(1)
+func useBit(nums []int) []int {
+	var diff int
+	for i := range nums {
+		diff ^= nums[i]
+	}
+	// pick the rightmost 1 bit
+	diff &= -diff
+	ans := make([]int, 2)
+	for i := range nums {
+		if nums[i]&diff == 0 {
+			ans[0] ^= nums[i]
+		} else {
+			ans[1] ^= nums[i]
+		}
+	}
+	return ans
 }
