b9654e723f46b1430cc43a8907c746bcfeb81f7d
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-27 13:26:18 +0800

    sovle 1529 use brute force

--- 1529_bulb_switcher_iv/bulb.go
+++ 1529_bulb_switcher_iv/bulb.go
@@ -1,5 +1,37 @@
 package bulb
 
 func minFlips(target string) int {
-	return 0
+	return useBruteForce(target)
+}
+
+
+// useBruteForce time complexity O(N), space complexity O(1)
+func useBruteForce(target string) int {
+	var ans int
+	n := len(target)
+	if n < 1 {
+		return ans
+	}
+	var first int
+	for {
+		for ; first < n; first++ {
+			if target[first] == '0'{
+				continue
+			}
+			ans++
+			break
+		}
+		first++
+		if first >= n {
+			break
+		}
+		for ; first < n; first++ {
+			if target[first] == '1' {
+				continue
+			}
+			ans++
+			break
+		}
+	}
+	return ans
 }
