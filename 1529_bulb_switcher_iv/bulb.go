944e0d2a813539f1049557629207af74c56e7771
Catorpilor <jiaojiangonghitster@gmail.com>
2020-07-28 13:48:09 +0800

    solve 1529 use a simpler greedy approach

--- 1529_bulb_switcher_iv/bulb.go
+++ 1529_bulb_switcher_iv/bulb.go
@@ -1,12 +1,11 @@
 package bulb
 
 func minFlips(target string) int {
-	return useBruteForce(target)
+	return useGreedy(target)
 }
 
-
 // useBruteForce time complexity O(N), space complexity O(1)
-func useBruteForce(target string) int {
+func useGreedy(target string) int {
 	var ans int
 	n := len(target)
 	if n < 1 {
@@ -15,7 +14,7 @@ func useBruteForce(target string) int {
 	var first int
 	for {
 		for ; first < n; first++ {
-			if target[first] == '0'{
+			if target[first] == '0' {
 				continue
 			}
 			ans++
@@ -31,6 +30,24 @@ func useBruteForce(target string) int {
 			}
 			ans++
 			break
+		}
+	}
+	return ans
+}
+
+// simple is a simpler approach for useGreedy, just use one loop.
+// time complexity O(N), space complexity O(1)
+func simple(target string) int {
+	var ans int
+	start := '0'
+	for _, c := range target {
+		if c != start {
+			ans++
+			if start == '0' {
+				start = '1'
+			} else {
+				start = '0'
+			}
 		}
 	}
 	return ans
--- 1529_bulb_switcher_iv/bulb_test.go
+++ 1529_bulb_switcher_iv/bulb_test.go
@@ -2,12 +2,11 @@ package bulb
 
 import "testing"
 
-
-func TestMinFlips(t *testing.T){
-	st := []struct{
-		name string
+func TestMinFlips(t *testing.T) {
+	st := []struct {
+		name   string
 		target string
-		exp int
+		exp    int
 	}{
 		{"testcase1", "10111", 3},
 		{"testcase2", "000", 0},
@@ -15,8 +14,30 @@ func TestMinFlips(t *testing.T){
 		{"testcase4", "001011101", 5},
 	}
 	for _, tt := range st {
-		t.Run(tt.name, func(t *testing.T){
+		t.Run(tt.name, func(t *testing.T) {
 			out := minFlips(tt.target)
+			if out != tt.exp {
+				t.Fatalf("with input target:%s wanted %d but got %d", tt.target, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
+
+func TestMinFlipsUseSimple(t *testing.T) {
+	st := []struct {
+		name   string
+		target string
+		exp    int
+	}{
+		{"testcase1", "10111", 3},
+		{"testcase2", "000", 0},
+		{"testcase3", "101", 3},
+		{"testcase4", "001011101", 5},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := simple(tt.target)
 			if out != tt.exp {
 				t.Fatalf("with input target:%s wanted %d but got %d", tt.target, tt.exp, out)
 			}
