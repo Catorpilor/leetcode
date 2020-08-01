cebc355e33b749a7dd7557f21266c242f1326377
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-01 18:21:28 +0800

    sovle 520 use one pass

--- 520_detect_caps/caps.go
+++ 520_detect_caps/caps.go
@@ -1,5 +1,26 @@
 package caps
 
 func detectCaps(word string) bool {
-	return false
+	return useOnepass(word)
+}
+
+// useOnepass time complexity O(N), space complexity O(1)
+func useOnepass(word string) bool {
+	seenNonCaps, allCaps := false, false
+	for i, c := range word {
+		if c >= 'A' && c <= 'Z' {
+			if i > 0 && seenNonCaps {
+				return false
+			}
+			if i > 0 {
+				allCaps = true
+			}
+		} else {
+			seenNonCaps = true
+			if allCaps {
+				return false
+			}
+		}
+	}
+	return true
 }
