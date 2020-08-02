406e7fd757f442c98bc06f8288084f2a8d3a69c8
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-02 19:45:12 +0800

    solve 1535 use two passes with stack

--- 1535_find_winner_of_an_array_game/winner.go
+++ 1535_find_winner_of_an_array_game/winner.go
@@ -1,5 +1,49 @@
 package winner
 
+import "github.com/catorpilor/leetcode/utils"
+
 func winnerOfGame(arr []int, k int) int {
-	return 0
+	return useTwoPassWithStack(arr, k)
+}
+
+// useStack time complexity O(N), space complexity O(N)
+func useTwoPassWithStack(arr []int, k int) int {
+	1n := len(arr)
+	posOfPeeks := make([]int, 0, n)
+	var maxV int
+	for i := 0; i < n; i++ {
+		if i > 0 && arr[i] < arr[i-1] {
+			continue
+		}
+		if i < n-1 && arr[i] < arr[i+1] {
+			continue
+		}
+		posOfPeeks = append(posOfPeeks, i)
+		maxV = utils.Max(maxV, arr[i])
+	}
+	// len(posOfPeeks) means the arr is strictly increasing or decreasing
+	if len(posOfPeeks) == 1 || n < k {
+		return maxV
+	}
+	st := make([]int, 0, n)
+	count := make([]int, n)
+	for i := 0; i < n; i++ {
+		nst := len(st)
+		if nst > 0 {
+			top := st[nst-1]
+			if arr[top] > arr[i] {
+				count[top]++
+				if count[top] >= k {
+					return arr[top]
+				}
+				continue
+			} else {
+				st = st[:nst-1]
+				nst--
+				count[i]++
+			}
+		}
+		st = append(st, i)
+	}
+	return maxV
 }
