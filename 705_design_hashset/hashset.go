36073c5bc2b469be3d146fda0fcea607ffe85a16
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-03 20:41:45 +0800

    solve 705 use cheated 1d array

--- 705_design_hashset/hashset.go
+++ 705_design_hashset/hashset.go
@@ -1,17 +1,22 @@
 package hashset
 
-type MyHashSet struct{}
+type MyHashSet struct {
+	// cheated with 1d array
+	store [1000001]bool
+}
 
 func Constructor() *MyHashSet {
 	return &MyHashSet{}
 }
 
 func (this *MyHashSet) Add(key int) {
+	this.store[key] = true
 }
 
 func (this *MyHashSet) Contains(key int) bool {
-	return false
+	return this.store[key]
 }
 
 func (this *MyHashSet) Remove(key int) {
+	this.store[key] = false
 }
