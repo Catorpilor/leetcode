4db516651e53b88571b9fa6256fada6726ff0a23
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-03 20:38:46 +0800

    add testcases for 705

---
+++ 705_design_hashset/hashset.go
@@ -0,0 +1,17 @@
+package hashset
+
+type MyHashSet struct{}
+
+func Constructor() *MyHashSet {
+	return &MyHashSet{}
+}
+
+func (this *MyHashSet) Add(key int) {
+}
+
+func (this *MyHashSet) Contains(key int) bool {
+	return false
+}
+
+func (this *MyHashSet) Remove(key int) {
+}
