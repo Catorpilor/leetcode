f890654f7f48f6e3ffdaa7dffb2d68a473f3bee2
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-07 15:29:30 +0800

    solve 113 use dfs

--- 113_path_sum_ii/path.go
+++ 113_path_sum_ii/path.go
@@ -33,3 +33,28 @@ func helper(node *utils.TreeNode, k int, nodes *[]int, res *[][]int) {
 	}
 	*nodes = (*nodes)[:n]
 }
+
+func useDFS(root *utils.TreeNode, sum int) [][]int {
+	var res [][]int
+	helper2(&res, root, sum, 0, []int{})
+	return res
+}
+
+func helper2(res *[][]int, root *utils.TreeNode, sum, cur int, path []int) {
+	if root == nil {
+		return
+	}
+	path = append(path, root.Val)
+	if root.Left == nil && root.Right == nil {
+		if root.Val+cur == sum {
+			local := make([]int, len(path))
+			copy(local, path)
+			*res = append((*res), local)
+			return
+		}
+	} else {
+		helper2(res, root.Left, sum, cur+root.Val, path)
+		helper2(res, root.Right, sum, cur+root.Val, path)
+	}
+	return
+}
--- 113_path_sum_ii/path_test.go
+++ 113_path_sum_ii/path_test.go
@@ -35,3 +35,31 @@ func TestPathSum(t *testing.T) {
 		})
 	}
 }
+
+func TestPathSumUseDFS(t *testing.T) {
+	st := []struct {
+		name string
+		nums []int
+		sum  int
+		exp  [][]int
+	}{
+		{"empty", []int{}, 2, nil},
+		{"single", []int{1}, 2, nil},
+		{"test1", []int{5, 4, 8}, 4, nil},
+		{"test2", []int{5, 4, 8, 11, math.MinInt32, 13, 4, 7, 2, math.MinInt32, math.MinInt32, 5, 1}, 22, [][]int{
+			[]int{5, 4, 11, 2}, []int{5, 8, 4, 5},
+		}},
+		{"failed 1", []int{-2, math.MinInt32, -3}, -5, [][]int{[]int{-2, -3}}},
+		{"failed 2", []int{1, -2, -3, 1, 3, -2, math.MinInt32, -1}, 2, [][]int{[]int{1, -2, 3}}},
+	}
+	for _, c := range st {
+		t.Run(c.name, func(t *testing.T) {
+			root := utils.ConstructTree(c.nums)
+			ret := useDFS(root, c.sum)
+			if !reflect.DeepEqual(ret, c.exp) {
+				t.Fatalf("expected %v but got %v, with input %v and %d",
+					c.exp, ret, c.nums, c.sum)
+			}
+		})
+	}
+}
