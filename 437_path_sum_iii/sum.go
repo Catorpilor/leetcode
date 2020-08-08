40f77a9ad16f3e81a613c62a7a5fad040bd585c0
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-08 18:50:44 +0800

    solve 437 based on discussion use brute force

--- 437_path_sum_iii/sum.go
+++ 437_path_sum_iii/sum.go
@@ -3,5 +3,24 @@ package sum
 import "github.com/catorpilor/leetcode/utils"
 
 func pathSum(root *utils.TreeNode, sum int) int {
-	return 0
+	return useBruteForce(root, sum)
+}
+
+// useBruteForce space complexity O(N), time complexity O(N^2) worst case, and O(N*Log(N)) in best case.
+func useBruteForce(root *utils.TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+	return helper(root, sum) + helper(root.Left, sum) + helper(root.Right, sum)
+}
+
+func helper(root *utils.TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+	extra := 0
+	if root.Val == sum {
+		extra = 1
+	}
+	return extra + helper(root.Left, sum-root.Val) + helper(root.Right, sum-root.Val)
 }
