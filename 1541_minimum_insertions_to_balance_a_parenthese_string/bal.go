befaa4af7d9572758602a249b75cbdb1ea63e9a2
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-12 14:23:27 +0800

    solve 1541 use stack

--- 1541_minimum_insertions_to_balance_a_parenthese_string/bal.go
+++ 1541_minimum_insertions_to_balance_a_parenthese_string/bal.go
@@ -1,5 +1,40 @@
 package bal
 
 func minInserts(s string) int {
-	return 0
+	return useStack(s)
+}
+
+// useStack time complexity O(N), space complexity O(N)
+func useStack(s string) int {
+	n := len(s)
+	st := make([]int, 0, n)
+	var ans int
+	for i := 0; i < n; i++ {
+		if s[i] == '(' {
+			st = append(st, i)
+			continue
+		}
+		nst := len(st)
+		if i < n-1 && s[i+1] == ')' {
+			if nst > 0 {
+				st = st[:nst-1]
+			} else {
+				// add one left parenthese
+				ans++
+			}
+			// update i skip 1
+			i++
+		} else {
+			if nst > 0 {
+				st = st[:nst-1]
+				// add one right parenthese
+				ans++
+			} else {
+				// add one left parenthese and right parentheses
+				ans += 2
+			}
+		}
+	}
+
+	return ans + 2*len(st)
 }
