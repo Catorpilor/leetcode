58344f6469e82d07db5110edb23472a7967c1c9e
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-15 19:22:13 +0800

    use greedy and hashmap to solve 1546

--- 1546_maximum_number_of_non_overlapping_subarrays_with_sum_equals_target/targetsum.go
+++ 1546_maximum_number_of_non_overlapping_subarrays_with_sum_equals_target/targetsum.go
@@ -1,5 +1,7 @@
 package targetsum
 
+import "sort"
+
 func maxSubarray(nums []int, target int) int {
 	return useHashmap(nums, target)
 }
@@ -25,4 +27,38 @@ func clear(set map[int]int) {
 	for k := range set {
 		delete(set, k)
 	}
+}
+
+// useHashmapAndGreedy time complexity O(N*Log(N)), space complexity O(N)
+func useHashmapAndGreedy(nums []int, target int) int {
+	n := len(nums)
+	set := make(map[int]int, n)
+	// key -> sum, val -> start index.
+	set[0] = -1
+	// pairs store the subarraies with the sum equals the target
+	pairs := make([][]int, 0, n)
+	var sum int
+	for i := 0; i < n; i++ {
+		sum += nums[i]
+		if v, exists := set[sum-target]; exists {
+			pairs = append(pairs, []int{v + 1, i})
+		}
+		set[sum] = i
+	}
+	np := len(pairs)
+	if np <= 1 {
+		return np
+	}
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i][1] < pairs[j][1]
+	})
+	ans := 1
+	end := pairs[0][1]
+	for i := 1; i < np; i++ {
+		if pairs[i][0] > end {
+			end = pairs[i][1]
+			ans++
+		}
+	}
+	return ans
 }
--- 1546_maximum_number_of_non_overlapping_subarrays_with_sum_equals_target/targetsum_test.go
+++ 1546_maximum_number_of_non_overlapping_subarrays_with_sum_equals_target/targetsum_test.go
@@ -24,3 +24,26 @@ func TestMaxSubarray(t *testing.T) {
 		})
 	}
 }
+
+func TestMaxSubarrayWithGreedy(t *testing.T) {
+	st := []struct {
+		name   string
+		nums   []int
+		target int
+		exp    int
+	}{
+		{"testcase1", []int{1, 1, 1, 1, 1}, 2, 2},
+		{"testcase2", []int{-1, 3, 5, 1, 4, 2, -9}, 6, 2},
+		{"testcase3", []int{-2, 6, 6, 3, 5, 4, 1, 2, 8}, 10, 3},
+		{"testcase4", []int{0, 0, 0}, 0, 3},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useHashmapAndGreedy(tt.nums, tt.target)
+			if out != tt.exp {
+				t.Fatalf("with input nums:%v and target:%d wanted %d but got %d", tt.nums, tt.target, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
