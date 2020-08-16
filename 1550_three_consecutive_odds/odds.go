50247580fb812f9513327c03e3b9b1aac074366d
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-16 14:08:06 +0800

    solve 1550 use one pass

--- 1550_three_consecutive_odds/odds.go
+++ 1550_three_consecutive_odds/odds.go
@@ -1,5 +1,23 @@
 package odds
 
 func isStatisfy(nums []int) bool {
+	return useOnepass(nums)
+}
+
+// useOnepass time complexity O(N), space complexity O(1)
+func useOnepass(nums []int) bool {
+	var count int
+	for i := range nums {
+		if nums[i]&1 != 0 {
+			count++
+			if count == 3 {
+				return true
+
+			}
+			continue
+		}
+		count = 0
+	}
 	return false
+
 }
