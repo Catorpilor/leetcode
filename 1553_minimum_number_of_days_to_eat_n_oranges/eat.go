cdfa3571f10af15e7fbc6ca2037e93815ac32911
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-17 08:51:18 +0800

    solve 1553 use hashmap and recursion

--- 1553_minimum_number_of_days_to_eat_n_oranges/eat.go
+++ 1553_minimum_number_of_days_to_eat_n_oranges/eat.go
@@ -1,5 +1,25 @@
 package eat
 
+import (
+	"github.com/catorpilor/leetcode/utils"
+)
+
 func minDays(n int) int {
-	return 0
+	return useHashmap(n)
+}
+
+// useHashmap time complexity O(lgN), space complexity O(N)
+func useHashmap(n int) int {
+	set := make(map[int]int, n)
+	return helper(set, n)
+}
+
+func helper(set map[int]int, n int) int {
+	if n <= 1 {
+		return n
+	}
+	if _, exists := set[n]; !exists {
+		set[n] = 1 + utils.Min(n%2+helper(set, n/2), n%3+helper(set, n/3))
+	}
+	return set[n]
 }
