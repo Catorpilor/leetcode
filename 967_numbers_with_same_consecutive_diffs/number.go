78a943185224c02803b4852f40a3e2a881ed3605
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-18 19:46:04 +0800

    simplify the code for 967

--- 967_numbers_with_same_consecutive_diffs/number.go
+++ 967_numbers_with_same_consecutive_diffs/number.go
@@ -10,32 +10,21 @@ func useBruteForce(n, k int) []int {
 	for i := range base {
 		base[i] = i
 	}
-	if n == 1 {
-		if k == 0 {
-			return base
-		}
-		return nil
-	}
-	base = base[1:]
-	pre := 10
 	for p := 2; p <= n; p++ {
 		// fmt.Printf("base:%v, pre:%d\n", base, pre)
 		tmp := []int{}
 		for i := range base {
 			orig := base[i] % 10
-			base[i] *= 10
 			// fmt.Printf("orig:%d, base[i]:%d\n", orig, base[i])
-			if orig >= k {
-				tmp = append(tmp, base[i]+orig-k)
+			if base[i] > 0 && orig >= k {
+				tmp = append(tmp, base[i]*10+orig-k)
 			}
 			// only add when k!=0 remove duplicates
-			if orig+k <= 9 && k != 0 {
-				tmp = append(tmp, base[i]+orig+k)
+			if base[i] > 0 && orig+k <= 9 && k != 0 {
+				tmp = append(tmp, base[i]*10+orig+k)
 			}
 		}
 		base = tmp
-		pre *= 10
 	}
-	// fmt.Println(base)
 	return base
 }
