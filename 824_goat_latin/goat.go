3d883cb88c9d03e9f2687909d301798f56bd3543
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-20 13:54:00 +0800

    sovle 824 use pretty straight way

--- 824_goat_latin/goat.go
+++ 824_goat_latin/goat.go
@@ -1,5 +1,38 @@
 package goat
 
+import (
+	"bytes"
+	"strings"
+)
+
 func toGoatLatin(s string) string {
-	return s
+	return useStraight(s)
+}
+
+func useStraight(s string) string {
+	sb := []byte(s)
+	sbs := bytes.Fields(sb)
+	var bb bytes.Buffer
+	for i, w := range sbs {
+		if isVowel(w[0]) {
+			bb.Write(w)
+		} else {
+			w = append(w, w[0])
+			bb.Write(w[1:])
+		}
+		bb.WriteString("ma" + strings.Repeat("a", i+1))
+		if i < len(sbs)-1 {
+			bb.WriteString(" ")
+		}
+	}
+	return bb.String()
+}
+
+func isVowel(b byte) bool {
+	switch b {
+	case 'a', 'e', 'o', 'i', 'u', 'A', 'E', 'I', 'O', 'U':
+		return true
+	default:
+		return false
+	}
 }
