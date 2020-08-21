18fbb0360396d2edb7afe6f7a5274c255af1b6d5
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-21 16:52:09 +0800

    solve 905 use one pass

--- 905_sort_array_by_parity/array.go
+++ 905_sort_array_by_parity/array.go
@@ -1,5 +1,24 @@
 package array
 
 func sortArrayByParity(arr []int) []int {
+	return useOnePass(arr)
+}
+
+// useOnePass time complexity O(N), space complexity O(1)
+func useOnePass(arr []int) []int {
+	n := len(arr)
+	l, r := 0, n-1
+	for l < r {
+		if arr[l]%2 == 0 {
+			l++
+		} else {
+			arr[l], arr[r] = arr[r], arr[l]
+		}
+		if arr[r]%2 != 0 {
+			r--
+		} else {
+			arr[r], arr[l] = arr[l], arr[r]
+		}
+	}
 	return arr
 }
