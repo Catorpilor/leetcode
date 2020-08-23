08f6669184016f94b2d5b05a069a613376b73d8f
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-23 10:13:12 +0800

    solve 1556 use straight forward way

--- 1556_thousand_separator/ts.go
+++ 1556_thousand_separator/ts.go
@@ -1,5 +1,34 @@
 package ts
 
+import (
+	"bytes"
+	"strconv"
+)
+
 func addSeps(num int) string {
-	return ""
+	return useStraight(num)
+}
+
+// useStraight time complexity O(N), space complexity O(N)
+func useStraight(num int) string {
+	s := strconv.Itoa(num)
+	var tmp []string
+	ns := len(s)
+	end := ns
+	for ns >= 3 {
+		tmp = append(tmp, s[ns-3:end])
+		end = ns - 3
+		ns -= 3
+	}
+	if ns > 0 {
+		tmp = append(tmp, s[:ns])
+	}
+	var sb bytes.Buffer
+	for k := len(tmp) - 1; k >= 0; k-- {
+		sb.WriteString(tmp[k])
+		if k > 0 {
+			sb.WriteByte('.')
+		}
+	}
+	return sb.String()
 }
