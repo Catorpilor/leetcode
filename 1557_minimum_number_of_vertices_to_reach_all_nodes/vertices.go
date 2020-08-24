a452bc3ac0bb708b83d0104289c5460fbecb2f26
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-24 09:48:40 +0800

    solve 1557 use in-degree

--- 1557_minimum_number_of_vertices_to_reach_all_nodes/vertices.go
+++ 1557_minimum_number_of_vertices_to_reach_all_nodes/vertices.go
@@ -1,5 +1,20 @@
 package verticies
 
 func minimumVerticies(n int, edge [][]int) []int {
-	return nil
+	return useIndegree(n, edge)
+}
+
+// useIndegree time complexity O(E), space complexity O(N)
+func useIndegree(n int, edges [][]int) []int {
+	indegree := make([]int, n)
+	for _, edge := range edges {
+		indegree[edge[1]]++
+	}
+	ans := make([]int, 0, n)
+	for i := range indegree {
+		if indegree[i] == 0 {
+			ans = append(ans, i)
+		}
+	}
+	return ans
 }
