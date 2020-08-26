d83740ffbad3ea260a10376a4d2ffbc49a02b4a1
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-26 10:12:41 +0800

    solve 1561 use sort and two pointers

--- 1561_maximum_number_of_coins_you_can_get/coins.go
+++ 1561_maximum_number_of_coins_you_can_get/coins.go
@@ -1,5 +1,20 @@
 package coins
 
+import "sort"
+
 func maxCoins(piles []int) int {
-	return 0
+	return useSort(piles)
+}
+
+// useSort time complexity O(N*log(N)), space complexity O(1)
+func useSort(piles []int) int {
+	sort.Ints(piles)
+	l, r := 0, len(piles)-1
+	var ans int
+	for l < r {
+		ans += piles[r-1]
+		l++
+		r -= 2
+	}
+	return ans
 }
