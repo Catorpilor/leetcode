e7d178b28dd1db1d7bef01de3995e26ce1084d66
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-26 09:25:18 +0800

    add testcases for 144

---
+++ 144_binary_tree_preorder_traversal/preorder.go
@@ -0,0 +1,7 @@
+package preorder
+
+import "github.com/catorpilor/leetcode/utils"
+
+func preorderTraversal(root *utils.TreeNode) []int {
+	return nil
+}
---
+++ 144_binary_tree_preorder_traversal/preorder_test.go
@@ -0,0 +1,30 @@
+package preorder
+
+import (
+	"math"
+	"testing"
+
+	"github.com/catorpilor/leetcode/utils"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestPreorderTraversal(t *testing.T) {
+	st := []struct {
+		name string
+		root *utils.TreeNode
+		exp  []int
+	}{
+		{"nil tree", nil, nil},
+		{"single node", utils.ConstructTree([]int{1}), []int{1}},
+		{"testcase1", utils.ConstructTree([]int{1, math.MinInt32, 2, 3}), []int{1, 2, 3}},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := preorderTraversal(tt.root)
+			if diff := cmp.Diff(tt.exp, out); diff != "" {
+				t.Fatalf("(-wanted, +got) %s", diff)
+			}
+			t.Log("pass")
+		})
+	}
+}
