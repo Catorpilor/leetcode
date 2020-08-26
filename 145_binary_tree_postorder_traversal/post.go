76dcab87798bfe78d5a1148d65b21732d81c5127
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-26 13:52:15 +0800

    solve 145 use stack with double push which is brilliant

--- 145_binary_tree_postorder_traversal/post.go
+++ 145_binary_tree_postorder_traversal/post.go
@@ -21,3 +21,31 @@ func helper(root *utils.TreeNode, ans *[]int) {
 	helper(root.Right, ans)
 	*ans = append((*ans), root.Val)
 }
+
+// useStack time complexity O(N), space complexity O(N)
+func useStack(root *utils.TreeNode) []int {
+	var ans []int
+	if root == nil {
+		return ans
+	}
+	st := make([]*utils.TreeNode, 0, 1000)
+	st = append(st, root, root)
+	n := len(st)
+	for n != 0 {
+		top := st[n-1]
+		st = st[:n-1]
+		n--
+		if n != 0 && st[n-1] == top {
+			if top.Right != nil {
+				st = append(st, top.Right, top.Right)
+			}
+			if top.Left != nil {
+				st = append(st, top.Left, top.Left)
+			}
+		} else {
+			ans = append(ans, top.Val)
+		}
+		n = len(st)
+	}
+	return ans
+}
--- 145_binary_tree_postorder_traversal/post_test.go
+++ 145_binary_tree_postorder_traversal/post_test.go
@@ -29,3 +29,25 @@ func TestPostorderTraverse(t *testing.T) {
 		})
 	}
 }
+
+func TestPostorderTraverseUseStack(t *testing.T) {
+	st := []struct {
+		name string
+		root *utils.TreeNode
+		exp  []int
+	}{
+		{"nil root", nil, nil},
+		{"single node", utils.ConstructTree([]int{1}), []int{1}},
+		{"testcase1", utils.ConstructTree([]int{1, math.MinInt32, 2, 3}), []int{3, 2, 1}},
+		{"testcase2", utils.ConstructTree([]int{1, 4, 3, 2}), []int{2, 4, 3, 1}},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useStack(tt.root)
+			if diff := cmp.Diff(tt.exp, out); diff != "" {
+				t.Fatalf("(-wanted, +got) %s", diff)
+			}
+			t.Log("pass")
+		})
+	}
+}
