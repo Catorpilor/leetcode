8fab315cfd49475743d62192a7b64a15e7d982f5
Catorpilor <jiaojiangonghitster@gmail.com>
2020-08-27 10:32:55 +0800

    solve 1551 use two pointers

--- 1551_minimum_operations_to_make_array_equal/array.go
+++ 1551_minimum_operations_to_make_array_equal/array.go
@@ -1,5 +1,19 @@
 package array
 
 func minOps(n int) int {
-	return n
+	return useTwoPointers(n)
+}
+
+// useTwoPointers time complexity O(N), space complexity O(1)
+func useTwoPointers(n int) int {
+	// the array is arithmetic
+	avg := n
+	l, r := 0, n-1
+	var ans int
+	for l < r {
+		ans += 2*r + 1 - avg
+		l++
+		r--
+	}
+	return ans
 }
