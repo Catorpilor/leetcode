c544514dd62c9440e30fcb2239be471ec87c998c
Catorpilor <jiaojiangonghitster@gmail.com>
2020-09-03 21:30:02 +0800

    solve 470 use generic solution

--- 470_implement_rand10_using_rand7/rand10.go
+++ 470_implement_rand10_using_rand7/rand10.go
@@ -3,7 +3,16 @@ package rand10
 import "math/rand"
 
 func rand10() int {
-	return 0
+	return useFormular()
+}
+
+// useFormular time complexity O(1), space complexity O(1)
+func useFormular() int {
+	res := 40
+	for res >= 40 {
+		res = 7*(rand7()-1) + (rand7() - 1)
+	}
+	return res%10 + 1
 }
 
 func rand7() int {
