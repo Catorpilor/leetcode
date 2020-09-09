db126a312744553ddf884c965b6affd62a24b98a
Catorpilor <jiaojiangonghitster@gmail.com>
2020-09-09 12:21:34 +0800

    solve 1022 use backtracking

--- 1022_sum_of_root_to_leaf_binary_numbers/sum.go
+++ 1022_sum_of_root_to_leaf_binary_numbers/sum.go
@@ -3,5 +3,37 @@ package sum
 import "github.com/catorpilor/leetcode/utils"
 
 func pathSum(root *utils.TreeNode) int {
-	return 0
+	return useBackTracking(root)
+}
+
+// useBackTracking time compelxity O(N), space complexity O(log(N))
+func useBackTracking(root *utils.TreeNode) int {
+	var ans int
+	helper(root, &ans, []int{})
+	return ans
+}
+
+func helper(root *utils.TreeNode, ans *int, path []int) {
+	if root == nil {
+		return
+	}
+	np := len(path)
+	path = append(path, root.Val)
+	if root.Left == nil && root.Right == nil {
+		// it's a leaf node
+		*ans += genNum(path)
+		path = path[:np]
+		return
+	}
+	helper(root.Left, ans, path)
+	helper(root.Right, ans, path)
+}
+
+func genNum(nums []int) int {
+	var ans int
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		ans += nums[i] * (1 << (n - 1 - i))
+	}
+	return ans
 }
