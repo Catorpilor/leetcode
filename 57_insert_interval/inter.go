3748a99ae742273ca957204371f2fe982dc2e967
Catorpilor <jiaojiangonghitster@gmail.com>
2020-09-14 20:01:55 +0800

    fix input format and useStraight way to solve 57

--- 57_insert_interval/inter.go
+++ 57_insert_interval/inter.go
@@ -2,33 +2,26 @@ package inter
 
 import "github.com/catorpilor/leetcode/utils"
 
-type Interval struct {
-	Start, End int
+func insertIntervals(intervals [][]int, newInterval []int) [][]int {
+	return useStraight(intervals, newInterval)
 }
 
-func Insert(intervals []Interval, newInterval Interval) []Interval {
-	var ret []Interval
-	var idx int
+// uesStraight time complexity O(N), space complexity O(1)
+func useStraight(intervals [][]int, newInterval []int) [][]int {
 	n := len(intervals)
-	// add all the intervals ending befor newInterval starts
-	for idx < n && intervals[idx].End < newInterval.Start {
-		ret = append(ret, intervals[idx])
+	var idx int
+	ans := make([][]int, 0, n+1)
+	for idx < n && intervals[idx][1] < newInterval[0] {
+		ans = append(ans, intervals[idx])
 		idx++
 	}
-	// mrege the overlapped ones
-	for idx < n && intervals[idx].Start <= newInterval.End {
-		// newInterval = Interval{
-		// 	Start: utils.Min(intervals[idx].Start, newInterval.Start),
-		// 	End:   utils.Max(intervals[idx].End, newInterval.End),
-		// }
-		newInterval.Start = utils.Min(intervals[idx].Start, newInterval.Start)
-		newInterval.End = utils.Max(intervals[idx].End, newInterval.End)
+	for idx < n && intervals[idx][0] <= newInterval[1] {
+		newInterval = []int{utils.Min(newInterval[0], intervals[idx][0]), utils.Max(newInterval[1], intervals[idx][1])}
 		idx++
 	}
-	ret = append(ret, newInterval)
+	ans = append(ans, newInterval)
 	if idx < n {
-		// add teh rest
-		ret = append(ret, intervals[idx:]...)
+		ans = append(ans, intervals[idx:]...)
 	}
-	return ret
+	return ans
 }
--- 57_insert_interval/inter_test.go
+++ 57_insert_interval/inter_test.go
@@ -8,23 +8,23 @@ import (
 func TestInsert(t *testing.T) {
 	st := []struct {
 		name        string
-		intervals   []Interval
-		newInterval Interval
-		exp         []Interval
+		intervals   [][]int
+		newInterval []int
+		exp         [][]int
 	}{
-		{"empty slice", []Interval{}, Interval{1, 2}, []Interval{Interval{1, 2}}},
-		{"single", []Interval{Interval{1, 3}}, Interval{2, 4}, []Interval{Interval{1, 4}}},
-		{"single", []Interval{Interval{1, 5}}, Interval{2, 4}, []Interval{Interval{1, 5}}},
-		{"test 1", []Interval{Interval{1, 3}, Interval{6, 9}}, Interval{2, 5}, []Interval{Interval{1, 5}, Interval{6, 9}}},
-		{"test 2", []Interval{Interval{1, 2}, Interval{3, 5}, Interval{6, 7}, Interval{8, 10}, Interval{12, 16}},
-			Interval{4, 9}, []Interval{Interval{1, 2}, Interval{3, 10}, Interval{12, 16}}},
-		{"failed 1", []Interval{Interval{1, 5}}, Interval{0, 3}, []Interval{Interval{0, 5}}},
-		{"failed 2", []Interval{Interval{1, 5}}, Interval{0, 6}, []Interval{Interval{0, 6}}},
-		{"failed 3", []Interval{Interval{1, 5}, Interval{6, 8}}, Interval{0, 9}, []Interval{Interval{0, 9}}},
+		{"empty slice", nil, []int{1, 2}, [][]int{[]int{1, 2}}},
+		{"single", [][]int{[]int{1, 3}}, []int{2, 4}, [][]int{[]int{1, 4}}},
+		{"single", [][]int{[]int{1, 5}}, []int{2, 4}, [][]int{[]int{1, 5}}},
+		{"test 1", [][]int{[]int{1, 3}, []int{6, 9}}, []int{2, 5}, [][]int{[]int{1, 5}, []int{6, 9}}},
+		{"test 2", [][]int{[]int{1, 2}, []int{3, 5}, []int{6, 7}, []int{8, 10}, []int{12, 16}},
+			[]int{4, 9}, [][]int{[]int{1, 2}, []int{3, 10}, []int{12, 16}}},
+		{"failed 1", [][]int{[]int{1, 5}}, []int{0, 3}, [][]int{[]int{0, 5}}},
+		{"failed 2", [][]int{[]int{1, 5}}, []int{0, 6}, [][]int{[]int{0, 6}}},
+		{"failed 3", [][]int{[]int{1, 5}, []int{6, 8}}, []int{0, 9}, [][]int{[]int{0, 9}}},
 	}
 	for _, c := range st {
 		t.Run(c.name, func(t *testing.T) {
-			ret := Insert(c.intervals, c.newInterval)
+			ret := insertIntervals(c.intervals, c.newInterval)
 			if !reflect.DeepEqual(ret, c.exp) {
 				t.Fatalf("expected %v but got %v with input %v and %v",
 					c.exp, ret, c.intervals, c.newInterval)
