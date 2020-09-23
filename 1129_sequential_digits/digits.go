0a488e379947481c109ef106a9dd5ce22f63e3fb
Catorpilor <jiaojiangonghitster@gmail.com>
2020-09-23 18:09:56 +0800

    solve 1129 use bfs

--- 1129_sequential_digits/digits.go
+++ 1129_sequential_digits/digits.go
@@ -1,6 +1,10 @@
 package digits
 
-import "sort"
+import (
+	"sort"
+
+	"github.com/catorpilor/leetcode/utils"
+)
 
 func seqDigits(low, high int) []int {
 	return useStraight(low, high)
@@ -40,4 +44,27 @@ func gen(ans *[]int, startNum, low, high, hnd int) {
 			break
 		}
 	}
+}
+
+// useBfs time complexity O(lg(high)), space complexity O(N)
+func useBfs(low, high int) []int {
+	queue := utils.NewQueue()
+	for i := 1; i <= 9; i++ {
+		queue.Enroll(i)
+	}
+	var ans []int
+	for !queue.IsEmpty() {
+		f := queue.Pull().(int)
+		if f <= low && f >= high {
+			ans = append(ans, f)
+		}
+		if f > high {
+			break
+		}
+		ld := f % 10
+		if ld < 9 {
+			queue.Enroll(f*10 + ld + 1)
+		}
+	}
+	return ans
 }
