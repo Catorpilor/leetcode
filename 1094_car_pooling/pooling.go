8b227128fb253363cb9ac37319ed96cb76228dfb
Catorpilor <jiaojiangonghitster@gmail.com>
2020-09-25 20:16:30 +0800

    solve 1094 use bucket, this idea is awesome.

--- 1094_car_pooling/pooling.go
+++ 1094_car_pooling/pooling.go
@@ -1,5 +1,19 @@
 package pooling
 
 func carPooling(trips [][]int, capacity int) bool {
-	return false
+	return useBucket(trips, capacity)
+}
+
+// useBucket time complexity O(N+1001), space complexity O(1)
+func useBucket(trips [][]int, capacity int) bool {
+	stops := [1001]int{}
+	for _, trip := range trips {
+		p, s, e := trip[0], trip[1], trip[2]
+		stops[s] += p
+		stops[e] -= p
+	}
+	for i := 0; i < 1001 && capacity > 0; i++ {
+		capacity -= stops[i]
+	}
+	return capacity >= 0
 }
