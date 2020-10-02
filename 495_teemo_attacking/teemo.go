d206944cc144817ff32ff4ca1eea8986c5d40730
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-02 19:56:05 +0800

    solve 495 use merge interval approach

--- 495_teemo_attacking/teemo.go
+++ 495_teemo_attacking/teemo.go
@@ -24,3 +24,24 @@ func useStraight(ts []int, duration int) int {
 	ans += duration
 	return ans
 }
+
+// useMerge time complexity O(N), space complexity O(1)
+func useMerge(ts []int, duration int) int {
+	n := len(ts)
+	if n < 1 || duration == 0 {
+		return 0
+	}
+	var ans int
+	// start, end represent an interval
+	start, end := ts[0], ts[0]+duration
+	for i := 1; i < n; i++ {
+		if ts[i] > end {
+			// no overlapping
+			ans += end - start
+			start = ts[i]
+		}
+		end = ts[i] + duration
+	}
+	ans += end - start
+	return ans
+}
