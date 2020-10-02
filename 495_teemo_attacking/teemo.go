9788cf48e4d0f1ffcbd96164030c5c8434b48a49
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-02 19:44:50 +0800

    solve 495 use straight way

--- 495_teemo_attacking/teemo.go
+++ 495_teemo_attacking/teemo.go
@@ -1,5 +1,26 @@
 package teemo
 
 func teemoAttack(timeSeries []int, duration int) int {
-	return -1
+	return useStraight(timeSeries, duration)
+}
+
+// useStraight time complexity O(N), space complexity O(1)
+func useStraight(ts []int, duration int) int {
+	n := len(ts)
+	if n < 1 {
+		return 0
+	}
+	var ans int
+	pre := ts[0]
+	for i := 1; i < n; i++ {
+		diff := ts[i] - pre
+		if diff > duration {
+			ans += duration
+			pre = ts[i]
+		} else {
+			ans += diff
+		}
+	}
+	ans += duration
+	return ans
 }
