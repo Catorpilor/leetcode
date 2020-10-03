ff6da218f768870347e1f5096b64a3d88c4a113b
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-03 18:12:16 +0800

    add testcases for 399

---
+++ 399_evaluate_division/div.go
@@ -0,0 +1,5 @@
+package div
+
+func calcEquation(eqs [][]string, vals []float64, queries [][]string) []float64 {
+	return nil
+}
---
+++ 399_evaluate_division/div_test.go
@@ -0,0 +1,31 @@
+package div
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestCalcEquation(t *testing.T) {
+	st := []struct {
+		name      string
+		equations [][]string
+		values    []float64
+		queries   [][]string
+		exp       []float64
+	}{
+		{"testcase1", [][]string{[]string{"a", "b"}, []string{"b", "c"}}, []float64{2.0, 3.0}, [][]string{[]string{"a", "c"}, []string{"b", "a"}, []string{"a", "e"}, []string{"a", "a"}, []string{"x", "x"}},
+			[]float64{6.00000, 0.50000, -1.00000, 1.00000, -1.00000}},
+		{"testcase2", [][]string{[]string{"a", "b"}, []string{"b", "c"}, []string{"bc", "cd"}}, []float64{1.5, 2.5, 5.0}, [][]string{[]string{"a", "c"}, []string{"c", "b"}, []string{"bc", "cd"}, []string{"cd", "bc"}},
+			[]float64{3.75000, 0.40000, 5.00000, 0.20000}},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := calcEuqation(tt.equations, tt.values, tt.queries)
+			if diff := cmp.Diff(tt.exp, out); diff != "" {
+				t.Fatalf("(-wanted, +got) %s", diff)
+			}
+			t.Log("pass")
+		})
+	}
+}
