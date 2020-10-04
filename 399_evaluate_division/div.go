a98e44e1f88e2ae2e23ec78e5a9160d85f80e780
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-04 22:16:20 +0800

    solve 399 use dfs

--- 399_evaluate_division/div.go
+++ 399_evaluate_division/div.go
@@ -1,7 +1,10 @@
 package div
 
+import "fmt"
+
 func calcEquation(eqs [][]string, vals []float64, queries [][]string) []float64 {
-	return useBfs(eqs, vals, queries)
+	// return useBfs(eqs, vals, queries)
+	return useDFS(eqs, queries, vals)
 }
 
 type pair struct {
@@ -61,4 +64,49 @@ func helper(adj map[string][]pair, cache map[string]float64, start, end string) 
 	}
 	cache[start+"/"+end] = -1.0
 	return
+}
+
+func useDFS(eqs, queries [][]string, vals []float64) []float64 {
+	adj := make(map[string][]pair)
+	for i, eq := range eqs {
+		s, e, val := eq[0], eq[1], vals[i]
+		// there is an edge from f->e with value val
+		adj[s] = append(adj[s], pair{e, val})
+		// there is an edge from e->ef with valus 1/val
+		adj[e] = append(adj[e], pair{s, 1 / val})
+	}
+	ans := make([]float64, len(queries))
+	cache := make(map[string]float64)
+	for i, query := range queries {
+		key := fmt.Sprintf("%s/%s", query[0], query[1])
+		if _, exists := cache[key]; !exists {
+			cache[key] = calWeight(query[0], query[1], make(map[string]bool), adj, cache, 1.0)
+		}
+		ans[i] = cache[key]
+	}
+	return ans
+}
+
+func calWeight(start, end string, visited map[string]bool, adj map[string][]pair, cache map[string]float64, val float64) float64 {
+	if len(adj[start]) == 0 || len(adj[end]) == 0 {
+		return -1.0
+	}
+	if start == end {
+		// cache[start+"/"+end] = val
+		// cache[end+"/"+start] = 1 / val
+		return val
+	}
+	// key := fmt.Sprintf("%s/%s", start, end)
+	// if v, exists := cache[key]; exists {
+	//	return v
+	// }
+	visited[start] = true
+	for _, next := range adj[start] {
+		if !visited[next.node] {
+			lv := calWeight(next.node, end, visited, adj, cache, val*next.val)
+			cache[next.node+"/"+end] = lv
+			cache[end+"/"+next.node] = 1 / lvx
+		}
+	}
+	return -1.0
 }
