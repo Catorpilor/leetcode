dd2e3cdcda126ca6b306f007cd4187470395edd6
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-08 22:18:06 +0800

    solve 532 use hashmap

--- 532_k_diff_pairs_in_an_array/diff.go
+++ 532_k_diff_pairs_in_an_array/diff.go
@@ -1,5 +1,21 @@
 package diff
 
 func findPairs(nums []int, k int) int {
-	return -1
+	return useHashmap(nums, k)
+}
+
+// useHashmap time complexity O(N), space complexity O(N)
+func useHashmap(nums []int, k int) int {
+	n := len(nums)
+	set := make(map[int]int, n)
+	for _, num := range nums {
+		set[num]++
+	}
+	var ans int
+	for key := range set {
+		if (k > 0 && set[key+k] > 0) || (k == 0 && set[key] > 1) {
+			ans++
+		}
+	}
+	return ans
 }
