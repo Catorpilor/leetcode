1e61451fdbdcbc1951df18fcf6c9a783a7253b9a
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-10 23:35:53 +0800

    solve 1009 use bit wise mask.

--- 1009_complement_of_base_10_integer/comp.go
+++ 1009_complement_of_base_10_integer/comp.go
@@ -1,5 +1,15 @@
 package comp
 
 func bitwiseComp(n int) int {
-	return -1
+	return useMask(n)
+}
+
+// useMask time complexity O(lgN), space complexity O(1)
+func useMask(n int) int {
+	mask := 1
+	// for example n = 5 -> 101, mask = 111, n's complement is 010
+	for n > mask {
+		mask = mask<<1 + 1
+	}
+	return mask - n
 }
