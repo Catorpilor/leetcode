f025c4042231b6143e5adb4b77eebbf10ae2645c
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-19 09:14:56 +0800

    solve 859 use straight way just compare.

--- 859_buddy_string/buddy.go
+++ 859_buddy_string/buddy.go
@@ -1,5 +1,27 @@
 package buddy
 
 func buddyString(a, b string) bool {
-	return false
+	return useStraight(a, b)
+}
+
+// time complexity O(N), space complexity O(1)
+func useStraight(A, B string) bool {
+	na, nb := len(A), len(B)
+	if na != nb {
+		return false
+	}
+	seen := make(map[byte]bool, 26)
+	for i := range A {
+		seen[A[i]] = true
+	}
+	if A == B && len(seen) < na {
+		return true
+	}
+	diff := make([]int, 0, na)
+	for i := 0; i < na; i++ {
+		if A[i] != B[i] {
+			diff = append(diff, i)
+		}
+	}
+	return len(diff) == 2 && (A[diff[0]] == B[diff[1]] && A[diff[1]] == B[diff[0]])
 }
