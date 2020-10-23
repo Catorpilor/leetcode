10fcc773379e3bee633a6ff91b0c3060209966fc
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-23 19:46:09 +0800

    solve 187 use bruteforce

--- 187_repeated_dna_sequences/dna.go
+++ 187_repeated_dna_sequences/dna.go
@@ -1,5 +1,28 @@
 package dna
 
+import (
+	"strings"
+)
+
 func findRepeatedSeq(s string) []string {
-	return nil
+	return useBruteForce(s)
+}
+
+// useBruteForce time complexity O(N^2), space complexity O(1)
+func useBruteForce(s string) []string {
+	n := len(s)
+	if n <= 10 {
+		return nil
+	}
+	var ans []string
+	found := make(map[string]bool, n)
+	for i := 0; i < n-9; i++ {
+		tmp := s[i : i+10]
+		if !found[tmp] && strings.Index(s[i+1:], tmp) != -1 {
+			found[tmp] = true
+			ans = append(ans, tmp)
+		}
+
+	}
+	return ans
 }
