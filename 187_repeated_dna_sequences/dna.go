ea15d2a7c983c080977ed613e51273ade6e5223c
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-24 20:00:45 +0800

    solve 187 use bit operation

--- 187_repeated_dna_sequences/dna.go
+++ 187_repeated_dna_sequences/dna.go
@@ -5,10 +5,11 @@ import (
 )
 
 func findRepeatedSeq(s string) []string {
-	return useBruteForce(s)
+	// return useBruteForce(s)
+	return useBits(s)
 }
 
-// useBruteForce time complexity O(N^2), space complexity O(1)
+// useBruteForce time complexity O(N^2), space complexity O(n)
 func useBruteForce(s string) []string {
 	n := len(s)
 	if n <= 10 {
@@ -23,6 +24,24 @@ func useBruteForce(s string) []string {
 			ans = append(ans, tmp)
 		}
 
+	}
+	return ans
+}
+
+// useBits time complexity O(N), space complexity O(N)
+func useBits(s string) []string {
+	n := len(s)
+	if n <= 10 {
+		return nil
+	}
+	var ans []string
+	occ := make(map[int]int)
+	for i, t := 0, 0; i < n; i++ {
+		t = t<<3&0x3FFFFFFF | int(s[i]&7)
+		if occ[t] == 1 {
+			ans = append(ans, s[i-9:i+1])
+		}
+		occ[t]++
 	}
 	return ans
 }
