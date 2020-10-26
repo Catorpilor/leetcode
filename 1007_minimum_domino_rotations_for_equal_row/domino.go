79fb228c1f7093861e50e1718d96a039c1c70034
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-26 20:01:38 +0800

    solve 1007 use brutefoce

--- 1007_minimum_domino_rotations_for_equal_row/domino.go
+++ 1007_minimum_domino_rotations_for_equal_row/domino.go
@@ -1,5 +1,60 @@
 package domino
 
+import (
+	"math"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
 func minRotations(a, b []int) int {
-	return -1
+	return useBruteForce(a, b)
+}
+
+// useBruteForce time complexity O(N), space complexity O(1)
+func useBruteForce(a, b []int) int {
+	n := len(a)
+	var ans int
+	bucket1, bucket2 := make([]int, 7), make([]int, 7)
+	for i := range a {
+		bucket1[a[i]]++
+		bucket2[b[i]]++
+	}
+	p1, p2 := 1, 1
+	for i := range bucket1 {
+		if bucket1[i] == n || bucket2[i] == n {
+			return ans
+		}
+		if bucket1[i] > bucket1[p1] {
+			p1 = i
+		}
+		if bucket2[i] > bucket2[p2] {
+			p2 = i
+		}
+	}
+	var r1, r2 int
+	for i := range a {
+		if a[i] != p1 {
+			if b[i] == p1 {
+				r1++
+			} else {
+				r1 = math.MaxInt32
+				break
+			}
+		}
+	}
+	for i := range b {
+		if b[i] != p2 {
+			if a[i] == p2 {
+				r2++
+			} else {
+				r2 = math.MaxInt32
+				break
+			}
+		}
+	}
+	ans = utils.Min(r1, r2)
+	if ans == math.MaxInt32 {
+		return -1
+	}
+	return ans
 }
