4e90f393e96c95244bb457e76e93bfd222aa1a4b
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-30 18:25:28 +0800

    solve 735 use a simpler version of stack

--- 735_asteroid_collision/aster.go
+++ 735_asteroid_collision/aster.go
@@ -43,3 +43,26 @@ func useStack(arr []int) []int {
 	}
 	return st
 }
+
+// useStackV2 time complexity O(N), space complexity O(1)
+func useStackV2(arr []int) []int {
+	n := len(arr)
+	if n <= 1 {
+		return arr
+	}
+	st := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nst := len(st)
+		if arr[i] > 0 || nst == 0 || st[nst-1] < 0 {
+			st = append(st, arr[i])
+		} else if st[nst-1] <= -arr[i] {
+			if st[nst-1] < -arr[i] {
+				// revisit arr[i]
+				i--
+			}
+			// if arr[i] == -st[nst-1] both explode
+			st = st[:nst-1]
+		}
+	}
+	return st
+}
