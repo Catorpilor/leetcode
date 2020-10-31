83a0db9e7e204ee15689289be7f94026bb20c3f8
Catorpilor <jiaojiangonghitster@gmail.com>
2020-10-31 17:38:40 +0800

    solve 456 use a better brute force.

--- 456_132_pattern/pattern.go
+++ 456_132_pattern/pattern.go
@@ -1,5 +1,11 @@
 package pattern
 
+import (
+	"math"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
 func findPattern(nums []int) bool {
 	return useBruteForce(nums)
 }
@@ -16,6 +22,25 @@ func useBruteForce(nums []int) bool {
 				if nums[i] < nums[k] && nums[j] > nums[k] {
 					return true
 				}
+			}
+		}
+	}
+	return false
+}
+
+// useBetterBF time complexity O(N^2), space complexity O(1)
+func useBetterBF(nums []int) bool {
+	n := len(nums)
+	if n < 3 {
+		return false
+	}
+	// we choose nums[i] as the min value we met so far from [0..j)
+	minI := math.MaxInt32
+	for j := 0; j < n-1; j++ {
+		minI = utils.Min(minI, nums[j])
+		for k := j + 1; k < n; k++ {
+			if nums[j] > nums[k] && nums[k] > minI {
+				return true
 			}
 		}
 	}
