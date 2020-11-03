adb82c96ac75df62f3f393708352772a8ddb72aa
Catorpilor <jiaojiangonghitster@gmail.com>
2020-11-03 21:14:48 +0800

    solve 948 use prefix sum and lower_bound

--- 948_bag_of_tokens/tokens.go
+++ 948_bag_of_tokens/tokens.go
@@ -7,7 +7,8 @@ import (
 )
 
 func maxScore(tokens []int, p int) int {
-	return useTwoPointers(tokens, p)
+	// return useTwoPointers(tokens, p)
+	return usePrefixSum(tokens, p)
 }
 
 // useTwoPointers time complexity O(N*logN), space complexity O(1)
@@ -32,4 +33,60 @@ func useTwoPointers(tokens []int, p int) int {
 		}
 	}
 	return ans
+}
+
+// usePrefixSum time complexity O(N*logN), space complexity O(N)
+func usePrefixSum(nums []int, p int) int {
+	n := len(nums)
+	sort.Ints(nums)
+	ps := make([]int, n)
+	copy(ps, nums)
+	for i := 1; i < n; i++ {
+		ps[i] += ps[i-1]
+	}
+	if p < ps[0] {
+		return 0
+	}
+	lp := lower_bound(ps, p)
+	score, ans := lp, lp
+	for i := n - 1; i >= lp && score > 0; i-- {
+		p += nums[i]
+		// case two
+		score--
+		pp := lower_bound(ps[:i], p) - lp
+		score += pp
+		// update left boundary
+		lp += pp
+		ans = utils.Max(ans, score)
+	}
+	return ans
+
+}
+
+func binary_search(nums []int, target int) bool {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)>>1
+		if nums[mid] == target {
+			return true
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return false
+}
+
+func lower_bound(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)>>1
+		if nums[mid] <= target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return l
 }
