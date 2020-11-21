3894fa3d8b6e7a7f0f5de4f6b7743d64725ddd27
Catorpilor <jiaojiangonghitster@gmail.com>
2020-11-21 22:59:05 +0800

    solve 1446 use onepass

--- 1446_concecutive_characters/cc.go
+++ 1446_concecutive_characters/cc.go
@@ -1,5 +1,25 @@
 package cc
 
+import "github.com/catorpilor/leetcode/utils"
+
 func maxPower(s string) int {
-	return 0
+	return useOnepass(s)
+}
+
+// useOnepass time complexity O(N), space complexity O(1)
+func useOnepass(s string) int {
+	n := len(s)
+	if n <= 1 {
+		return n
+	}
+	ans, cur := 1, 1
+	for i := 1; i < n; i++ {
+		if s[i] != s[i-1] {
+			cur = 1
+			continue
+		}
+		cur++
+		ans = utils.Max(ans, cur)
+	}
+	return ans
 }
