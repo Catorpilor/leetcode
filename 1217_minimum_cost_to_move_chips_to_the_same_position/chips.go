c4f98115dadf293659436ceb89b37d7714a60e38
Catorpilor <jiaojiangonghitster@gmail.com>
2020-11-25 23:47:15 +0800

    solve 1217 use simple math.

--- 1217_minimum_cost_to_move_chips_to_the_same_position/chips.go
+++ 1217_minimum_cost_to_move_chips_to_the_same_position/chips.go
@@ -1,5 +1,22 @@
 package chips
 
+import "github.com/catorpilor/leetcode/utils"
+
 func minCost(position []int) int {
-	return 0
+	return useSimple(position)
+}
+
+// useSimple time complexity O(n), space complexity O(1)
+func useSimple(position []int) int {
+	// because we can move both odds and evens chips with zero cost.
+	// in the end there are just two piles.
+	cnt := make([]int, 2)
+	for _, p := range position {
+		if p&1 != 0 {
+			cnt[1] += p
+		} else {
+			cnt[0] += p
+		}
+	}
+	return utils.Min(cnt[0], cnt[1])
 }
