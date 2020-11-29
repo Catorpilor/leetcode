65caa5a4ea307d75e29a78d92309e860fe9d17c6
Catorpilor <jiaojiangonghitster@gmail.com>
2020-11-29 19:11:12 +0800

    solve 563 use postorder traversal

--- 563_binary_tree_tilt/bst.go
+++ 563_binary_tree_tilt/bst.go
@@ -3,5 +3,18 @@ package bst
 import "github.com/catorpilor/leetcode/utils"
 
 func findTilt(root *utils.TreeNode) int {
-	return 0
+	var ans int
+	usePostOrder(root, &ans)
+	return ans
+}
+
+// usePostOrder time complexity O(N), space complexity O(logN)
+func usePostOrder(root *utils.TreeNode, ans *int) int {
+	if root == nil {
+		return 0
+	}
+	left := usePostOrder(root.Left, ans)
+	right := usePostOrder(root.Right, ans)
+	*ans += utils.Abs(left - right)
+	return left + right + root.Val
 }
