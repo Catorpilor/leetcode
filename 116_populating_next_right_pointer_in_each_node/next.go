5ec22a597b11a9361d38b6b78de75005e7978b73
Catorpilor <jiaojiangonghitster@gmail.com>
2020-12-03 23:07:48 +0800

    solve 116 use levelorder traversal not constant space

--- 116_populating_next_right_pointer_in_each_node/next.go
+++ 116_populating_next_right_pointer_in_each_node/next.go
@@ -6,5 +6,29 @@ type TreeNode struct {
 }
 
 func connect(root *TreeNode) *TreeNode {
+	return uselevelorder(root)
+}
+
+// uselevelorder time complexity O(N), space complexity O(N) which is not constant.
+func uselevelorder(root *TreeNode) *TreeNode {
+	if root == nil || root.Left == nil && root.Right == nil {
+		return root
+	}
+	var st []*TreeNode
+	st = append(st, root)
+	nst := len(st)
+	for nst != 0 {
+		var tmp []*TreeNode
+		for i, node := range st {
+			if node.Left != nil && node.Right != nil {
+				tmp = append(tmp, node.Left, node.Right)
+			}
+			if i+1 < nst {
+				node.Next = st[i+1]
+			}
+		}
+		st = tmp
+		nst = len(st)
+	}
 	return root
 }
