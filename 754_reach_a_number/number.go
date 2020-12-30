317d50bf1b08a1124297f9a0cd8be544aae7585b
Catorpilor <jiaojiangonghitster@gmail.com>
2020-12-30 22:48:47 +0800

    solve 754 use math

--- 754_reach_a_number/number.go
+++ 754_reach_a_number/number.go
@@ -1,5 +1,46 @@
 package number
 
+import "github.com/catorpilor/leetcode/utils"
+
 func reachNumber(target int) int {
-	return -1
+    return useMath(target)
+}
+
+func useMath(target int) int {
+    target = utils.Abs(target)
+    ans := 0
+    var sum int
+    for sum < target || (sum-target)%2 != 0 {
+        ans++
+        sum += ans
+    }
+    return ans
+}
+
+// useBruteForce got tle
+func useBruteForce(target int) int {
+    store := make(map[int]map[int]bool)
+    store[1] = map[int]bool{1: true, -1: true}
+    store[2] = map[int]bool{3: true, 1: true, -1: true, -3: true}
+    ans := 1
+    for {
+        if store[ans] == nil {
+            pvn := len(store[ans-1])
+            store[ans] = make(map[int]bool, pvn<<1)
+            for k := range store[ans-1] {
+                store[ans][k+ans] = true
+                store[ans][k-ans] = true
+                if target == k+ans || target == k-ans {
+                    return ans
+                }
+            }
+        } else {
+            for k := range store[ans] {
+                if k == target {
+                    return ans
+                }
+            }
+        }
+        ans++
+    }
 }
--- 754_reach_a_number/number_test.go
+++ 754_reach_a_number/number_test.go
@@ -10,12 +10,13 @@ func TestReachNumber(t *testing.T) {
 	}{
 		{"testcase1", 3, 2},
 		{"testcase2", 2, 3},
+		{"tle1", -1000000000, 44723},
 	}
 	for _, tt := range st {
 		t.Run(tt.name, func(t *testing.T) {
 			out := reachNumber(tt.target)
 			if out != tt.exp {
-				t.Fataf("with input target: %d wanted %d", tt.target, tt.exp)
+				t.Fatalf("with input target: %d wanted %d but got %d", tt.target, tt.exp, out)
 			}
 			t.Log("pass")
 		})
