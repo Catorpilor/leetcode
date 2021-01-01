aa46c83e2a154ea43dc5fd4255d5dcf1c070c6b5
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-01 23:47:12 +0800

    solve 1640 use hashmap

--- 1640_check_array_formation_through_concatenation/concat.go
+++ 1640_check_array_formation_through_concatenation/concat.go
@@ -1,5 +1,39 @@
 package concat
 
 func canFormArray(arr []int, pcs [][]int) bool {
-	return false
+	return useHashmap(arr, pcs)
+}
+
+// useHashmap time complexity O(N) where (N is the length of arr), space compelxity O(N)
+func useHashmap(arr []int, pcs [][]int) bool {
+	n := len(arr)
+	set := make(map[int]int, n)
+	for i := range arr {
+		set[arr[i]] = i
+	}
+	for i := range pcs {
+		cur := pcs[i]
+		if len(cur) == 1 {
+			if _, exists := set[cur[0]]; !exists {
+				return false
+			}
+			continue
+		}
+		start := -1
+		cj := 0
+		for j := range cur {
+			if p, exists := set[cur[j]]; exists {
+				if start == -1 {
+					start = p
+				} else {
+					if j-cj != p-start {
+						return false
+					}
+				}
+			} else {
+				return false
+			}
+		}
+	}
+	return true
 }
