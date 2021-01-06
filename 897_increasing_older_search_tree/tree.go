c1be830dec4fc090d1b6f7b9b15999ece7d0eba0
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-06 23:30:20 +0800

    solve 897 use dfs

--- 897_increasing_older_search_tree/tree.go
+++ 897_increasing_older_search_tree/tree.go
@@ -3,5 +3,55 @@ package tree
 import "github.com/catorpilor/leetcode/utils"
 
 func increasingBST(root *utils.TreeNode) *utils.TreeNode {
-    return root
+    return useDFS(root)
+}
+
+// useDFS time complexity O(N), space complexity O(lgN)
+func useDFS(root *utils.TreeNode) *utils.TreeNode {
+    if root == nil || root.Left == nil && root.Right == nil {
+        return root
+    }
+    var lh, lt *utils.TreeNode
+    if root.Left != nil {
+        lh, lt = helper(root.Left)
+    }
+    var rh *utils.TreeNode
+    if root.Right != nil {
+        rh, _ = helper(root.Right)
+    }
+    if lt != nil {
+        lt.Right = root
+    } else {
+        lh = root
+    }
+    // rh, _ := helper(root.Right)
+    root.Left = nil
+    root.Right = rh
+    return lh
+}
+
+func helper(root *utils.TreeNode) (head, tail *utils.TreeNode) {
+    if root.Left == nil {
+        head = root
+        if root.Right != nil {
+            lh, lt := helper(root.Right)
+            root.Right = lh
+            tail = lt
+        } else {
+            tail = root
+        }
+    } else {
+        var lt *utils.TreeNode
+        head, lt = helper(root.Left)
+        lt.Right = root
+        root.Left = nil
+        if root.Right == nil {
+            tail = root
+        } else {
+            rh, rt := helper(root.Right)
+            root.Right = rh
+            tail = rt
+        }
+    }
+    return
 }
--- 897_increasing_older_search_tree/tree_test.go
+++ 897_increasing_older_search_tree/tree_test.go
@@ -15,7 +15,10 @@ func TestIncreasingBST(t *testing.T) {
     }{
         {"testcase1", utils.ConstructTree([]int{5, 1, 7}), utils.ConstructTree([]int{1, math.MinInt32, 5, math.MinInt32, 7})},
         {"empty tree", nil, nil},
+        {"failed1", utils.ConstructTree([]int{106, math.MinInt32, 783}), utils.ConstructTree([]int{106, math.MinInt32, 783})},
         {"single node", utils.ConstructTree([]int{1}), utils.ConstructTree([]int{1})},
+        {"testcase2", utils.ConstructTree([]int{5, 3, 6, 2, 4, math.MinInt32, 8, 1, math.MinInt32, math.MinInt32, math.MinInt32, 7, 9}),
+            utils.ConstructTree([]int{1, math.MinInt32, 2, math.MinInt32, 3, math.MinInt32, 4, math.MinInt32, 5, math.MinInt32, 6, math.MinInt32, 7, math.MinInt32, 8, math.MinInt32, 9})},
     }
     for _, tt := range st {
         t.Run(tt.name, func(t *testing.T) {
