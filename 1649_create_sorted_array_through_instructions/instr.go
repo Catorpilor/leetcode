8ff2baac6005b8b59c2456c832e80f1fb7aedb86
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-10 20:58:40 +0800

    solve 1649 use slice tricks and fix upper_bound

--- 1649_create_sorted_array_through_instructions/instr.go
+++ 1649_create_sorted_array_through_instructions/instr.go
@@ -1,8 +1,6 @@
 package inster
 
 import (
-	"sort"
-
 	"github.com/catorpilor/leetcode/utils"
 )
 
@@ -10,24 +8,30 @@ func createSortedArray(ins []int) int {
 	return useBF(ins)
 }
 
+const (
+	mod = int(1e9 + 7)
+)
+
 // useBF time complexity O(N^2*log(N)), space complexity O(N)
 func useBF(instructions []int) int {
 	n := len(instructions)
 	nums := make([]int, 0, n)
 	var ans int
-	for _, num := range instructions {
-		ans += cost(nums, num)
-		nums = append(nums, num)
-		sort.Ints(nums)
+	for i, num := range instructions {
+		lc, rc := cost(nums, num)
+		ans += utils.Min(lc, i-rc)
+		nums = append(nums, 0)
+		copy(nums[rc+1:], nums[rc:])
+		nums[rc] = num
 	}
-	return ans
+	return ans % mod
 }
 
-func cost(nums []int, target int) int {
-	if len(nums) == 0 || nums[0] >= target || nums[len(nums)-1] <= target {
-		return 0
+func cost(nums []int, target int) (int, int) {
+	if len(nums) == 0 {
+		return 0, 0
 	}
-	return utils.Min(lb(nums, target), up(nums, target))
+	return lb(nums, target), upper_bound(nums, target)
 }
 
 // lb lower_bound find the proper position for target, time complexity O(log(N))
@@ -46,16 +50,59 @@ func lb(nums []int, target int) int {
 }
 
 // up upper_bound find the proper position for target, time complexity O(log(N))
-func up(nums []int, target int) int {
-	l, r := 0, len(nums)-1
-	for l < r {
-		mid := r - (r-l)>>1
+// func up(nums []int, target int) int {
+// 	l, r := 0, len(nums)-1
+// 	if l == r && target > nums[l] {
+// 		return l + 1
+// 	}
+// 	for l < r {
+// 		mid := r - (r-l)>>1
+// 		if nums[mid] <= target {
+// 			l = mid
+// 		} else {
+// 			r = mid - 1
+// 		}
+// 	}
+// 	fmt.Printf("upper_bound nums:%v with target: %d return: %d\n", nums, target, l)
+// 	return l
+// }
+
+/*
+template<class ForwardIt, class T, class Compare>
+ForwardIt upper_bound(ForwardIt first, ForwardIt last, const T& value, Compare comp)
+{
+    ForwardIt it;
+    typename std::iterator_traits<ForwardIt>::difference_type count, step;
+    count = std::distance(first, last);
+
+    while (count > 0) {
+        it = first;
+        step = count / 2;
+        std::advance(it, step);
+        if (!comp(value, *it)) {
+            first = ++it;
+            count -= step + 1;
+        }
+        else
+            count = step;
+    }
+    return first;
+}
+*/
+
+func upper_bound(nums []int, target int) int {
+	count := len(nums)
+	l := 0
+	for count > 0 {
+		step := count >> 1
+		mid := l + step
 		if nums[mid] <= target {
-			l = mid
+			l = mid + 1
+			count -= step + 1
 		} else {
-			r = mid - 1
+			count = step
 		}
 	}
 	// fmt.Printf("upper_bound nums:%v with target: %d return: %d\n", nums, target, l)
-	return len(nums) - l - 1
+	return l
 }
