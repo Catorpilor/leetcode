7de39744e21e6a4f8d8a0e4672663aa71536857f
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-11 14:38:27 +0800

    add upper_bound utility funcion

--- utils/array.go
+++ utils/array.go
@@ -14,3 +14,60 @@ func LowerBound(nums []int, target int) int {
 	}
 	return low
 }
+
+/*
+template<class ForwardIt, class T, class Compare>
+ForwardIt upper_bound(ForwardIt first, ForwardIt last, const T& value, Compare comp)
+{
+    ForwardIt it;
+    typename std::iterator_traits<ForwardIt>::difference_type count, step;
+    count = std::distance(first, last);
+
+    while (count > 0) {
+        it = first;
+        step = count / 2;
+        std::advance(it, step);
+        if (!comp(value, *it)) {
+            first = ++it;
+            count -= step + 1;
+        }
+        else
+            count = step;
+    }
+    return first;
+}
+*/
+
+// Upperbound find the proper posion for target in nums, time complexity O(log(N))
+func Upperbound(nums []int, target int) int {
+	count := len(nums)
+	l := 0
+	for count > 0 {
+		step := count >> 1
+		mid := l + step
+		if nums[mid] <= target {
+			l = mid + 1
+			count -= step + 1
+		} else {
+			count = step
+		}
+	}
+	// fmt.Printf("upper_bound nums:%v with target: %d return: %d\n", nums, target, l)
+	return l
+}
+
+// up upper_bound find the proper position for target, time complexity O(log(N))
+func up(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		// mid := r - (r-l)>>1
+		mid := l + (r-l+1)>>1
+		if nums[mid] <= target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	// fmt.Printf("upper_bound nums:%v with target: %d return: %d\n", nums, target, l)
+	return l
+}
--- utils/array_test.go
+++ utils/array_test.go
@@ -25,3 +25,27 @@ func TestLowerBound(t *testing.T) {
 		})
 	}
 }
+
+func TestUpperBound(t *testing.T) {
+	st := []struct {
+		name   string
+		arr    []int
+		target int
+		exp    int
+	}{
+		{"empty slice", nil, 1, 0},
+		{"all identical", []int{1, 1, 1}, 1, 3},
+		{"testcase1", []int{1, 2, 2, 2, 3, 4}, 2, 4},
+		{"testcase2", []int{2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4}, 2, 6},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			// out := Upperbound(tt.arr, tt.target)
+			out := up(tt.arr, tt.target)
+			if out != tt.exp {
+				t.Fatalf("with input arr:%v and target:%d wanted %d but got %d", tt.arr, tt.target, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
