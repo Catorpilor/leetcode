97ca67fb1c4fa802433e774c1b2f69233f9b3707
catorpilor <jiaojiangonghitster@gmail.com>
2021-01-14 00:14:29 +0800

    add comments for 881

--- 881_boats_to_save_people/boat.go
+++ 881_boats_to_save_people/boat.go
@@ -8,6 +8,7 @@ func minRes(ppl []int, limit int) int {
 	return useSort(ppl, limit)
 }
 
+// useSort time complexity O(N*log(N)), space complexity O(1)
 func useSort(ppl []int, limit int) int {
 	sort.Ints(ppl)
 	l, r := 0, len(ppl)-1
