b8bf639b1ad7127a1be74f17dcb61f665fdbb0ec
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-15 00:03:36 +0800

    useBruteForce got tle

--- 1658_minimum_operations_to_reduce_x_to_zero/x.go
+++ 1658_minimum_operations_to_reduce_x_to_zero/x.go
@@ -1,5 +1,30 @@
 package x
 
+import (
+    "math"
+
+    "github.com/catorpilor/leetcode/utils"
+)
+
 func minRes(nums []int, x int) int {
-    return -1
+    return useBruteForce(nums, x)
+}
+
+func useBruteForce(nums []int, x int) int {
+    ans := helper(nums, x, len(nums))
+    if ans == math.MaxInt32 {
+        return -1
+    }
+    return ans
+}
+
+func helper(nums []int, x, ol int) int {
+    n := len(nums)
+    if x == 0 {
+        return ol - n
+    }
+    if x < 0 || n == 0 {
+        return math.MaxInt32
+    }
+    return utils.Min(helper(nums[1:], x-nums[0], ol), helper(nums[:n-1], x-nums[n-1], ol))
 }
