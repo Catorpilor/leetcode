8d554d2f9c46f11668fbd229c0b71ac7af37dc77
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-16 17:35:34 +0800

    solve 1646 use additional space

--- 1646_get_maximum_in_generated_array/gen.go
+++ 1646_get_maximum_in_generated_array/gen.go
@@ -1,5 +1,27 @@
 package gen
 
+import "github.com/catorpilor/leetcode/utils"
+
 func getMaximumGen(n int) int {
-	return -1
+	return useIter(n)
+}
+
+// useIter time complexity O(N), space complexity O(N)
+func useIter(n int) int {
+	if n <= 1 {
+		return n
+	}
+	res := make([]int, n+1)
+	res[1] = 1
+	ans := 0
+	for i := 1; i <= n; i++ {
+		if i<<1 <= n {
+			res[i<<1] = res[i]
+		}
+		if i<<1+1 <= n {
+			res[i<<1+1] = res[i] + res[i+1]
+		}
+		ans = utils.Max(ans, res[i])
+	}
+	return ans
 }
