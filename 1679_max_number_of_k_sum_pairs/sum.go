5f8960b453237386d3a30234b08c4feb5ea3c69e
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-18 19:33:04 +0800

    solve 1679 use two pointers

--- 1679_max_number_of_k_sum_pairs/sum.go
+++ 1679_max_number_of_k_sum_pairs/sum.go
@@ -1,5 +1,28 @@
 package sum
 
+import "sort"
+
 func maxKSumOps(nums []int, k int) int {
-	return -1
+	return useTwoPointers(nums, k)
+}
+
+// useTwoPointers time complexity O(N*log(N)), space complexity O(1)
+func useTwoPointers(nums []int, k int) int {
+	n := len(nums)
+	sort.Ints(nums)
+	l, r := 0, n-1
+	var ans int
+	for l < r {
+		cs := nums[l] + nums[r]
+		if cs == k {
+			ans++
+			l++
+			r--
+		} else if cs < k {
+			l++
+		} else {
+			r--
+		}
+	}
+	return ans
 }
