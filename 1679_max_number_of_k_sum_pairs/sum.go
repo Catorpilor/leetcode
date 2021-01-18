86a082e171d48551b787b4ba39be233614d428b6
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-18 19:55:56 +0800

    solve 1679 use hashmap

--- 1679_max_number_of_k_sum_pairs/sum.go
+++ 1679_max_number_of_k_sum_pairs/sum.go
@@ -1,6 +1,10 @@
 package sum
 
-import "sort"
+import (
+	"sort"
+
+	"github.com/catorpilor/leetcode/utils"
+)
 
 func maxKSumOps(nums []int, k int) int {
 	return useTwoPointers(nums, k)
@@ -22,6 +26,27 @@ func useTwoPointers(nums []int, k int) int {
 			l++
 		} else {
 			r--
+		}
+	}
+	return ans
+}
+
+// useHashmap time complexity O(N), space complexity O(N)
+func useHashmap(nums []int, k int) int {
+	n := len(nums)
+	set := make(map[int]int, n)
+	for _, num := range nums {
+		set[num]++
+	}
+	var ans int
+	for num, v := range set {
+		if k-num == num {
+			ans += v / 2
+		} else if set[k-num] != 0 {
+			cm := utils.Min(v, set[k-num])
+			ans += cm
+			set[num] -= cm
+			set[k-num] -= cm
 		}
 	}
 	return ans
--- 1679_max_number_of_k_sum_pairs/sum_test.go
+++ 1679_max_number_of_k_sum_pairs/sum_test.go
@@ -24,3 +24,26 @@ func TestMaxKSumOps(t *testing.T) {
 		})
 	}
 }
+
+func TestMaxKSumOpsUseHashmap(t *testing.T) {
+	st := []struct {
+		name string
+		nums []int
+		k    int
+		exp  int
+	}{
+		{"empty nums", nil, 1, 0},
+		{"single number", []int{1}, 1, 0},
+		{"testcase1", []int{1, 2, 3, 4}, 5, 2},
+		{"testcase2", []int{3, 1, 3, 4, 3}, 6, 1},
+	}
+	for _, tt := range st {
+		t.Run(tt.name, func(t *testing.T) {
+			out := useHashmap(tt.nums, tt.k)
+			if out != tt.exp {
+				t.Fatalf("with input nums:%v, k:%d wanted %d but got %d", tt.nums, tt.k, tt.exp, out)
+			}
+			t.Log("pass")
+		})
+	}
+}
