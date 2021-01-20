f7516a23f4d67187aa2f8b5593f3f0aab207f446
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-20 18:31:04 +0800

    solve 20 use stack

--- 20_valid_parentheses/validp.go
+++ 20_valid_parentheses/validp.go
@@ -1,5 +1,38 @@
 package validp
 
 func isValid(s string) bool {
-	return false
+	return useStack(s)
+}
+
+// useStack time complexity O(N), space complexity O(N)
+func useStack(s string) bool {
+	n := len(s)
+	st := make([]rune, 0, n)
+	for i := range s {
+		switch s[i] {
+		case '(', '{', '[':
+			st = append(st, s[i])
+		case ')':
+			nst := len(st)
+			if nst == 0 || st[nst-1] != '(' {
+				return false
+			}
+			st = st[:nst-1]
+		case '}':
+			nst := len(st)
+			if nst == 0 || st[nst-1] != '{' {
+				return false
+			}
+			st = st[:nst-1]
+		case ']':
+			nst := len(st)
+			if nst == 0 || st[nst-1] != '[' {
+				return false
+			}
+			st = st[:nst-1]
+		default:
+			return false
+		}
+	}
+	return len(st) == 0
 }
