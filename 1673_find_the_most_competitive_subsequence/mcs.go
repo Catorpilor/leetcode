3b6ed999be07b14d351043248b4429cd3be06e15
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-21 19:17:04 +0800

    solve 1673 use stack

--- 1673_find_the_most_competitive_subsequence/mcs.go
+++ 1673_find_the_most_competitive_subsequence/mcs.go
@@ -1,6 +1,29 @@
 package mcs
 
 func mostCompetitive(nums []int, k int) []int {
-	return nil
+	return useMonoIncStack(nums, k)
+}
 
+// useMonoIncStack time complexity O(N) space complexity O(N)
+func useMonoIncStack(nums []int, k int) []int {
+	n := len(nums)
+	if n == 0 || n == k {
+		return nums
+	}
+	st := make([]int, 0, n)
+	ans := make([]int, k)
+	for i := range nums {
+		nst := len(st)
+		for nst > 0 && nums[i] < nums[st[nst-1]] && nst-1+n-i >= k {
+			st = st[:nst-1]
+			nst--
+		}
+		if len(st) < k {
+			st = append(st, i)
+		}
+	}
+	for i := k - 1; i >= 0; i-- {
+		ans[i] = nums[st[i]]
+	}
+	return ans
 }
--- 1673_find_the_most_competitive_subsequence/mcs_test.go
+++ 1673_find_the_most_competitive_subsequence/mcs_test.go
@@ -13,6 +13,10 @@ func TestMostCompetitive(t *testing.T) {
 		k    int
 		exp  []int
 	}{
+		{"nil slice", nil, 1, nil},
+		{"n = k", []int{1, 2, 3}, 3, []int{1, 2, 3}},
+		{"k=1", []int{4, 3, 1, 5, 2}, 1, []int{1}},
+		{"all identical", []int{1, 1, 1, 1, 1, 1}, 3, []int{1, 1, 1}},
 		{"testcase1", []int{3, 5, 2, 6}, 2, []int{2, 6}},
 		{"testcase2", []int{2, 4, 3, 3, 5, 4, 9, 6}, 4, []int{2, 3, 3, 4}},
 	}
