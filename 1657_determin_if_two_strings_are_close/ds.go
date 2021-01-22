e8a27f1680b784e2e3354bc6d37e15fd11866d5b
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-22 19:00:00 +0800

    solve 1657 use hashmap

--- 1657_determin_if_two_strings_are_close/ds.go
+++ 1657_determin_if_two_strings_are_close/ds.go
@@ -1,5 +1,46 @@
 package ds
 
+import (
+	"sort"
+
+	"github.com/google/go-cmp/cmp"
+)
+
 func isClose(word1, word2 string) bool {
-	return false
+	return useHashMap(word1, word2)
+}
+
+// useHashMap time complexity O(N), space complexity O(N)
+func useHashMap(word1, word2 string) bool {
+	n1, n2 := len(word1), len(word2)
+	if n1 != n2 {
+		return false
+	}
+	if n1 == 0 {
+		return true
+	}
+	occ := make(map[byte]int, n1)
+	for i := range word1 {
+		occ[word1[i]]++
+	}
+	occ2 := make(map[byte]int, n2)
+	for i := range word2 {
+		if occ[word2[i]] == 0 {
+			return false
+		}
+		occ2[word2[i]]++
+	}
+	val1, val2 := make([]int, 0, n1), make([]int, 0, n2)
+	for _, v := range occ {
+		val1 = append(val1, v)
+	}
+	for _, v := range occ2 {
+		val2 = append(val2, v)
+	}
+	sort.Ints(val1)
+	sort.Ints(val2)
+	if diff := cmp.Diff(val1, val2); diff != "" {
+		return false
+	}
+	return true
 }
