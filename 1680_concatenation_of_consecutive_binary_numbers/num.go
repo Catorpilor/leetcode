d996905a080032cb88ac0dd8bfd6bbe59589413d
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-28 20:03:08 +0800

    solve 1680 use math

--- 1680_concatenation_of_consecutive_binary_numbers/num.go
+++ 1680_concatenation_of_consecutive_binary_numbers/num.go
@@ -1,5 +1,22 @@
 package num
 
+import (
+	"strconv"
+)
+
 func concatenateBinary(n int) int {
-	return -1
+	return useMath(n)
+}
+
+const (
+	mod = int64(1e9 + 7)
+)
+
+func useMath(n int) int {
+	var ans int64
+	for i := 1; i <= n; i++ {
+		bi := strconv.FormatInt(int64(i), 2)
+		ans = ((ans << len(bi)) + int64(i)) % mod
+	}
+	return int(ans)
 }
--- 1680_concatenation_of_consecutive_binary_numbers/num_test.go
+++ 1680_concatenation_of_consecutive_binary_numbers/num_test.go
@@ -3,22 +3,23 @@ package num
 import "testing"
 
 func TestConcatenateBinary(t *testing.T) {
-	st := []struct{
+	st := []struct {
 		name string
-		n int
-		exp int
+		n    int
+		exp  int
 	}{
 		{"n=1", 1, 1},
 		{"n=3", 3, 27},
 		{"n=12", 12, 505379714},
+		{"failed1", 42, 727837408},
 	}
 	for _, tt := range st {
 		t.Run(tt.name, func(t *testing.T) {
-			out := cancatenateBinary(tt.n)
+			out := concatenateBinary(tt.n)
 			if out != tt.exp {
 				t.Fatalf("wanted %d but got %d", tt.exp, out)
 			}
-			t.Log(:"pass")
+			t.Log("pass")
 		})
 	}
 }
