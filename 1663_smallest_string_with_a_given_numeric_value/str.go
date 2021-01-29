5bd7f191c84399ce64bd313d32f0d72a0f65a6db
Catorpilor <jiaojiangonghitster@gmail.com>
2021-01-29 16:54:30 +0800

    solve 1663 use greedy

--- 1663_smallest_string_with_a_given_numeric_value/str.go
+++ 1663_smallest_string_with_a_given_numeric_value/str.go
@@ -1,5 +1,23 @@
 package str
 
+import "github.com/catorpilor/leetcode/utils"
+
 func getSmallestString(n, k int) string {
-	return ""
+	return useGreedy(n, k)
+}
+
+func useGreedy(n, k int) string {
+	sb := make([]byte, n)
+	for i := range sb {
+		sb[i] = 'a'
+	}
+	k -= n
+	i := n - 1
+	for k > 0 {
+		tmp := utils.Min(k, 25)
+		sb[i] = sb[i] + byte(tmp)
+		k -= tmp
+		i--
+	}
+	return string(sb)
 }
