649ae0a31266a9e28882df127eb53586cdb95ebf
Catorpilor <jiaojiangonghitster@gmail.com>
2021-02-04 20:57:50 +0800

    solve 594 use hashmap

--- 594_longest_harmonious_subsequences/lhs.go
+++ 594_longest_harmonious_subsequences/lhs.go
@@ -1,5 +1,26 @@
 package lhs
 
+import "github.com/catorpilor/leetcode/utils"
+
 func findLHS(nums []int) int {
-	return -1
+	return useHashmap(nums)
+}
+
+// useHashmap time complexity O(N), space compelxity O(N)
+func useHashmap(nums []int) int {
+	n := len(nums)
+	if n < 2 {
+		return 0
+	}
+	set := make(map[int]int, n)
+	for i := range nums {
+		set[nums[i]]++
+	}
+	var ans int
+	for _, num := range nums {
+		if v, exists := set[num+1]; exists {
+			ans = utils.Max(ans, v+set[num])
+		}
+	}
+	return ans
 }
