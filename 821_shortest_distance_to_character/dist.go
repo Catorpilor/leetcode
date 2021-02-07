20cb330bdec27c6afbc241846eeed18f8668f698
Catorpilor <jiaojiangonghitster@gmail.com>
2021-02-07 17:40:51 +0800

    solve 821 use binary search

--- 821_shortest_distance_to_character/dist.go
+++ 821_shortest_distance_to_character/dist.go
@@ -1,5 +1,41 @@
 package dist
 
+import (
+	"math"
+
+	"github.com/catorpilor/leetcode/utils"
+)
+
 func shortestDistance(s string, c byte) []int {
-	return nily
+	return useBinarySearch(s, c)
+}
+
+// useBinarySearch time complexity O(N*log(N)), space complexity O(N)
+func useBinarySearch(s string, c byte) []int {
+	n := len(s)
+	pos := make([]int, 0, n)
+	for i := range s {
+		if s[i] == c {
+			pos = append(pos, i)
+		}
+	}
+	ans := make([]int, n)
+	for i := range ans {
+		ans[i] = math.MaxInt32
+	}
+	for i := range s {
+		if s[i] != c {
+			pp := utils.LowerBound(pos, i)
+			if pp == 0 {
+				ans[i] = pos[pp] - i
+			} else if pp == len(pos) {
+				ans[i] = i - pos[pp-1]
+			} else {
+				ans[i] = utils.Min(i-pos[pp-1], pos[pp]-i)
+			}
+		} else {
+			ans[i] = 0
+		}
+	}
+	return ans
 }
--- 821_shortest_distance_to_character/dist_test.go
+++ 821_shortest_distance_to_character/dist_test.go
@@ -22,7 +22,7 @@ func TestShortestDistance(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			out := shortestDistance(tt.s, tt.c)
 			if diff := cmp.Diff(tt.exp, out); diff != "" {
-				t.Fatalf("(-wanted, +got) %s")
+				t.Fatalf("(-wanted, +got) %s", diff)
 			}
 			t.Log("pass")
 		})
