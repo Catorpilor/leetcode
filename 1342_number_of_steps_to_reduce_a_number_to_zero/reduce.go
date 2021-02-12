f340276152fb873777451f31c670a39d04ee77c5
Catorpilor <jiaojiangonghitster@gmail.com>
2021-02-12 21:03:07 +0800

    solve 1342 use bit ops

--- 1342_number_of_steps_to_reduce_a_number_to_zero/reduce.go
+++ 1342_number_of_steps_to_reduce_a_number_to_zero/reduce.go
@@ -1,5 +1,19 @@
 package reduce
 
 func numOfSteps(num int) int {
-	return 0
+	return useBitOps(num)
+}
+
+// useBitOps time complexity O(32), space complexity O(1)
+func useBitOps(num int) int {
+	var ans int
+	for num > 0 {
+		if num&1 != 0 {
+			num ^= 1
+		} else {
+			num >>= 1
+		}
+		ans++
+	}
+	return ans
 }
