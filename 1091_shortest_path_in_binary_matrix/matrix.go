aff225fd250e3691d367f77c1d6b816fccde6d10
Catorpilor <jiaojiangonghitster@gmail.com>
2021-02-15 21:20:51 +0800

    tmp

--- 1091_shortest_path_in_binary_matrix/matrix.go
+++ 1091_shortest_path_in_binary_matrix/matrix.go
@@ -6,8 +6,8 @@ import (
 	"github.com/catorpilor/leetcode/utils"
 )
 
-func shortestPath(matrix [][]int) int {
-	return -1
+func shortestPath(grid [][]int) int {
+	return useBFS(grid)
 
 }
 
@@ -22,7 +22,7 @@ func useDFS(grid [][]int) int {
 	}
 	ans := math.MaxInt32
 	visited := make([]bool, n*n)
-	helper(grid, *ans, 0, 0, 1, visited, n)
+	helper(grid, &ans, 0, 0, 1, visited, n)
 	if ans == math.MaxInt32 {
 		return -1
 	}
@@ -46,4 +46,33 @@ func helper(grid [][]int, ans *int, x, y, k int, visited []bool, n int) {
 	}
 	visited[x*n+y] = false
 	return
+}
+
+func useBFS(grid [][]int) int {
+	n := len(grid)
+	if n < 1 {
+		return 0
+	}
+	if grid[0][0] == 1 || grid[n-1][n-1] == 1 {
+		return -1
+	}
+	q := [][3]int{[3]int{0, 0, 1}}
+	// make gird[i][j] = 1 to avoid duplicates
+	grid[0][0] = 1
+	dirs := [][]int{[]int{-1, -1}, []int{-1, 0}, []int{-1, 1}, []int{0, -1}, []int{0, 1}, []int{1, -1}, []int{1, 0}, []int{1, 1}}
+	for _, pair := range q {
+		x, y, d := pair[0], pair[1], pair[2]
+		if x == n-1 && y == n-1 {
+			return d
+		}
+		for _, dir := range dirs {
+			nx, ny := x+dir[0], y+dir[1]
+			if nx < 0 || nx >= n || ny < 0 || ny >= n || grid[nx][ny] != 0 {
+				continue
+			}
+			grid[nx][ny] = 1
+			q = append(q, [3]int{nx, ny, d + 1})
+		}
+	}
+	return -1
 }
