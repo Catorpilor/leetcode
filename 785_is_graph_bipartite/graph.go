f6775173e964cf2442e26e82d00a430dc12764fb
Catorpilor <jiaojiangonghitster@gmail.com>
2021-02-18 16:12:37 +0800

    solve 785 use bfs

--- 785_is_graph_bipartite/graph.go
+++ 785_is_graph_bipartite/graph.go
@@ -1,7 +1,8 @@
 package graph
 
 func isBipartite(graph [][]int) bool {
-	return useDFS(graph)
+	// return useDFS(graph)
+	return useBFS(graph)
 }
 
 // useDFS time complexity O(N), space complexity O(N)
@@ -26,6 +27,34 @@ func helper(graph [][]int, colors []int, color, node int) bool {
 	for _, adj := range graph[node] {
 		if !helper(graph, colors, -color, adj) {
 			return false
+		}
+	}
+	return true
+}
+
+// useBFS time complexity O(N), space complexity O(N)
+func useBFS(graph [][]int) bool {
+	n := len(graph)
+	colors := make([]int, n)
+	q := make([]int, 0, n)
+	// the graph may not be connected just traverse all the nodes.
+	for i := range graph {
+		if colors[i] != 0 {
+			continue
+		}
+		colors[i] = 1
+		q = append(q, i)
+		for len(q) != 0 {
+			top := q[0]
+			q = q[1:]
+			for _, adj := range graph[top] {
+				if colors[adj] == 0 {
+					colors[adj] = -colors[top]
+					q = append(q, adj)
+				} else if colors[adj] == colors[top] {
+					return false
+				}
+			}
 		}
 	}
 	return true
--- 785_is_graph_bipartite/graph_test.go
+++ 785_is_graph_bipartite/graph_test.go
@@ -10,6 +10,7 @@ func TestIsBipartite(t *testing.T) {
 	}{
 		{"testcase1", [][]int{[]int{1, 2, 3}, []int{0, 2}, []int{0, 1, 3}, []int{0, 2}}, false},
 		{"testcase2", [][]int{[]int{1, 3}, []int{0, 2}, []int{1, 3}, []int{0, 2}}, true},
+		{"failed1", [][]int{{}, []int{2, 4, 6}, []int{1, 4, 8, 9}, []int{7, 8}, []int{1, 2, 8, 9}, []int{6, 9}, []int{1, 5, 7, 8, 9}, []int{3, 6, 9}, []int{2, 3, 4, 6, 9}, []int{2, 4, 5, 6, 7, 8}}, false},
 	}
 	for _, tt := range st {
 		t.Run(tt.name, func(t *testing.T) {
