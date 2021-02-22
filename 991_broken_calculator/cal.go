2f16b640f6e0630cf6eaf913db3466b571f3ba29
Catorpilor <jiaojiangonghitster@gmail.com>
2021-02-22 19:24:11 +0800

    solve 991 use recursion, instead of modify X, we modify Y(<<1, +1)

--- 991_broken_calculator/cal.go
+++ 991_broken_calculator/cal.go
@@ -1,6 +1,16 @@
 package cal
 
-
 func brokenCalc(x, y int) int {
-	return -1
+	return useRec(x, y)
+}
+
+// useRec time complexity O(log(Y)), space complexity O(logY)
+func useRec(x, y int) int {
+	if x >= y {
+		return x - y
+	}
+	if y&1 == 1 {
+		return 1 + useRec(x, y+1)
+	}
+	return 1 + useRec(x, y<<1)
 }
