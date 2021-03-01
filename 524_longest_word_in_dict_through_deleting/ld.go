adfc997718bff6b1ccf0cac886b2dab2876c80ad
Catorpilor <jiaojiangonghitster@gmail.com>
2021-03-01 19:42:24 +0800

    solve 524 use two pointers

--- 524_longest_word_in_dict_through_deleting/ld.go
+++ 524_longest_word_in_dict_through_deleting/ld.go
@@ -1,5 +1,28 @@
 package ld
 
 func longestWord(s string, dict []string) string {
-	return ""
+	return useTwoPointers(s, dict)
+}
+
+// useTwoPointers time complexity O(MN), space complexity O(1)
+func useTwoPointers(s string, dict []string) string {
+	m := len(s)
+	var ans string
+	for i := range dict {
+		cur := dict[i]
+		var si, ci int
+		for si < len(cur) && ci < m {
+			if cur[si] == s[ci] {
+				si++
+			}
+			ci++
+		}
+		if si == len(cur) {
+			// find one in s
+			if len(ans) < len(cur) || len(ans) == len(cur) && cur < ans {
+				ans = cur
+			}
+		}
+	}
+	return ans
 }
