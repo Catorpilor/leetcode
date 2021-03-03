99d6e441ccb9ee23d1ff526551449d6d168d5601
Ubuntu <ubuntu@ubuntu.lan>
2021-03-03 15:53:22 +0000

    solve 895 use frequence map

--- 895_maximum_frequency_stack/stack.go
+++ 895_maximum_frequency_stack/stack.go
@@ -2,18 +2,42 @@ package stack
 
 // FreqStack is a stack with frequences.
 type FreqStack struct {
+	freq map[int]int
+	store map[int][]int
+	maxFreq int
 }
 
 // Construct create a FreqStack
 func Construct() *FreqStack {
-	return nil
+	return &FreqStack{
+		freq: make(map[int]int),
+		store: make(map[int][]int),
+	}
 }
 
 // Push push x into the stack
 func (fs *FreqStack) Push(x int) {
+	fs.freq[x]++
+	cf := fs.freq[x]
+	if cf > fs.maxFreq {
+		fs.maxFreq = cf
+	}
+	fs.store[cf] = append(fs.store[cf], x)
 }
 
 // Pop pop the most frequces one from the stack.
 func (fs *FreqStack) Pop() int {
-	return 0
+	var ans int
+	cands := fs.store[fs.maxFreq]
+	n := len(cands)
+	if n > 1 {
+		ans = cands[n-1]
+		cands = cands[:n-1]
+		fs.store[fs.maxFreq] = cands
+	}else{
+		ans = cands[0]
+		delete(fs.store, fs.maxFreq)
+		fs.maxFreq--
+	}
+	return ans
 }
