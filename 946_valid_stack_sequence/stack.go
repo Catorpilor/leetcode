9b15c9e7cad0d80b99221a6ee3b7ea181d18e278
Catorpilor <jiaojiangonghitster@gmail.com>
2021-03-08 20:11:58 +0800

    solve 946 use greedy

--- 946_valid_stack_sequence/stack.go
+++ 946_valid_stack_sequence/stack.go
@@ -1,5 +1,22 @@
 package stack
 
 func isValid(push, pop []int) bool {
-	return false
+	return useGreedy(push, pop)
+}
+
+// useGreedy time complexity O(N), space complexity O(N)
+func useGreedy(push, pop []int) bool {
+	n := len(push)
+	st := make([]int, 0, n)
+	var j int
+	for i := range push {
+		st = append(st, push[i])
+		nst := len(st)
+		for nst > 0 && j < n && st[nst-1] == pop[j] {
+			nst--
+			st = st[:nst]
+			j++
+		}
+	}
+	return j == n
 }
