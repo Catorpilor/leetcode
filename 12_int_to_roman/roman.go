7e20147cf0b47dbd9315ead6a18baf6d797eec0e
Catorpilor <jiaojiangonghitster@gmail.com>
2021-03-10 19:43:22 +0800

    solve 12 use iterator

--- 12_int_to_roman/roman.go
+++ 12_int_to_roman/roman.go
@@ -1,5 +1,20 @@
 package roman
 
-func intToRoman(num int) string{
-	return ""
+import "bytes"
+
+func intToRoman(num int) string {
+	return useIter(num)
+}
+
+func useIter(num int) string {
+	var sb bytes.Buffer
+	symbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	vals := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	for i := 0; num != 0; i++ {
+		for num >= vals[i] {
+			num -= vals[i]
+			sb.WriteString(symbols[i])
+		}
+	}
+	return sb.String()
 }
